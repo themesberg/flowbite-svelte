e8508195315f3d4e0fab13682e3de96f3280f2ca
Shinichi Okada <okada.shin@gmail.com>
2022-01-31 16:57:00 +0900

    Images added.

--- src/lib/navbar/navbar.svelte
+++ src/lib/navbar/navbar.svelte
@@ -1,6 +1,7 @@
 <script>
   export let sitename = "Svelte Flow";
   export let textSize = "base";
+  export let logoSrc = "/images/logo.png";
   export let menus = [
     {
       name: "Home",
@@ -38,30 +39,7 @@
 >
   <div class="container flex flex-wrap justify-between items-center mx-auto">
     <a href="/" class="flex">
-      <svg
-        class="mr-3 h-10"
-        width="51"
-        height="70"
-        viewBox="0 0 51 70"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        ><g clip-path="url(#clip0)"
-          ><path
-            d="M1 53H27.9022C40.6587 53 51 42.7025 51 30H24.0978C11.3412 30 1 40.2975 1 53Z"
-            fill="#76A9FA"
-          /><path
-            d="M-0.876544 32.1644L-0.876544 66.411C11.9849 66.411 22.4111 55.9847 22.4111 43.1233L22.4111 8.87674C10.1196 8.98051 0.518714 19.5571 -0.876544 32.1644Z"
-            fill="#A4CAFE"
-          /><path
-            d="M50 5H23.0978C10.3413 5 0 15.2975 0 28H26.9022C39.6588 28 50 17.7025 50 5Z"
-            fill="#1C64F2"
-          /></g
-        ><defs
-          ><clipPath id="clip0"
-            ><rect width="51" height="70" fill="white" /></clipPath
-          ></defs
-        ></svg
-      >
+      <img src={logoSrc} alt="" />
       <span
         class="self-center text-lg font-semibold text-gray-900 whitespace-nowrap dark:text-white"
         >{sitename}</span
@@ -75,28 +53,7 @@
       aria-expanded="false"
     >
       <span class="sr-only">Open main menu</span>
-      <svg
-        class="w-6 h-6"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-        ><path
-          fill-rule="evenodd"
-          d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-          clip-rule="evenodd"
-        /></svg
-      >
-      <svg
-        class="hidden w-6 h-6"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-        ><path
-          fill-rule="evenodd"
-          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-          clip-rule="evenodd"
-        /></svg
-      >
+      <img src={logoSrc} alt="" />
     </button>
     <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
       <ul
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,8 @@
 <script>
   import { Navbar } from "$lib/index";
+  let logoSrc = "/images/mkdir-logo.png";
+  let sitename = "Svelte Flow";
 </script>
 
-<Navbar />
+<Navbar {logoSrc} {sitename} />
 <slot />
