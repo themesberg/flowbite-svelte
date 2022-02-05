beb9b334e3b3d1dd8a6db7d0b9efa836fa3ab424
Shinichi Okada <okada.shin@gmail.com>
2022-02-05 17:20:44 +0900

    Updated Small modal page.

--- src/routes/getting-started.svelte
+++ src/routes/getting-started.svelte
@@ -1,3 +1,84 @@
-<div class="container mt-8 mx-auto">
-  <h1 class="text-2xl">Getting Started</h1>
+<script>
+  import Prism from "prismjs";
+  import "prismjs/themes/prism.css";
+  import "prism-themes/themes/prism-duotone-light.css";
+  // let language = "html";
+</script>
+
+<div class="container mt-4 flex flex-wrap mx-auto">
+  <h1 class="text-2xl w-full">Getting Started</h1>
+  <h2 class="text-xl w-full">Installation</h2>
+  <p>Install SvelteKit.</p>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+$ npm init svelte@next sveltekit-demo
+$ cd sveltekit-demo
+$ npm install
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
+  <p>Install Tailwind CSS and Flowbite.</p>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+$ npx svelte-add@latest tailwindcss
+$ npm i flowbite
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
+  <p>Install svelte-flow.</p>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+$ npm i -D svelte-flow
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
+  <p>Add the following in the script tag in the __layout.svelte :</p>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+  import "../app.css";
+  import "flowbite/dist/flowbite.css";
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
+  <p>Add the following in the script tag in the __layout.svelte :</p>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+<div class="mx-auto p-2">
+  <slot />
+</div>
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
+  <p>And modify the src/app.html adding flowbite.js as the following:</p>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <link rel="icon" href="%svelte.assets%/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	%svelte.head%
+    </head>
+  <body>
+    <div id="svelte">%svelte.body%</div>
+    <script src="https://unpkg.com/flowbite@1.3.2/dist/flowbite.js" ✂prettier:content✂="" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=">{}</script>
+  </body>
+</html>
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
 </div>
--- src/routes/modals/small.svelte
+++ src/routes/modals/small.svelte
@@ -1,6 +1,9 @@
 <script>
   import { SmallModal, ModalButton } from "$lib/index";
   import { goto } from "$app/navigation";
+  import Prism from "prismjs";
+  import "prismjs/themes/prism.css";
+  import "prism-themes/themes/prism-duotone-light.css";
 
   // common
   const textSColor = "blue";
@@ -37,12 +40,122 @@
   };
 </script>
 
-<div class="block p-8 space-y-4 md:flex md:space-y-0 md:space-x-4">
-  <!-- Simple  -->
+<div class="container mt-4 flex flex-wrap mx-auto">
+  <h1 class="text-3xl w-full">Small Modals</h1>
+  <p>
+    Import SmallModal and ModalButton components and set variables in the script
+    tag.
+  </p>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+  import { SmallModal, ModalButton } from "svelte-flow";
+
+  // common
+  const textSColor = "blue";
+
+  // for basic
+  const idBasic = "basic-modal";
+  const btnBasicName = "Basic Modal";
+
+  // for small modal 1
+  const btnName1 = "Small Modal";
+  const id1 = "small-modal";
+  const btnColor1 = "purple";
+
+  const handlebtnS1 = () => {
+    toggleModal(id1, false);
+    goto("/dummy-pages/about");
+  };
+
+  const handlebtnS2 = () => {
+    toggleModal(id1, false);
+  };
+
+  // for small modal 2
+  const btnName2 = "Small Modal 2";
+  const id2 = "small-modal-2";
+  const btnColor2 = "red";
+
+  const handlebtnS3 = () => {
+    toggleModal(id2, false);
+  };
+
+  const handlebtnS4 = () => {
+    toggleModal(id2, false);
+  };
+      `,
+      Prism.languages["javascript"]
+    )}
+  </div>
+  <p>Create a button and modal.</p>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+<ModalButton id={idBasic} btnName={btnBasicName} />
+<SmallModal id={idBasic} title={"Basic Modal Title"}>
+  Basic Modal Content
+</SmallModal>
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
+</div>
+<div class="container flex flex-wrap mt-4 mx-auto justify-center  pb-8">
   <ModalButton id={idBasic} btnName={btnBasicName} />
-
+</div>
+<div class="container mt-4 flex flex-wrap mx-auto">
+  <h1 class="text-3xl w-full">Small Modals with Action Buttons</h1>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+<ModalButton id={id2} btnName={btnName2} btnColor={btnColor2} />
+<SmallModal
+  id={id1}
+  btnColor={"pink"}
+  title={"Small Modal Title"}
+  btn1="View"
+  btn2="No"
+  on:handlebtn1={handlebtnS1}
+  on:handlebtn2={handlebtnS2}
+>
+  Modal 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua.
+</SmallModal>
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
+</div>
+<div class="container flex flex-wrap mt-8 mx-auto justify-center">
   <ModalButton id={id1} btnName={btnName1} btnColor={btnColor1} />
+</div>
 
+<div class="container mt-4 flex flex-wrap mx-auto">
+  <h1 class="text-3xl w-full">Small Modals with different colors</h1>
+  <div class="code">
+    {@html Prism.highlight(
+      `
+<ModalButton id={id2} btnName={btnName2} btnColor={btnColor2} />
+<SmallModal
+  id={id2}
+  btnColor={"yellow"}
+  textColor={textSColor}
+  title={"Small Modal Title"}
+  btn1="Yes"
+  btn2="No"
+  on:handlebtn1={handlebtnS3}
+  on:handlebtn2={handlebtnS4}
+>
+  Modal 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+  eiusmod tempor incididunt ut labore et dolore magna aliqua.
+</SmallModal>
+      `,
+      Prism.languages["html"]
+    )}
+  </div>
+</div>
+<div class="container flex flex-wrap mt-8 mx-auto justify-center">
   <ModalButton id={id2} btnName={btnName2} btnColor={btnColor2} />
 </div>
 
@@ -76,3 +189,7 @@
   Modal 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
   eiusmod tempor incididunt ut labore et dolore magna aliqua.
 </SmallModal>
+
+<div class="container mt-4 flex flex-wrap mx-auto pb-48">
+  <a href="/modals"> See more different sizes.</a>
+</div>
