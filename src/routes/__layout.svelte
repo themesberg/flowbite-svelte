95e019bcca76f5636fdc16513d42286bd383b387
Shinichi Okada <okada.shin@gmail.com>
2022-02-06 21:00:56 +0900

    Dark mode position fixed.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -148,7 +148,7 @@
     on:click={toggleTheme}
     id="theme-toggle"
     type="button"
-    class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+    class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 fixed  top-20 z-50 "
   >
     <svg
       id="theme-toggle-dark-icon"
