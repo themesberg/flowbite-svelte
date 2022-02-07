863dc516955065da80c921ffb2df52af21c41866
Shinichi Okada <okada.shin@gmail.com>
2022-02-07 17:16:17 +0900

    svelte.config.js update.

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,16 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ...mdsvexConfig.extensions],
-
   kit: {
     adapter: adapter(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
   },
 
   preprocess: [
     preprocess({
       postcss: true,
     }),
-    mdsvex(mdsvexConfig),
   ],
 };
 
