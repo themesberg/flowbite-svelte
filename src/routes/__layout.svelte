f1e55c3bb9360b6d746ff61c8af261157901c8f4
Shinichi Okada <okada.shin@gmail.com>
2022-02-07 15:52:34 +0900

    Dark mode button position update.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -34,7 +34,7 @@
   let siteClass = "w-10/12 pt-1 pl-8 sm:w-1/2";
   let siteText = "text-lg";
   let topli =
-    "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50  md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-300 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white  dark:border-gray-700 text-lg z-50 bg-white dark:bg-gray-700";
+    "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50  md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-300 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white  dark:border-gray-700 text-lg z-50 bg-white dark:bg-gray-800";
   let topul =
     "flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium pt-1 dark:bg-gray-800";
   let topMenuDiv =
