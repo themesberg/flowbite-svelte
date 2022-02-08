e8c5ffa9aee2bd30ab428e0aa2f061ebec015677
Shinichi Okada <okada.shin@gmail.com>
2022-02-08 09:59:21 +0900

    DarkMode button position update.

--- src/lib/darkmode/DarkMode.svelte
+++ src/lib/darkmode/DarkMode.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
   export let btnClass =
-    "text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 fixed left-1 top-16 z-50";
+    "text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 fixed left-0 top-8 z-50";
 
   let mode;
 
