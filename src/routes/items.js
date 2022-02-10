f131f6727db82ce8408fe5e54a5eda15607c53dc
Shinichi Okada <okada.shin@gmail.com>
2022-02-10 21:04:27 +0900

    items.js update removing rel=external.

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,66 +1,61 @@
-export let components = [
-  { url: "alerts", name: "Alerts", rel: "external" },
-  { url: "buttons", name: "Buttons", rel: "external" },
-  { url: "cards", name: "Cards", rel: "external" },
-  { url: "list-group", name: "List Group", rel: "external" },
-  { url: "modals", name: "Modals", rel: "external" },
-  { url: "tabs", name: "Tabs", rel: "external" },
-];
-
 export let alerts = [
   { url: "alerts", name: "Alerts", rel: "external" },
 ];
 
 export let buttons = [
-  { url: "buttons", name: "Buttons", rel: "external" },
+  { url: "buttons", name: "Buttons" },
 ];
 
 export let cards = [
-  { url: "cards/card", name: "Card", rel: "external" },
-  { url: "cards/ecommerce", name: "E-commerce", rel: "external" },
-  { url: "cards/cta", name: "CTA", rel: "external" },
-  { url: "cards/horizontal", name: "Horizontal", rel: "external" },
+  { url: "cards/card", name: "Card" },
+  { url: "cards/ecommerce", name: "E-commerce" },
+  { url: "cards/cta", name: "CTA" },
+  { url: "cards/horizontal", name: "Horizontal" },
   { url: "cards/interactive", name: "Interactive", rel: "external" },
-  { url: "cards/list", name: "List", rel: "external" },
+  { url: "cards/list", name: "List" },
 ];
 
+export let darkmode = [
+  { url: "darkmode", name: "Dark mode" },
+]
+
 export let list_group = [
-  { url: "list-group", name: "List group", rel: "external" },
+  { url: "list-group", name: "List group" },
 ];
 
 export let modals = [
-  { url: "modals", name: "Modals", rel: "external" },
-  { url: "modals/small", name: "Small", rel: "external" },
-  { url: "modals/medium", name: "Medium", rel: "external" },
-  { url: "modals/large", name: "Large", rel: "external" },
-  { url: "modals/extra-large", name: "Extra-large", rel: "external" },
-  { url: "modals/signin", name: "Sign-in", rel: "external" },
-  { url: "modals/all-modals", name: "All modals", rel: "external" },
+  { url: "modals", name: "Modals" },
+  { url: "modals/small", name: "Small" },
+  { url: "modals/medium", name: "Medium" },
+  { url: "modals/large", name: "Large" },
+  { url: "modals/extra-large", name: "Extra-large" },
+  { url: "modals/signin", name: "Sign-in" },
+  { url: "modals/all-modals", name: "All modals" },
 ];
 
 export let navbar = [
-  { url: "navbar", name: "Navbar", rel: "external" },
+  { url: "navbar", name: "Navbar" },
+  { url: "navbar/dropdown", name: "Dropdown Navbar", rel: "external" },
 ];
 
 export let tabs = [
-  { url: "tabs", name: "Modals", rel: "external" },
-  { url: "tabs/default-tabs", name: "Default", rel: "external" },
+  { url: "tabs", name: "Tabs" },
+  { url: "tabs/default-tabs", name: "Default" },
   { url: "tabs/interactive-tabs", name: "Interactive", rel: "external" },
   { url: "tabs/multiple-interactive-tabs", name: "Multiple Interactive", rel: "external" },
-  { url: "tabs/pilltabs", name: "Pilltabs", rel: "external" },
+  { url: "tabs/pilltabs", name: "Pilltabs" },
 ];
 
 export let svelteflows = [
-  { url: "getting-started", name: "Getting Started", rel: "external" },
-  { url: "about", name: "About", rel: "external" },
+  { url: "getting-started", name: "Getting Started" },
+  { url: "about", name: "About" },
 ];
 
 export let topMenus = [
-  { route: "/", name: "Home", rel: "external" },
-  { route: "/about", name: "About", rel: "external" },
+  { route: "/", name: "Home" },
+  { route: "/about", name: "About" },
   {
     route: "https://github.com/shinokada/svelte-flow",
-    name: "GitHub",
-    rel: "",
+    name: "GitHub"
   },
 ];
