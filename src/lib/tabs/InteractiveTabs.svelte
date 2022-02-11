841dff3504b707618cf4de0b45f231eb263998d1
Shinichi Okada <okada.shin@gmail.com>
2022-02-11 22:26:04 +0900

    Tab interactive is done.

--- src/lib/tabs/InteractiveTabs.svelte
+++ src/lib/tabs/InteractiveTabs.svelte
@@ -1,29 +1,32 @@
 <script>
-  import { handleTabs } from "./handleTabs";
+  // import { handleTabs } from "./handleTabs";
   export let tabId = "myTab";
+
+  export let activeTabValue = 1;
+  const handleClick = (tabValue) => () => (activeTabValue = tabValue);
 
   export let tabs = [
     {
-      name: "Profile",
-      selected: true,
+      lavel: "Profile",
+      id: 1,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     },
     {
-      name: "Dashboard",
-      selected: false,
+      lavel: "Dashboard",
+      id: 2,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     },
     {
-      name: "Settings",
-      selected: false,
+      lavel: "Settings",
+      id: 3,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     },
     {
       name: "Contacts",
-      selected: false,
+      id: 4,
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     },
@@ -35,39 +38,37 @@
     class="flex flex-wrap -mb-px"
     id={tabId}
     data-tabs-toggle="#{tabId}Content"
-    on:click={handleTabs}
     role="tablist"
   >
-    {#each tabs as { name, selected }}
-      <li class="mr-2" role="presentation">
+    {#each tabs as { label, selected, id }}
+      <li class="mr-2" role="presentation" class:active={activeTabValue === id}>
         <button
-          class="{selected
-            ? 'active'
-            : ''} inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300"
-          id="{name}-tab"
-          data-tabs-target="#{name}"
+          on:click={handleClick(id)}
+          class="inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300"
+          id="{label}-tab"
+          data-tabs-target="#{label}"
           type="button"
           role="tab"
-          aria-controls={name}
-          aria-selected={selected}>{name}</button
+          aria-controls={label}
+          aria-selected={selected}>{label}</button
         >
       </li>
     {/each}
   </ul>
 </div>
 <div id="{tabId}Content">
-  {#each tabs as { name, content, selected }}
-    <div
-      class="{selected
-        ? ''
-        : 'hidden'} p-4 bg-gray-50 rounded-lg dark:bg-gray-800"
-      id={name}
-      role="tabpanel"
-      aria-labelledby="{name}-tab"
-    >
-      <p class="text-sm text-gray-500 dark:text-gray-400">
-        {content}
-      </p>
-    </div>
+  {#each tabs as { label, content, id }}
+    {#if activeTabValue == id}
+      <div
+        class="p-4 bg-gray-50 rounded-lg dark:bg-gray-800"
+        id={label}
+        role="tabpanel"
+        aria-labelledby="{label}-tab"
+      >
+        <p class="text-sm text-gray-500 dark:text-gray-400">
+          {content}
+        </p>
+      </div>
+    {/if}
   {/each}
 </div>
--- src/routes/tabs/interactive-tabs.svelte
+++ src/routes/tabs/interactive-tabs.svelte
@@ -2,26 +2,26 @@
   import { InteractiveTabs } from "$lib/index";
   let tabs1 = [
     {
-      name: "Profile-1",
-      selected: true,
+      label: "Profile-1",
+      id: 1,
       content:
         "1-1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     },
     {
-      name: "Dashboard-1",
-      selected: false,
+      label: "Dashboard-1",
+      id: 2,
       content:
         "1-2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     },
     {
-      name: "Settings-1",
-      selected: false,
+      label: "Settings-1",
+      id: 3,
       content:
         "1-3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     },
     {
-      name: "Contacts-1",
-      selected: false,
+      label: "Contacts-1",
+      id: 4,
       content:
         "1-4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     },
