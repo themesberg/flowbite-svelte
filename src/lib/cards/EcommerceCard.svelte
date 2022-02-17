567de40a651adbb80b1005598f6a949adc781423
Shinichi Okada <okada.shin@gmail.com>
2022-02-18 07:21:19 +0900

    main: EcommercCard updated.

--- src/lib/cards/EcommerceCard.svelte
+++ src/lib/cards/EcommerceCard.svelte
@@ -10,6 +10,54 @@
     "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport";
   export let stars = "5.0";
   export let price = "$555";
+  let spanClass;
+  let linkClass;
+  if (btnColor === "blue") {
+    spanClass =
+      "bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3";
+    linkClass =
+      "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800";
+  } else if (btnColor === "gray") {
+    spanClass =
+      "bg-gray-100 text-gray-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-gray-200 dark:text-gray-800 ml-3";
+    linkClass =
+      "text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800";
+  } else if (btnColor === "red") {
+    spanClass =
+      "bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-800 ml-3";
+    linkClass =
+      "text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800";
+  } else if (btnColor === "yellow") {
+    spanClass =
+      "bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-800 ml-3";
+    linkClass =
+      "text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800";
+  } else if (btnColor === "green") {
+    spanClass =
+      "bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-800 ml-3";
+    linkClass =
+      "text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800";
+  } else if (btnColor === "indigo") {
+    spanClass =
+      "bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-800 ml-3";
+    linkClass =
+      "text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800";
+  } else if (btnColor === "purple") {
+    spanClass =
+      "bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-800 ml-3";
+    linkClass =
+      "text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800";
+  } else if (btnColor === "pink") {
+    spanClass =
+      "bg-pink-100 text-pink-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-pink-200 dark:text-pink-800 ml-3";
+    linkClass =
+      "text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800";
+  } else {
+    spanClass =
+      "bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3";
+    linkClass =
+      "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800";
+  }
 </script>
 
 <div
@@ -38,21 +86,13 @@
           /></svg
         >
       {/each}
-      <span
-        class="bg-{btnColor}-100 text-{btnColor}-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-{btnColor}-200 dark:text-{btnColor}-800 ml-3"
-        >{stars}</span
-      >
+      <span class={spanClass}>{stars}</span>
     </div>
     <div class="flex justify-between items-center">
       <span class="text-3xl font-bold text-gray-900 dark:text-white"
         >{price}</span
       >
-      <a
-        href={link}
-        {rel}
-        class="text-white bg-{btnColor}-700 hover:bg-{btnColor}-800 focus:ring-4 focus:ring-{btnColor}-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-{btnColor}-600 dark:hover:bg-{btnColor}-700 dark:focus:ring-{btnColor}-800"
-        >Add to cart</a
-      >
+      <a href={link} {rel} class={linkClass}>Add to cart</a>
     </div>
   </div>
 </div>
