d2465abcaf5329b7b6a4c208c1ec71b6100e6766
Shinichi Okada <okada.shin@gmail.com>
2022-02-18 07:13:12 +0900

    main: CTA card is updated.

--- src/lib/cards/CtaCard.svelte
+++ src/lib/cards/CtaCard.svelte
@@ -20,12 +20,32 @@
       rounded: null,
     },
   ];
+  let headClass;
+  if (headColor === "blue") {
+    headClass = "mb-2 text-3xl font-bold text-blue-900 dark:text-white";
+  } else if (headColor === "gray") {
+    headClass = "mb-2 text-3xl font-bold text-gray-900 dark:text-white";
+  } else if (headColor === "red") {
+    headClass = "mb-2 text-3xl font-bold text-red-900 dark:text-white";
+  } else if (headColor === "yellow") {
+    headClass = "mb-2 text-3xl font-bold text-yellow-900 dark:text-white";
+  } else if (headColor === "green") {
+    headClass = "mb-2 text-3xl font-bold text-green-900 dark:text-white";
+  } else if (headColor === "indigo") {
+    headClass = "mb-2 text-3xl font-bold text-indigo-900 dark:text-white";
+  } else if (headColor === "purple") {
+    headClass = "mb-2 text-3xl font-bold text-purple-900 dark:text-white";
+  } else if (headColor === "pink") {
+    headClass = "mb-2 text-3xl font-bold text-pink-900 dark:text-white";
+  } else {
+    headClass = "mb-2 text-3xl font-bold text-blue-900 dark:text-white";
+  }
 </script>
 
 <div
   class="p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700"
 >
-  <h3 class="mb-2 text-3xl font-bold text-{headColor}-900 dark:text-white">
+  <h3 class={headClass}>
     {title}
   </h3>
   <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
--- src/routes/cards/cta-card.svelte
+++ src/routes/cards/cta-card.svelte
@@ -15,7 +15,24 @@
       size: "base",
       name: "Get in on",
       type: "green-outline",
-      link: "/#",
+      link: "/",
+      rel: undefined,
+    },
+  ];
+  let btns2 = [
+    {
+      size: "base",
+      name: "Download it",
+      type: "green",
+      link: "/about",
+      rel: undefined,
+      rounded: true,
+    },
+    {
+      size: "base",
+      name: "Get in on",
+      type: "red-outline",
+      link: "/",
       rel: undefined,
     },
   ];
@@ -24,6 +41,13 @@
 <div class="flex flex-wrap mx-auto p-8">
   <div class="flex-auto m-2">
     <CtaCard {btns} {title} {headColor}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt libero
+      dicta ex, suscipit, qui beatae in odio corrupti est quis quibusdam
+      explicabo non atque!
+    </CtaCard>
+  </div>
+  <div class="flex-auto m-2">
+    <CtaCard btns={btns2} {title} headColor="red">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt libero
       dicta ex, suscipit, qui beatae in odio corrupti est quis quibusdam
       explicabo non atque!
