c57e641117f5389194ecbcc8e0760a55e8a35f47
Shinichi Okada <okada.shin@gmail.com>
2022-02-18 22:15:11 +0900

    main: Signinmodal updated.

--- src/routes/modals/signin.svelte
+++ src/routes/modals/signin.svelte
@@ -3,23 +3,34 @@
 
   let signinId = "signin-modal2";
   let btnSignInName = "Sign In Modal";
-  let btnColor = "red";
+  let btnColor = "yellow";
 </script>
 
 <div class="block p-8 space-y-4 md:flex md:space-y-0 md:space-x-4">
   <!-- Modal toggle -->
 
   <ModalButton id={signinId} btnName={btnSignInName} {btnColor} />
+  <ModalButton id="id2" btnName={btnSignInName} btnColor="red" />
 </div>
 
 <div class="p-8">
   <SignInModal
     id={signinId}
-    btnSignInColor={"blue"}
+    btnSignInColor={"indigo"}
     titleSignIn={"SignIn Modal Title"}
-    textSignInColor="gray"
     lostPasswordLink={"/auth/lost-password"}
     signUpLink={"/auth/signup"}
     formLink={"/auth/signin"}
+  />
+</div>
+
+<div class="p-8">
+  <SignInModal
+    id="id2"
+    btnSignInColor="purple"
+    titleSignIn="SignIn Modal Title 2"
+    lostPasswordLink="/auth/lost-password"
+    signUpLink="/auth/signup"
+    formLink="/auth/signin"
   />
 </div>
