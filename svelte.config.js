eaf78dcf5a11bda3343058eb69a4a8fbdbe444c5
Shinichi Okada <okada.shin@gmail.com>
2022-03-09 14:10:55 +0900

    fix: update package.json in order to move src/routes to main.

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
-		})
+		}),
+		mdsvex(mdsvexConfig)
 	],
 
 	kit: {
