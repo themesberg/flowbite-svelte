980ff511f5c75c0a47c1f5378f952054c3c44e9a
Shinichi Okada <okada.shin@gmail.com>
2022-03-15 20:46:02 +0900

    fix: update links

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,64 +1,64 @@
 export const svelteflows = [
-  { href: "getting-started", name: "Getting Started" },
-  { href: "about", name: "About" },
+  { href: "/getting-started", name: "Getting Started" },
+  { href: "/about", name: "About" },
 ];
 
 export const accordions = [
-  { href: "accordions/default", name: "Accordion" },
-  { href: "accordions/icon-accordion", name: "Icon accordion" },
+  { href: "/accordions/default", name: "Accordion" },
+  { href: "/accordions/icon-accordion", name: "Icon accordion" },
 ];
 
 export let alerts = [
-  { href: "alerts/default-alert", name: "Alert" },
-  { href: "alerts/border-alert", name: "Border alert" },
-  { href: "alerts/information-alert", name: "Information alert" },
+  { href: "/alerts/default-alert", name: "Alert" },
+  { href: "/alerts/border-alert", name: "Border alert" },
+  { href: "/alerts/information-alert", name: "Information alert" },
 ];
 
 export const badges = [
-  { href: "badges/default", name: "Badges" },
-  { href: "badges/badge-icon", name: "Badges with icon" },
-  { href: "badges/badge-link", name: "Badges with link" },
+  { href: "/badges/default", name: "Badges" },
+  { href: "/badges/badge-icon", name: "Badges with icon" },
+  { href: "/badges/badge-link", name: "Badges with link" },
 ];
 
 export const buttons = [
-  { href: "buttons/setup", name: "Setup" },
-  { href: "buttons/default", name: "Default" },
-  { href: "buttons/colored-shadow", name: "Colored shadow" },
-  { href: "buttons/gradient-duotone", name: "Gradient duotone" },
-  { href: "buttons/gradient-monochrome", name: "Gradient monochrome" },
-  { href: "buttons/gradient-outline", name: "Gradient outline" },
-  { href: "buttons/outlined", name: "Outlined" },
-  { href: "buttons/rounded", name: "Rounded" },
+  { href: "/buttons/setup", name: "Setup" },
+  { href: "/buttons/default", name: "Default" },
+  { href: "/buttons/colored-shadow", name: "Colored shadow" },
+  { href: "/buttons/gradient-duotone", name: "Gradient duotone" },
+  { href: "/buttons/gradient-monochrome", name: "Gradient monochrome" },
+  { href: "/buttons/gradient-outline", name: "Gradient outline" },
+  { href: "/buttons/outlined", name: "Outlined" },
+  { href: "/buttons/rounded", name: "Rounded" },
 ];
 
 export const buttonGroups = [
-  { href: "button-groups/default", name: "Button group" },
-  { href: "button-groups/outline", name: "Outline button group" },
+  { href: "/button-groups/default", name: "Button group" },
+  { href: "/button-groups/outline", name: "Outline button group" },
 ];
 
 export const cards = [
-  { href: "cards/card", name: "Card" },
-  { href: "cards/ecommerce", name: "E-commerce" },
-  { href: "cards/cta", name: "CTA" },
-  { href: "cards/horizontal", name: "Horizontal" },
+  { href: "/cards/card", name: "Card" },
+  { href: "/cards/ecommerce", name: "E-commerce" },
+  { href: "/cards/cta", name: "CTA" },
+  { href: "/cards/horizontal", name: "Horizontal" },
   // { href: "cards/interactive", name: "Interactive" },
-  { href: "cards/list", name: "List" },
-  { href: "cards/signin", name: "SignIn" },
+  { href: "/cards/list", name: "List" },
+  { href: "/cards/signin", name: "SignIn" },
 ];
 
 // export const darkmode = [
 //   { href: "darkmode", name: "Dark mode" },
 // ]
 export const footers = [
-  { href: "footer/default", name: "Default" },
-  { href: "footer/logo", name: "Logo" },
-  { href: "footer/socialmedia", name: "Socialmedia" },
-  { href: "footer/sitemap", name: "Sitemap" },
+  { href: "/footer/default", name: "Default" },
+  { href: "/footer/logo", name: "Logo" },
+  { href: "/footer/socialmedia", name: "Socialmedia" },
+  { href: "/footer/sitemap", name: "Sitemap" },
 ]
 
 export const icons = [
-  { href: "icons/heroicons", name: "Heroicons" },
-  { href: "icons/simple-icons", name: "Simple-icons" },
+  { href: "/icons/heroicons", name: "Heroicons" },
+  { href: "/icons/simple-icons", name: "Simple-icons" },
 ]
 
 // export const list_group = [
@@ -66,42 +66,42 @@ export const icons = [
 // ];
 
 export const modals = [
-  { href: "modals/small", name: "Small" },
-  { href: "modals/medium", name: "Medium" },
-  { href: "modals/large", name: "Large" },
-  { href: "modals/extra-large", name: "Extra-large" },
-  { href: "modals/signin", name: "Sign-in" },
-  { href: "modals/all-modals", name: "All modals" },
+  { href: "/modals/small", name: "Small" },
+  { href: "/modals/medium", name: "Medium" },
+  { href: "/modals/large", name: "Large" },
+  { href: "/modals/extra-large", name: "Extra-large" },
+  { href: "/modals/signin", name: "Sign-in" },
+  { href: "/modals/all-modals", name: "All modals" },
 ];
 
 export const navbar = [
-  { href: "navbars/props", name: "Navbar Props" },
-  { href: "navbars/default", name: "Navbar" },
-  { href: "navbars/dropdown", name: "Dropdown" },
+  { href: "/navbars/props", name: "Navbar Props" },
+  { href: "/navbars/default", name: "Navbar" },
+  { href: "/navbars/dropdown", name: "Dropdown" },
 ];
 
 export const spinners = [
-  { href: "spinners/default", name: "Spinner" },
-  { href: "spinners/button-spinner", name: "Button spinner" },
+  { href: "/spinners/default", name: "Spinner" },
+  { href: "/spinners/button-spinner", name: "Button spinner" },
 ];
 
 export const tabs = [
-  { href: "tabs/default-tabs", name: "Default" },
-  { href: "tabs/interactive-tabs", name: "Interactive" },
-  { href: "tabs/multiple-interactive-tabs", name: "Multiple Interactive" },
-  { href: "tabs/pilltabs", name: "Pilltabs" },
+  { href: "/tabs/default-tabs", name: "Default" },
+  { href: "/tabs/interactive-tabs", name: "Interactive" },
+  { href: "/tabs/multiple-interactive-tabs", name: "Multiple Interactive" },
+  { href: "/tabs/pilltabs", name: "Pilltabs" },
 ];
 
 export const tooltips = [
-  { href: "tooltips/default", name: "Default" },
-  { href: "tooltips/light", name: "Light tooltip" },
+  { href: "/tooltips/default", name: "Default" },
+  { href: "/tooltips/light", name: "Light tooltip" },
 ];
 
 export const topMenus = [
-  { link: "/", name: "Home" },
-  { link: "/about", name: "About" },
+  { href: "/", name: "Home" },
+  { href: "/about", name: "About" },
   {
-    link: "https://github.com/shinokada/flowbite-svelte",
+    href: "https://github.com/shinokada/flowbite-svelte",
     name: "GitHub"
   },
 ];
