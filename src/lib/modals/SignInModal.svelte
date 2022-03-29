42f298609bd23b24a0be78f8fbf7999adc91bbe7
Garrett Suhm <garrett@bigroadadventure.com>
2022-03-28 18:00:12 -0600

    Added emailPlaceholder property

--- src/lib/modals/SignInModal.svelte
+++ src/lib/modals/SignInModal.svelte
@@ -8,6 +8,7 @@
 	export let rememberMe = false;
 	export let signUp: string;
 	export let action: string;
+	export let emailPlaceholder: string = 'name@company.com';
 
 	const closeModal = () => {
 		modalIdStore.update((value) => (value = null));
@@ -93,7 +94,7 @@
 							name="email"
 							id="email-{id}"
 							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-							placeholder="name@company.com"
+							placeholder={emailPlaceholder}
 							required
 						/>
 					</div>
