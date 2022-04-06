88eb6f2271728e448b4ca7b1a4dd2b89ea6c6e97
Shinichi Okada <okada.shin@gmail.com>
2022-04-06 18:20:36 +0900

    fix: Table component

--- src/lib/tables/Table.svelte
+++ src/lib/tables/Table.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	export let header;
-	export let products;
+	export let items;
 </script>
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -15,22 +15,23 @@
 			</tr>
 		</thead>
 		<tbody>
-			{#each products as { name, color, category, price, action }}
+			{#each items as item}
 				<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-					<th
-						scope="row"
-						class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-					>
-						{name}
-					</th>
-					<td class="px-6 py-4"> {color} </td>
-					<td class="px-6 py-4"> {category} </td>
-					<td class="px-6 py-4"> {price} </td>
-					<td class="px-6 py-4">
-						<a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-							>{action}</a
-						>
-					</td>
+					{#each item as cell, i}
+						{#if i === 0}
+							<th
+								scope="row"
+								class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+							>
+								{@html cell}
+								{i}
+							</th>
+						{:else}
+							<td class="px-6 py-4">
+								{@html cell}
+							</td>
+						{/if}
+					{/each}
 				</tr>
 			{/each}
 		</tbody>
--- src/routes/tests/table.svelte
+++ src/routes/tests/table.svelte
@@ -1,29 +1,11 @@
 <script>
 	import { Table } from '$lib/index';
 	let header = ['Product name', 'Color', 'Category', 'Price', 'Action'];
-	let products = [
-		{
-			name: 'Apple MacBook Pro 17',
-			color: 'Silver',
-			category: 'Laptop',
-			price: '$2222',
-			action: 'Edit'
-		},
-		{
-			name: 'Microsoft Surface Pro',
-			color: 'White',
-			category: 'Laptop PC',
-			price: '$1999',
-			action: 'Edit'
-		},
-		{
-			name: '	Magic Mouse 2',
-			color: 'Black',
-			category: 'Accessories',
-			price: '$99',
-			action: 'Edit'
-		}
+	let items = [
+		['Apple MacBook Pro 17', 'Silver', 'Laptop', '$2222', 'Edit'],
+		['Microsoft Surface Pro', 'White', 'Laptop PC', '$1999', 'Edit'],
+		['	Magic Mouse 2', 'Black', 'Accessories', '$99', 'Edit']
 	];
 </script>
 
-<Table {header} {products} />
+<Table {header} {items} />
