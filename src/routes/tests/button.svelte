3a1b452f925e14b2d159456c8f1f50b2972ae52b
Shinichi Okada <okada.shin@gmail.com>
2022-04-15 11:07:13 +0900

    fix: refresh dev environment

---
+++ src/routes/tests/button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { ColorShadowButton } from '$lib/index';
+</script>
+
+<section>
+  <h1>Welcome to your new SvelteKit app</h1>
+
+  <ColorShadowButton color="blue" name="Blue" />
+  <ColorShadowButton color="green" name="Green" />
+  <ColorShadowButton color="cyan" name="Cyan" />
+  <ColorShadowButton color="teal" name="Teal" />
+  <ColorShadowButton color="lime" name="Lime" />
+  <ColorShadowButton color="red" name="Red" />
+  <ColorShadowButton color="pink" name="Pink" />
+  <ColorShadowButton color="purple" name="Purple" />
+</section>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,14 @@
 const config = {
-  content: [
+	content: [
     './src/**/*.{html,js,svelte,ts}',
     "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"
   ],
-  safelist: [
-    'from-cyan-200', 'via-cyan-400', 'to-cyan-500', 'bg-cyan-300',
-  ],
 
-  theme: {
-    colors: {
-      'ebony': '#0E172A',
-      'bigstone': '#173242',
-    },
+	theme: {
 		extend: {}
 	},
 
-  plugins: [
+	plugins: [
     require('flowbite/plugin')
   ],
   darkMode: 'class',
