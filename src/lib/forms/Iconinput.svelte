34c8ad5dc0ca6c1ce39c6281c185274fd0f49ca7
Shinichi Okada <okada.shin@gmail.com>
2022-04-28 06:09:52 +0900

    fix: add 32997restProps to Iconinput

--- src/lib/forms/Iconinput.svelte
+++ src/lib/forms/Iconinput.svelte
@@ -18,9 +18,6 @@
 	export let noBorderDivClass: string = 'flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none';
 	export let iconClass: string = 'h-4 w-4 mr-2';
 	export let iconSize: number = 18;
-	export let required: boolean = false;
-	export let disabled: boolean = false;
-	export let readonly: boolean = false;
 
 	function setType(node) {
 		node.type = type;
@@ -34,14 +31,14 @@
 			<div class={noBorderDivClass}>
 				<svelte:component this={icon} size={iconSize} class={iconClass} />
 			</div>
-			<input bind:value use:setType {id} class={noBorderInputClass} {placeholder} {required} {disabled} {readonly} />
+			<input bind:value use:setType {id} class={noBorderInputClass} {placeholder} {...$$restProps} />
 		</div>
 	{:else}
 		<div class="flex">
 			<span class={spanClass}>
 				<svelte:component this={icon} size={iconSize} class={iconClass} />
 			</span>
-			<input {type} {id} class={inputClass} {placeholder} {required} {disabled} {readonly} />
+			<input {type} {id} class={inputClass} {placeholder} {...$$restProps} />
 		</div>
 	{/if}
 	{#if helper}
