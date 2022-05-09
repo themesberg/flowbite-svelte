7b8a642284947c7b8c6bbabecad2ddb2cc1f3d04
Shinichi Okada <okada.shin@gmail.com>
2022-05-09 15:42:36 +0900

    fix: remove svelte-collapse from Accordion component

--- src/lib/accordions/AccordionItem.svelte
+++ src/lib/accordions/AccordionItem.svelte
@@ -1,37 +1,35 @@
 <script lang="ts">
-	import { getContext } from 'svelte';
-	import collapse from 'svelte-collapse';
+	import { slide } from 'svelte/transition';
 	import { ChevronDownSolid, ChevronUpSolid } from 'svelte-heros';
+
 	export let id: string = '';
+
 	export let btnClass: string = 'flex items-center focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 justify-between p-5 w-full font-medium border border-gray-200 dark:border-gray-700 text-left text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800';
+
 	export let slotClass: string = 'p-5 border border-t-0 border-gray-200 dark:border-gray-700';
-	const store = getContext('svelte-collapsible-accordion');
-	$: params = {
-		open: $store.id === id,
-		duration: $store.duration,
-		easing: $store.easing
-	};
-	const handleToggle = () => {
-		if (params.open) {
-			store.update((s) => Object.assign(s, { id: null }));
-		} else {
-			store.update((s) => Object.assign(s, { id }));
-		}
+
+	let isOpen = false;
+	let activeDropdown: string = undefined;
+	const handleToggle = (id: string) => {
+		isOpen = !isOpen;
+		activeDropdown = id;
 	};
 </script>
 
-<h2 aria-expanded={params.open}>
-	<button on:click={handleToggle} type="button" class:rounded-t-xl={id === '1'} class:border-t-0={id !== '1'} class={btnClass}>
+<h2 aria-expanded={isOpen}>
+	<button on:click={() => handleToggle(id)} type="button" class:rounded-t-xl={id === '1'} class:border-t-0={id !== '1'} class={btnClass}>
 		<span><slot name="header" /></span>
-		{#if params.open}
+		{#if isOpen}
 			<ChevronUpSolid />
 		{:else}
 			<ChevronDownSolid />
 		{/if}
 	</button>
 </h2>
-<div use:collapse={params}>
-	<div class={slotClass}>
-		<slot name="body" />
+{#if isOpen}
+	<div transition:slide={{ duration: 500 }}>
+		<div class={slotClass}>
+			<slot name="body" />
+		</div>
 	</div>
-</div>
+{/if}
