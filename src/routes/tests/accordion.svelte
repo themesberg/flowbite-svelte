23abc14ffb4bda93f350863e6d5a2557f8a8fff4
Shinichi Okada <okada.shin@gmail.com>
2022-05-15 14:00:04 +0900

    chore: add accordion test

---
+++ src/routes/tests/accordion.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { AccordionItem } from '$lib/index';
+</script>
+
+<AccordionItem id="1" flush class="myclass">
+	<div slot="header">Test</div>
+	<div slot="body">Thing</div>
+</AccordionItem>
+
+<div class=" p-5 myclass">Testing</div>
+
+<AccordionItem id="1" flush class="py-0">
+	<div slot="header">Test</div>
+	<div slot="body">Thing</div>
+</AccordionItem>
+
+<style>
+	.myclass {
+		padding: 0;
+		color: red;
+	}
+</style>
