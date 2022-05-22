755ae7f39823f9cdbcea375722eadabcc80d385a
Shinichi Okada <okada.shin@gmail.com>
2022-05-22 16:15:34 +0900

    feat: add transition slide to navbar

--- src/lib/navbar/NavUl.svelte
+++ src/lib/navbar/NavUl.svelte
@@ -8,11 +8,16 @@
 		'flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium';
 	export let hidden = true;
 	// $: hidden;
-	// $: console.log('hidden: ', hidden);
+	$: console.log('hidden: ', hidden);
 </script>
 
-<div class={divClass} class:hidden id="mobile-menu">
-	<ul class="{ulClass} {$$props.class || ''}">
-		<slot />
-	</ul>
-</div>
+{#if !hidden}
+	<div class={divClass} class:hidden id="mobile-menu">
+		<ul
+			class="{ulClass} {$$props.class || ''}"
+			transition:slide={{ delay: 250, duration: 500, easing: quintOut }}
+		>
+			<slot />
+		</ul>
+	</div>
+{/if}
