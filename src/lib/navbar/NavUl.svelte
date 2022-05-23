da3d34a34d209b599440225baa92d440ff291e03
Shinichi Okada <okada.shin@gmail.com>
2022-05-24 08:35:42 +0900

    feat: add transition slide to navbar

--- src/lib/navbar/NavUl.svelte
+++ src/lib/navbar/NavUl.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
-	export let divClass: string =
-		'hidden justify-between items-center w-full md:flex md:w-auto md:order-1';
+	export let divClass: string = ' justify-between items-center w-full md:flex md:w-auto md:order-1';
 
 	export let ulClass =
 		'flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium';
@@ -11,8 +10,20 @@
 	// $: console.log('hidden: ', hidden);
 </script>
 
-<div class={divClass} class:hidden id="mobile-menu">
-	<ul class="{ulClass} {$$props.class || ''}">
-		<slot />
-	</ul>
-</div>
+{#if !hidden}
+	<div
+		class={divClass}
+		id="mobile-menu"
+		transition:slide={{ delay: 250, duration: 500, easing: quintOut }}
+	>
+		<ul class="{ulClass} {$$props.class || ''}">
+			<slot />
+		</ul>
+	</div>
+{:else}
+	<div class={divClass} {hidden} id="mobile-menu">
+		<ul class="{ulClass} {$$props.class || ''}">
+			<slot />
+		</ul>
+	</div>
+{/if}
