1aefa6bd85acabc05b9ba8016b8e1f46bd55baef
Shinichi Okada <okada.shin@gmail.com>
2022-06-07 12:37:16 +0900

    feat: add type to ButtonGroupItem

--- src/lib/buttongroups/ButtonGroupItem.svelte
+++ src/lib/buttongroups/ButtonGroupItem.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import type { ButtonType } from '../types';
+	export let type: ButtonType = 'button';
 	export let btnClass =
 		'inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white first:rounded-l-lg border-t border-b first:border last:rounded-r-md last:border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white';
 
@@ -15,7 +17,7 @@
 {#if href}
 	<a {href} class="{btnClass} {$$props.class}" on:click><slot /></a>
 {:else}
-	<button type="button" class="{btnClass} {$$props.class}" on:click>
+	<button {type} class="{btnClass} {$$props.class}" on:click>
 		<slot />
 	</button>
 {/if}
