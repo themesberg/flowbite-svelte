c3c455b33edf1a64aa0823188eba2010b8e9db5e
Shinichi Okada <okada.shin@gmail.com>
2022-06-14 13:17:27 +0900

    chore: add tests/buttongroup

--- src/routes/tests/buttongroup.svelte
+++ src/routes/tests/buttongroup.svelte
@@ -7,12 +7,14 @@
 	<ButtonGroupItem outline>Proflie</ButtonGroupItem>
 	<ButtonGroupItem outline>Settings</ButtonGroupItem>
 	<ButtonGroupItem outline>Messages</ButtonGroupItem>
+	<ButtonGroupItem outline>User</ButtonGroupItem>
 </ButtonGroup>
 
 <ButtonGroup>
 	<ButtonGroupItem href="/">Proflie</ButtonGroupItem>
 	<ButtonGroupItem href="/">Settings</ButtonGroupItem>
 	<ButtonGroupItem href="/">Messages</ButtonGroupItem>
+	<ButtonGroupItem href="/">User</ButtonGroupItem>
 </ButtonGroup>
 
 <h1>Icon</h1>
@@ -29,6 +31,10 @@
 		<CloudDownload size="18" class="pr-1" />
 		Messages</ButtonGroupItem
 	>
+	<ButtonGroupItem>
+		<CloudDownload size="18" class="pr-1" />
+		User</ButtonGroupItem
+	>
 </ButtonGroup>
 
 <ButtonGroup>
@@ -41,7 +47,11 @@
 		Settings</ButtonGroupItem
 	>
 	<ButtonGroupItem outline>
-		<CloudDownload size="18" class="pr-1" />
+		<CloudDownload size="18" class=" pr-1" />
 		Messages</ButtonGroupItem
+	>
+	<ButtonGroupItem outline>
+		<CloudDownload size="18" class="pr-1" />
+		User</ButtonGroupItem
 	>
 </ButtonGroup>
