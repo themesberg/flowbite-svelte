09b7ad971a2d5ba01eca463e251f2c078a86b708
Shinichi Okada <okada.shin@gmail.com>
2022-06-17 16:11:49 +0900

    fix: update Tabel component, add TableHead, TableBody, TableHeadCell, TabelRow

---
+++ src/lib/tables/TableRow.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import classNames from 'classnames';
+	import { getContext } from 'svelte';
+</script>
+
+<tr
+	{...$$restProps}
+	class={classNames(
+		{
+			'odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700':
+				getContext('striped') === true,
+			'hover:bg-gray-50 dark:hover:bg-gray-600': getContext('hoverable') === true
+		},
+		$$props.class
+	)}
+>
+	<slot />
+</tr>
