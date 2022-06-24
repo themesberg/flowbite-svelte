1811bdd5237c89ce92b01b6985a212a4d85de658
Shinichi Okada <okada.shin@gmail.com>
2022-06-25 06:11:53 +0900

    > Gitignore updated at 2022-06-25 06:11:53

--- src/routes/tests/color.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	const colors = ['lime', 'cyan'];
-</script>
-
-<div class="max-w-3xl mx-auto pb-8 pt-16">
-	<button type="button" class="py-3 px-5 text-base font-medium text-white rounded-lg bg-gradient-to-r from-cyan-200 via-cyan-400 to-cyan-500 bg-cyan-300"> Save </button>
-</div>
--- src/routes/tests/order.svelte
+++
@@ -1,8 +0,0 @@
-<div id="main" class="flex flex-row justify-evenly">
-	<div class="bg-green-700 order-3 w-24 h-12">3 green</div>
-	<div class="bg-red-700 order-1 w-24 h-12">1 red</div>
-	<div class="bg-blue-700 order-4 w-24 h-12">4 blue</div>
-	<div class="bg-yellow-700 order-6 w-24 h-12">6 yellow</div>
-	<div class="bg-gray-700 order-5 w-24 h-12">5 gray</div>
-	<div class="bg-pink-700 order-2 w-24 h-12">2 pink</div>
-</div>
