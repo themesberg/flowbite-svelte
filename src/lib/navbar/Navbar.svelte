ffdcd9fa2cde37a13103e493905d6076f61e5df6
Shinichi Okada <okada.shin@gmail.com>
2022-06-25 16:10:54 +0900

    fix: change from const to let navDivClass

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import classNames from 'classnames';
 	export let navClass: string = 'bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800';
-	const navDivClass: string = 'mx-auto flex flex-wrap justify-between items-center ';
+	export let navDivClass: string = 'mx-auto flex flex-wrap justify-between items-center ';
 	export let fluid: boolean = true;
 	export let rounded: boolean = false;
 	export let border: boolean = false;
