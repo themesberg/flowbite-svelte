fb90509f4d0d1566de0afa95249160155361f578
Shinichi Okada <okada.shin@gmail.com>
2022-07-17 19:26:23 +0200

    feat: add fileupload multiple files and sizes

--- src/lib/forms/Fileupload.svelte
+++ src/lib/forms/Fileupload.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+	import classNames from 'classnames';
 	export let value: string = '';
-	export let id: string = 'user_avatar';
+	export let size: 'xs' | 'sm' | 'lg' = 'sm';
 	export let inputClass: string =
-		'block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none focus:border-transparent dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400';
+		'block w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400';
 </script>
 
 <input
 	{...$$restProps}
 	bind:value
-	class={inputClass}
-	aria-describedby="{id}_help"
-	{id}
+	class={classNames(inputClass, {
+		'mb-5 text-xs': size === 'xs',
+		'mb-5 text-sm': size === 'sm',
+		'text-lg': size === 'lg'
+	})}
 	type="file"
 />
