f21f98313add34879713f657f702066269da0b27
Jakub Jagielka <jjagielka@gmail.com>
2022-07-18 18:05:13 +0200

    feature: dropdowns extended examples

--- src/lib/dropdowns/DropdownHeader.svelte
+++ src/lib/dropdowns/DropdownHeader.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import classNames from 'classnames';
 	import DropdownDivider from './DropdownDivider.svelte';
-	export let divClass: string = 'block py-2 px-4 text-sm text-gray-700 dark:text-gray-200';
+	export let divClass: string = 'py-2 px-4 text-gray-700 dark:text-white';
+	// 'block py-2 px-4 text-sm text-gray-700 dark:text-gray-200';
+	export let divider: boolean = true;
 </script>
 
-<div class={classNames(divClass, $$props.class)} {...$$restProps}>
+<div {...$$restProps} class={classNames(divClass, $$props.class)}>
 	<slot />
 </div>
-<DropdownDivider />
+{#if divider}
+	<DropdownDivider />
+{/if}
--- src/lib/dropdowns/DropdownItem.svelte
+++ src/lib/dropdowns/DropdownItem.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import classNames from 'classnames';
 	export let liClass: string =
-		'block cursor-pointer py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white';
+		'block font-medium cursor-pointer py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600';
+	export let color: string = 'default';
+
+	const colors = {
+		default: 'text-gray-700 dark:text-gray-200 dark:hover:text-white',
+		red: 'text-red-600 dark:text-red-500'
+	};
 </script>
 
-<li {...$$restProps} class={classNames(liClass, $$props.class)} on:click>
+<li
+	{...$$restProps}
+	class={classNames(liClass, colors[color] ?? colors.default, $$props.class)}
+	on:click|stopPropagation
+>
 	<slot />
 </li>
