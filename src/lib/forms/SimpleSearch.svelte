1eda6880cae8eb60fc3da7835eb67aa9a6c37fd5
Juanse <juansebr98@hotmail.com>
2022-08-19 18:27:49 +0200

    Update SimpleSearch.svelte

--- src/lib/forms/SimpleSearch.svelte
+++ src/lib/forms/SimpleSearch.svelte
@@ -11,6 +11,7 @@
 	export let btnClass: string =
 		'p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800';
 	export let placeholder: string = 'Search';
+	export let value = '';
 
 	// tainted if put in component having its own background
 	let background: boolean = getContext('background');
@@ -44,6 +45,7 @@
 			on:mouseleave
 			on:mouseover
 			on:paste
+		       	bind:value
 			{...$$restProps}
 			type="text"
 			{id}
