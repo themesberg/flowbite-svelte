5c9f9d4e3cb337a1566eeee85d8e335591cab72b
Juanse <juansebr98@hotmail.com>
2022-08-19 18:28:24 +0200

    Update Search.svelte

--- src/lib/forms/Search.svelte
+++ src/lib/forms/Search.svelte
@@ -7,6 +7,7 @@
 	export let btnClass: string =
 		'text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800';
 	export let placeholder: string = 'Search';
+	export let value = '';
 </script>
 
 <form on:submit>
@@ -40,6 +41,7 @@
 			on:mouseleave
 			on:mouseover
 			on:paste
+		       	bind:value
 			type="search"
 			{id}
 			class={inputClass}
