0a247c340910830d5ab82b83a8db1bd6907358f8
Shinichi Okada <okada.shin@gmail.com>
2022-08-23 06:53:29 +0200

    docs: add links menu

--- src/routes/moduleItems/+server.js
+++ src/routes/moduleItems/+server.js
@@ -24,6 +24,7 @@ export const forms = [
 export const typography = [
 	{ href: '/typography/headings', name: 'Headings' },
 	{ href: '/typography/blockquote', name: 'Blockquote' },
+	{ href: '/typography/links', name: 'Links' },
 	{ href: '/typography/hr', name: 'HR' }
 ];
 
