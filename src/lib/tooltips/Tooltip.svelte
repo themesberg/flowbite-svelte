d9dacf9f65e85be93a3458dd5359d614a0dd12a0
Jakub Jagielka <jakub.jagielka@commscope.com>
2022-08-29 10:27:31 +0200

    feat: tooltips new API

--- src/lib/tooltips/Tooltip.svelte
+++ src/lib/tooltips/Tooltip.svelte
@@ -1,38 +1,23 @@
 <script lang="ts">
 	import Popper from '$lib/utils/Popper.svelte';
 	import classNames from 'classnames';
-	import generateId from '$lib/utils/generateId';
 
-	export let content: string = '';
+	export let color: string = '';
 	export let style: 'dark' | 'light' | 'auto' | 'custom' = 'dark';
-
-	export let tipColor: string = '';
-
 	export let tipClass: string = 'py-2 px-3 text-sm font-medium rounded-lg shadow-sm tooltip';
-
 	export let triggeredBy: string;
 
-	const tipStyleClasses = {
+	const colors = {
 		dark: 'border border-gray-800 bg-gray-900 text-white dark:bg-gray-700 dark:border-gray-600',
 		light: 'border border-gray-200 bg-white text-gray-900',
 		auto: 'border border-gray-200 bg-white text-gray-900 dark:bg-gray-700 dark:text-white dark:border-gray-600 ',
-		custom: tipColor
+		custom: color
 	};
 
 	let toolTipClass;
-	$: toolTipClass = classNames(tipClass, tipStyleClasses[style], $$props.class);
-
-	let id = generateId();
+	$: toolTipClass = classNames(tipClass, colors[style], $$props.class);
 </script>
 
-<div {id}>
+<Popper activeContent={false} {triggeredBy} {...$$restProps} class={toolTipClass} on:show>
 	<slot />
-</div>
-<Popper
-	activeContent={false}
-	triggeredBy={triggeredBy ?? '#' + id}
-	{...$$restProps}
-	class={toolTipClass}
-	on:show>
-	<slot name="content">{content}</slot>
 </Popper>
