6ce830c2657c6727835ded2b01728c8830a1a7d2
Jakub Jagielka <jakub.jagielka@commscope.com>
2022-09-05 08:46:24 +0200

    fix: 683restProps for toolbarbutton

--- src/lib/toolbar/ToolbarButton.svelte
+++ src/lib/toolbar/ToolbarButton.svelte
@@ -34,9 +34,7 @@
 		sizing[size],
 		colors[color],
 		color === 'default' &&
-			(background
-				? 'hover:bg-gray-100 dark:hover:bg-gray-600'
-				: 'hover:bg-gray-100 dark:hover:bg-gray-700'),
+			(background ? 'hover:bg-gray-100 dark:hover:bg-gray-600' : 'hover:bg-gray-100 dark:hover:bg-gray-700'),
 		$$props.class
 	);
 
@@ -47,20 +45,14 @@
 	};
 </script>
 
-<button on:click type="button" class={buttonClass} aria-label={ariaLabel ?? name}>
+<button on:click type="button" {...$$restProps} class={buttonClass} aria-label={ariaLabel ?? name}>
 	{#if name}<span class="sr-only">{name}</span>{/if}
 	<slot>
-		<svg
-			class={svgSizes[size]}
-			fill="currentColor"
-			viewBox="0 0 20 20"
-			xmlns="http://www.w3.org/2000/svg"
-		>
+		<svg class={svgSizes[size]} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 			<path
 				fill-rule="evenodd"
 				d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-				clip-rule="evenodd"
-			/>
+				clip-rule="evenodd" />
 		</svg>
 	</slot>
 </button>
