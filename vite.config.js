c22a7bd815d5ea12b9f5431dd974f528efda64be
Jakub Jagielka <jjagielka@gmail.com>
2022-09-15 16:46:05 +0200

    feat: mdsvexample second try

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
+import examples from 'mdsvexamples/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()]
+  plugins: [sveltekit(), examples],
+  resolve: {
+    alias: {
+      'flowbite-svelte': path.resolve(process.cwd(), './src/lib/index.ts')
+    }
+  }
 };
 
 export default config;
