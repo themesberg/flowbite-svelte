024b8e6a8db1e4961b1d26df67728cc0fb8b3025
Jakub Jagielka <jakub.jagielka@commscope.com>
2022-09-18 09:58:51 +0200

    feat: wrapper added

---
+++ src/routes/utils/ExampleWrapper.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import classNames from 'classnames';
+  export let divClass =
+    'rounded-xl w-full my-4 mx-auto bg-gradient-to-r bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 p-2 sm:p-6';
+
+  // the source of the example, if you want it
+  //   export let src;
+
+  // all meta tags of the code block
+  export let meta;
+
+  function hack(node: HTMLElement) {
+    node.parentElement.classList.add('w-full');
+  }
+</script>
+
+<div use:hack class={classNames(divClass, meta.class)}>
+  <slot name="example" />
+</div>
+<div class="code">
+  <pre class="language-svelte"><slot name="code" /></pre>
+</div>
