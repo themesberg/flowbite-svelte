5f46d238ff1c81c1c66c1542565aa589f97cf530
Shinichi Okada <okada.shin@gmail.com>
2022-09-22 12:46:51 +0900

    fix: remove tabId from TabWrapper component

--- src/lib/tabs/TabWrapper.svelte
+++ src/lib/tabs/TabWrapper.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import classNames from 'classnames';
-	export let divClass: string = 'w-full';
-	export let tabStyle: 'default' | 'full' | 'icon' | 'pill' | 'underline' | 'custom' = 'default';
-	let tabId: number;
-	export let activeTabValue: number;
+  import classNames from 'classnames';
+  export let divClass: string = 'w-full';
+  export let tabStyle: 'default' | 'full' | 'icon' | 'pill' | 'underline' | 'custom' = 'default';
+
+  export let activeTabValue: number;
 </script>
 
 <div class={classNames(divClass, $$props.class)}>
-	<slot {tabStyle} {tabId} {activeTabValue} />
+  <slot {tabStyle} {activeTabValue} />
 </div>
