5168c27c69420d0afa956c58545f148d8740d3de
Shinichi Okada <okada.shin@gmail.com>
2022-09-23 06:24:45 +0900

    fix: remove activeTabvalue from TabWrapper and let:tabId vrom tab page examples

--- src/lib/tabs/TabWrapper.svelte
+++ src/lib/tabs/TabWrapper.svelte
@@ -2,10 +2,8 @@
   import classNames from 'classnames';
   export let divClass: string = 'w-full';
   export let tabStyle: 'default' | 'full' | 'icon' | 'pill' | 'underline' | 'custom' = 'default';
-
-  export let activeTabValue: number;
 </script>
 
 <div class={classNames(divClass, $$props.class)}>
-  <slot {tabStyle} {activeTabValue} />
+  <slot {tabStyle} />
 </div>
