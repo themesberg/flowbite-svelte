4845f38ee58342dbe6aa47cf9a298b1c5aed6622
Jakub Jagielka <jjagielka@gmail.com>
2022-09-22 23:13:07 +0200

    modal svelte style

--- src/lib/modals/Modal.svelte
+++ src/lib/modals/Modal.svelte
@@ -18,81 +18,58 @@
     | 'bottom-center'
     | 'bottom-right' = 'center';
   export let autoclose: boolean = true;
-  export let backdropClasses: string =
-    'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40';
+  export let backdropClasses: string = 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80';
 
   const dispatch = createEventDispatcher();
 
-  const allPlacementClasses = [
-    'justify-start',
-    'justify-center',
-    'justify-end',
-    'items-start',
-    'items-center',
-    'items-end'
-  ];
+  function blockEvent(e: Event) {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  }
 
-  let backdropEl: HTMLElement | undefined;
+  function blockkeys(node: HTMLElement) {
+    function preventScroll2(e: KeyboardEvent) {
+      if (node.contains(e.target)) return true;
 
-  const init = (node: HTMLElement, _open: boolean) => {
-    getPlacementClasses().map((c) => node.classList.add(c));
-    _open && createBackdrop(node);
-
-    function preventScroll(e: Event) {
-      if (e.target === node) {
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
-      }
+      if (e.key === 'ArrowDown' || e.key === 'ArrowUp') return blockEvent(e);
     }
+
+    document.addEventListener('keydown', preventScroll2, { passive: false });
+    node.focus();
+
     return {
-      update(_open: boolean) {
-        allPlacementClasses.map((c) => node.classList.remove(c));
-        getPlacementClasses().map((c) => node.classList.add(c));
-
-        _open ? createBackdrop(node) : destroyBackdrop(node);
-        _open
-          ? node.addEventListener('wheel', preventScroll, { passive: false })
-          : node.removeEventListener('wheel', preventScroll);
-      },
-
       destroy() {
-        destroyBackdrop(node);
-        node.removeEventListener('wheel', preventScroll);
+        document.removeEventListener('keydown', preventScroll2);
       }
     };
-  };
+  }
+
+  function init(node: HTMLElement) {
+    function preventScroll(e: Event) {
+      if (e.target === node) {
+        blockEvent(e);
+        return;
+      }
+    }
+
+    node.addEventListener('wheel', preventScroll, { passive: false });
+    node.addEventListener('keydown', handleEscape, true);
+
+    dispatch('show', node);
+
+    return {
+      destroy() {
+        node.removeEventListener('wheel', preventScroll);
+        node.removeEventListener('keydown', handleEscape, true);
+
+        dispatch('hide', node);
+      }
+    };
+  }
 
   const handleEscape = (e: KeyboardEvent) => {
     if (open && e.key === 'Escape') open = false;
-  };
-
-  const createBackdrop = (node: HTMLElement) => {
-    if (!backdropEl) {
-      backdropEl = document.createElement('div');
-      backdropEl.classList.add(...backdropClasses.split(' '));
-      backdropEl.style.overscrollBehavior = 'contain';
-
-      const body = document.body;
-      body.append(backdropEl);
-      //   body.style.overflow = 'hidden';
-
-      document.addEventListener('keydown', handleEscape, true);
-    }
-
-    dispatch('show', node);
-  };
-
-  const destroyBackdrop = (node: HTMLElement) => {
-    const body = document.body;
-    // body.style.overflow = 'auto';
-
-    if (backdropEl) backdropEl.remove();
-    backdropEl = undefined;
-
-    document.removeEventListener('keydown', handleEscape, true);
-
-    dispatch('hide', node);
   };
 
   const getPlacementClasses = () => {
@@ -145,44 +122,49 @@
 </script>
 
 <!-- Main modal -->
-<div
-  tabindex="-1"
-  class="hidden overflow-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full"
-  class:flex={open}
-  class:hidden={!open}
-  aria-hidden={open ? undefined : 'true'}
-  aria-modal={open ? 'true' : undefined}
-  role={open ? 'dialog' : undefined}
-  use:init={open}
-  use:focusTrap={open}
-  on:click={onButtonsClick}>
-  <div class="relative p-4 w-full {sizes[size]} h-full md:h-auto">
-    <!-- Modal content -->
-    <Frame rounded shadow class="relative flex flex-col max-h-screen">
-      <!-- Modal header -->
-      {#if $$slots.header || title}
-        <div class="flex justify-between items-center p-4 rounded-t border-b dark:border-gray-600">
-          <slot name="header">
-            <h3 class="text-xl font-semibold text-gray-900 dark:text-white p-0">
-              {title}
-            </h3>
-          </slot>
-          <CloseButton name="Close modal" on:click={hide} />
-        </div>
-      {:else}
-        <CloseButton name="Close modal" class="absolute top-3 right-2.5" on:click={hide} />
-      {/if}
-      <!-- Modal body -->
-      <div class="p-6 space-y-6 flex-1 overflow-y-auto overscroll-contain" tabindex="0">
-        <slot />
-      </div>
-      <!-- Modal footer -->
-      {#if $$slots.footer}
+{#if open}
+  <div
+    tabindex="-1"
+    class="flex overflow-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full {backdropClasses} {getPlacementClasses().join(
+      ' '
+    )}"
+    aria-modal="true"
+    role="dialog"
+    use:init
+    use:focusTrap={open}
+    on:click={onButtonsClick}>
+    <div class="flex p-4 w-full {sizes[size]} h-full md:h-auto ">
+      <!-- Modal content -->
+      <Frame rounded shadow class="relative flex flex-col max-h-screen">
+        <!-- Modal header -->
+        {#if $$slots.header || title}
+          <div
+            class="flex justify-between items-center p-4 rounded-t border-b dark:border-gray-600">
+            <slot name="header">
+              <h3 class="text-xl font-semibold text-gray-900 dark:text-white p-0">
+                {title}
+              </h3>
+            </slot>
+            <CloseButton name="Close modal" on:click={hide} />
+          </div>
+        {:else}
+          <CloseButton name="Close modal" class="absolute top-3 right-2.5" on:click={hide} />
+        {/if}
+        <!-- Modal body -->
         <div
-          class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-          <slot name="footer" />
+          class="p-6 space-y-6 flex-1 overflow-y-auto overscroll-contain"
+          tabindex="0"
+          use:blockkeys>
+          <slot />
         </div>
-      {/if}
-    </Frame>
+        <!-- Modal footer -->
+        {#if $$slots.footer}
+          <div
+            class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+            <slot name="footer" />
+          </div>
+        {/if}
+      </Frame>
+    </div>
   </div>
-</div>
+{/if}
