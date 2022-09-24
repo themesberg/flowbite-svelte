dc5e73feaa44c3fc7f020967f2e342b08bafccdf
Shinichi Okada <okada.shin@gmail.com>
2022-09-24 09:21:33 +0900

    fix: add other dir

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,27 @@
 export const handle = async ({ event, resolve }) => {
   // vite function to get all md files from components dir
-  const allPostFiles = import.meta.glob('/src/routes/components/*.md')
+  const componentFiles = import.meta.glob('/src/routes/components/*.md')
+  const formFiles = import.meta.glob('/src/routes/forms/*.md')
+  const typographyFiles = import.meta.glob('/src/routes/typography/*.md')
+  const utilFiles = import.meta.glob('/src/routes/utils/*.md')
+  const pageFiles = import.meta.glob('/src/routes/pages/*.md')
+  const extendFiles = import.meta.glob('/src/routes/extend/*.md')
   // returns an array of files
-  const iterablePostFiles = Object.entries(allPostFiles)
+  const iterableComponentFiles = Object.entries(componentFiles)
+  const iterableFormFiles = Object.entries(formFiles)
+  const iterableTypographyFiles = Object.entries(typographyFiles)
+  const iterableUtilFiles = Object.entries(utilFiles)
+  const iterablePageFiles = Object.entries(pageFiles)
+  const iterableExtendFiles = Object.entries(extendFiles)
   // returns an array of paths, /accordion etc.
-  const allPosts = await Promise.all(
-    iterablePostFiles.map(async ([path]) => {
+  const allComponents = await Promise.all(
+    iterableComponentFiles.map(async ([path]) => {
+      const compoPath = path.slice(22, -3)
+      return compoPath
+    })
+  )
+  const allForms = await Promise.all(
+    iterableFormFiles.map(async ([path]) => {
       const postPath = path.slice(22, -3)
       return postPath
     })
