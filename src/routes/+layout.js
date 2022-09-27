a3af319fdcd0a775527d57316acf2efc0655a24d
Shinichi Okada <okada.shin@gmail.com>
2022-09-28 05:52:52 +0900

    fix: move /utils/+server.js to /api/+server.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,5 @@
 export const load = async ({ fetch }) => {
-  const response = await fetch('/utils');
+  const response = await fetch('/api');
 
   return await response.json()
 };
--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,4 +1,4 @@
-import { fetchMarkdownPosts } from './'
+import { fetchMarkdownPosts } from '../utils'
 import { json } from '@sveltejs/kit'
 
 export const GET = async () => {
