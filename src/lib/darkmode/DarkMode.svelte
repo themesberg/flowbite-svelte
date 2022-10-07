ea82a2bc22085bad26967a82d078d8ab86b2477d
Shinichi Okada <okada.shin@gmail.com>
2022-10-07 14:45:29 +0900

    fix: remove // @ts-ignore

--- src/lib/darkmode/DarkMode.svelte
+++ src/lib/darkmode/DarkMode.svelte
@@ -32,6 +32,5 @@
     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
       <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
     </svg>
-    // @ts-ignore
   </span>
 </button>
