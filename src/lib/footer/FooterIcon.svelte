b272599420091c9f39a8ce6e29284fd1949a609e
Shinichi Okada <okada.shin@gmail.com>
2022-10-31 06:37:43 +0900

    feat: add target and {...$$restProps} to all footer components

--- src/lib/footer/FooterIcon.svelte
+++ src/lib/footer/FooterIcon.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import classNames from 'classnames';
-	export let href: string = '';
-	export let ariaLabel: string = '';
-	export let aClass: string = 'text-gray-500 hover:text-gray-900 dark:hover:text-white';
+  import classNames from 'classnames';
+  export let href: string = '';
+  export let ariaLabel: string = '';
+  export let aClass: string = 'text-gray-500 hover:text-gray-900 dark:hover:text-white';
+  export let target: string | undefined = undefined;
 </script>
 
 {#if href}
-	<a {href} aria-label={ariaLabel} class={classNames(aClass, $$props.class)}>
-		<slot />
-	</a>
+  <a {...$$restProps} {href} {target} aria-label={ariaLabel} class={classNames(aClass, $$props.class)}>
+    <slot />
+  </a>
 {:else}
-	<slot />
+  <slot />
 {/if}
--- src/lib/footer/FooterLink.svelte
+++ src/lib/footer/FooterLink.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import classNames from 'classnames';
-	export let liClass: string = 'mr-4 last:mr-0 md:mr-6';
-	export let aClass: string = 'hover:underline';
-	export let href: string = '';
+  import A from '$lib/typography/A.svelte';
+  import classNames from 'classnames';
+  export let liClass: string = 'mr-4 last:mr-0 md:mr-6';
+  export let aClass: string = 'hover:underline';
+  export let href: string = '';
+  export let target: string | undefined = undefined;
 </script>
 
 <li class={classNames(liClass, $$props.class)}>
-	<a {href} class={aClass}>
-		<slot />
-	</a>
+  <a {...$$restProps} {href} class={aClass} {target}>
+    <slot />
+  </a>
 </li>
