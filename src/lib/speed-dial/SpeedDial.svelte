8b211da16188b626c9212df2c17149be3c8695a9
Jakub Jagielka <jjagielka@gmail.com>
2022-11-17 21:51:43 +0100

    feat: SpeedDial

--- src/lib/speed-dial/SpeedDial.svelte
+++ src/lib/speed-dial/SpeedDial.svelte
@@ -1,17 +1,27 @@
+<script context="module" lang="ts">
+  import type { Placement } from '@popperjs/core';
+
+  export interface SpeedCtxType {
+    pill: boolean;
+    tooltip: Placement | 'none';
+    textOutside: boolean;
+  }
+</script>
+
 <script lang="ts">
   import classNames from 'classnames';
   import Button from '$lib/buttons/Button.svelte';
   import Popper from '$lib/utils/Popper.svelte';
-  import type { Placement } from '@popperjs/core';
   import { setContext } from 'svelte';
 
   export let defaultClass: string = 'fixed right-6 bottom-6';
   export let placement: Placement = 'top';
-  export let rounded: boolean = false;
-  export let tooltip: Placement = 'left';
+  export let pill: boolean = true;
+  export let tooltip: Placement | 'none' = 'left';
   export let trigger: 'hover' | 'click' = 'hover';
+  export let textOutside: boolean = false;
 
-  setContext('speed-dial', { rounded, placement: tooltip });
+  setContext<SpeedCtxType>('speed-dial', { pill, tooltip, textOutside });
 
   let divClass: string;
   $: divClass = classNames(defaultClass, 'group', $$props.class);
@@ -25,24 +35,26 @@
 
 <div class={divClass}>
   <Button
-    pill={!rounded}
+    {pill}
     name="Open actions menu"
     aria-controls="speed-dial-menu-default"
     aria-expanded="false"
     color="blue"
     class="!p-3">
-    <svg
-      aria-hidden="true"
-      class="w-8 h-8 transition-transform group-hover:rotate-45"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
+    <slot name="icon">
+      <svg
+        aria-hidden="true"
+        class="w-8 h-8 transition-transform group-hover:rotate-45"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>
+    </slot>
     <span class="sr-only">Open actions menu</span>
   </Button>
   <Popper {trigger} arrow={false} color="none" activeContent {placement} class={poperClass}>
--- src/lib/speed-dial/SpeedDialButton.svelte
+++ src/lib/speed-dial/SpeedDialButton.svelte
@@ -4,31 +4,42 @@
   import type { Placement } from '@popperjs/core';
   import classNames from 'classnames';
   import { getContext } from 'svelte';
+  import type { SpeedCtxType } from './SpeedDial.svelte';
 
   export let name: string = '';
-  export let placement: Placement | 'none';
-  export let rounded: boolean | undefined = undefined;
+  export let tooltip: Placement | 'none';
+  export let pill: boolean | undefined = undefined;
+  export let textOutside: boolean | undefined = undefined;
 
-  $: rounded = rounded ?? getContext('speed-dial')?.rounded;
-  $: placement = placement ?? getContext('speed-dial')?.placement;
+  const context = getContext<SpeedCtxType>('speed-dial');
+
+  $: {
+    // set defaults
+    pill = pill ?? context?.pill ?? true;
+    tooltip = tooltip ?? context?.tooltip ?? 'left';
+    textOutside = textOutside ?? context?.textOutside ?? false;
+  }
 
   let btnClass: string;
   $: btnClass = classNames(
     'w-[52px] h-[52px] shadow-sm !p-2',
-    placement === 'none' && 'flex-col',
+    tooltip === 'none' && 'flex-col',
+    textOutside && 'relative',
     $$props.class
   );
 </script>
 
-<Button pill={!rounded} outline color="light" {...$$restProps} class={btnClass}>
+<Button {pill} outline color="light" {...$$restProps} class={btnClass} on:click>
   <slot />
-  {#if placement === 'none'}
-    <span class="block mb-px text-xs font-medium">{name}</span>
-  {:else}
+  {#if tooltip !== 'none'}
     <span class="sr-only">{name}</span>
+  {:else if textOutside}
+    <span class="block absolute -left-14 top-1/2 mb-px text-sm font-medium -translate-y-1/2">{name}</span>
+  {:else}
+    <span class="block mb-px text-xs font-medium">{name}</span>
   {/if}
 </Button>
 
-{#if placement !== 'none'}
-  <Tooltip {placement}>{name}</Tooltip>
+{#if tooltip !== 'none'}
+  <Tooltip placement={tooltip} style="dark">{name}</Tooltip>
 {/if}
