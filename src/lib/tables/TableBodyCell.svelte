cbc16095990624ab953633976ced27087cf2d327
Zhi Nie <ryanniezhi@163.com>
2022-11-25 18:56:37 +0800

    feat: make tr and td clickable

--- src/lib/tables/TableBodyCell.svelte
+++ src/lib/tables/TableBodyCell.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	import classNames from 'classnames';
-	import { getContext } from 'svelte';
+  import classNames from 'classnames';
+  import { getContext } from 'svelte';
 
-	export let tdClass: string = 'px-6 py-4 whitespace-nowrap font-medium ';
+  export let tdClass: string = 'px-6 py-4 whitespace-nowrap font-medium ';
 
-	let color = 'default';
-	color = getContext('color');
-	let tdClassfinal;
-	$: tdClassfinal = classNames(
-		tdClass,
-		color === 'default'
-			? 'text-gray-900 dark:text-white'
-			: 'text-blue-50 whitespace-nowrap dark:text-blue-100',
-		$$props.class
-	);
+  let color = 'default';
+  color = getContext('color');
+  let tdClassfinal: string;
+  $: tdClassfinal = classNames(
+    tdClass,
+    color === 'default'
+      ? 'text-gray-900 dark:text-white'
+      : 'text-blue-50 whitespace-nowrap dark:text-blue-100',
+    $$props.class
+  );
 </script>
 
-<td {...$$restProps} class={tdClassfinal}>
-	<slot />
+<td {...$$restProps} class={tdClassfinal} on:click>
+  <slot />
 </td>
--- src/lib/tables/TableHead.svelte
+++ src/lib/tables/TableHead.svelte
@@ -1,49 +1,49 @@
 <script lang="ts">
-	import classNames from 'classnames';
-	import { getContext } from 'svelte';
+  import classNames from 'classnames';
+  import { getContext } from 'svelte';
 
-	export let theadClass: string = 'text-xs uppercase';
+  export let theadClass: string = 'text-xs uppercase';
 
-	let color: string;
-	color = getContext('color');
-	let noborder: boolean = getContext('noborder');
-	let striped: boolean = getContext('striped');
-	let defatultBgColor = noborder || striped ? '' : 'bg-gray-50 dark:bg-gray-700';
-	const bgColors = {
-		default: defatultBgColor,
-		blue: 'bg-blue-600',
-		green: 'bg-green-600',
-		red: 'bg-red-600',
-		yellow: 'bg-yellow-600',
-		purple: 'bg-purple-600',
-		custom: ''
-	};
+  let color: 'blue' | 'green' | 'red' | 'yellow' | 'purple' | 'default' | 'custom';
+  color = getContext('color');
+  let noborder: boolean = getContext('noborder');
+  let striped: boolean = getContext('striped');
+  let defatultBgColor = noborder || striped ? '' : 'bg-gray-50 dark:bg-gray-700';
+  const bgColors = {
+    default: defatultBgColor,
+    blue: 'bg-blue-600',
+    green: 'bg-green-600',
+    red: 'bg-red-600',
+    yellow: 'bg-yellow-600',
+    purple: 'bg-purple-600',
+    custom: ''
+  };
 
-	let textColor =
-		color === 'default'
-			? 'text-gray-700 dark:text-gray-400'
-			: color === 'custom'
-			? ''
-			: 'text-white  dark:text-white';
-	let borderColors = striped
-		? ''
-		: color === 'default'
-		? 'border-gray-700'
-		: color === 'custom'
-		? ''
-		: `border-${color}-400`;
+  let textColor =
+    color === 'default'
+      ? 'text-gray-700 dark:text-gray-400'
+      : color === 'custom'
+      ? ''
+      : 'text-white  dark:text-white';
+  let borderColors = striped
+    ? ''
+    : color === 'default'
+    ? 'border-gray-700'
+    : color === 'custom'
+    ? ''
+    : `border-${color}-400`;
 
-	$: theadClassfinal = classNames(
-		theadClass,
-		textColor,
-		striped && borderColors,
-		bgColors[color],
-		$$props.class
-	);
+  $: theadClassfinal = classNames(
+    theadClass,
+    textColor,
+    striped && borderColors,
+    bgColors[color],
+    $$props.class
+  );
 </script>
 
 <thead {...$$restProps} class={theadClassfinal}>
-	<tr>
-		<slot />
-	</tr>
+  <tr>
+    <slot />
+  </tr>
 </thead>
