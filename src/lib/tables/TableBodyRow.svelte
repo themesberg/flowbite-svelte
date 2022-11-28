3bc7f9e227e3bfe70b0c184eae132abafd071760
Zhi Nie <ryanniezhi@163.com>
2022-11-29 07:43:17 +0800

    feat: make tr and td clickable (#452)

--- src/lib/tables/TableBodyRow.svelte
+++ src/lib/tables/TableBodyRow.svelte
@@ -1,49 +1,49 @@
 <script lang="ts">
-	import classNames from 'classnames';
-	import { getContext } from 'svelte';
+  import classNames from 'classnames';
+  import { getContext } from 'svelte';
 
-	export let trClass: string = 'bg-white';
-	const colors = {
-		default: 'dark:border-gray-700 dark:bg-gray-800',
-		blue: 'bg-blue-500 border-b border-blue-400',
-		green: 'bg-green-500 border-b border-green-400',
-		red: 'bg-red-500 border-b border-red-400',
-		yellow: 'bg-yellow-500 border-b border-yellow-400',
-		purple: 'bg-purple-500 border-b border-purple-400',
-		custom: ''
-	};
-	const hoverColors = {
-		default: 'hover:bg-gray-50 dark:hover:bg-gray-600',
-		blue: 'hover:bg-blue-400',
-		green: 'hover:bg-green-400',
-		red: 'hover:bg-red-400',
-		yellow: 'hover:bg-yellow-400',
-		purple: 'hover:bg-purple-400',
-		custom: ''
-	};
+  export let trClass: string = 'bg-white';
+  const colors = {
+    default: 'dark:border-gray-700 dark:bg-gray-800',
+    blue: 'bg-blue-500 border-b border-blue-400',
+    green: 'bg-green-500 border-b border-green-400',
+    red: 'bg-red-500 border-b border-red-400',
+    yellow: 'bg-yellow-500 border-b border-yellow-400',
+    purple: 'bg-purple-500 border-b border-purple-400',
+    custom: ''
+  };
+  const hoverColors = {
+    default: 'hover:bg-gray-50 dark:hover:bg-gray-600',
+    blue: 'hover:bg-blue-400',
+    green: 'hover:bg-green-400',
+    red: 'hover:bg-red-400',
+    yellow: 'hover:bg-yellow-400',
+    purple: 'hover:bg-purple-400',
+    custom: ''
+  };
 
-	const stripColors = {
-		default: 'odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700',
-		blue: 'odd:bg-blue-800 even:bg-blue-700 odd:dark:bg-blue-800 even:dark:bg-blue-700',
-		green: 'odd:bg-green-800 even:bg-green-700 odd:dark:bg-green-800 even:dark:bg-green-700',
-		red: 'odd:bg-red-800 even:bg-red-700 odd:dark:bg-red-800 even:dark:bg-red-700',
-		yellow: 'odd:bg-yellow-800 even:bg-yellow-700 odd:dark:bg-yellow-800 even:dark:bg-yellow-700',
-		purple: 'odd:bg-purple-800 even:bg-purple-700 odd:dark:bg-purple-800 even:dark:bg-purple-700',
-		custom: ''
-	};
+  const stripColors = {
+    default: 'odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700',
+    blue: 'odd:bg-blue-800 even:bg-blue-700 odd:dark:bg-blue-800 even:dark:bg-blue-700',
+    green: 'odd:bg-green-800 even:bg-green-700 odd:dark:bg-green-800 even:dark:bg-green-700',
+    red: 'odd:bg-red-800 even:bg-red-700 odd:dark:bg-red-800 even:dark:bg-red-700',
+    yellow: 'odd:bg-yellow-800 even:bg-yellow-700 odd:dark:bg-yellow-800 even:dark:bg-yellow-700',
+    purple: 'odd:bg-purple-800 even:bg-purple-700 odd:dark:bg-purple-800 even:dark:bg-purple-700',
+    custom: ''
+  };
 
-	let color = 'default';
-	color = getContext('color');
-	let trClassfinal;
-	$: trClassfinal = classNames(
-		getContext('noborder') ? 'bg-white dark:bg-gray-800' : trClass,
-		colors[color],
-		getContext('hoverable') && hoverColors[color],
-		getContext('striped') && stripColors[color],
-		$$props.class
-	);
+  let color: 'blue' | 'green' | 'red' | 'yellow' | 'purple' | 'default' | 'custom' = 'default';
+  color = getContext('color');
+  let trClassfinal: string;
+  $: trClassfinal = classNames(
+    getContext('noborder') ? 'bg-white dark:bg-gray-800' : trClass,
+    colors[color],
+    getContext('hoverable') && hoverColors[color],
+    getContext('striped') && stripColors[color],
+    $$props.class
+  );
 </script>
 
-<tr {...$$restProps} class={trClassfinal}>
-	<slot />
+<tr {...$$restProps} class={trClassfinal} on:click>
+  <slot />
 </tr>
