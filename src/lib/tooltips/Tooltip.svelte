852a10b90ad78b2a0cc277787c3bc5e9ac67c09d
Jakub Jagielka <jjagielka@gmail.com>
2022-11-29 12:07:10 +0100

    remove border

--- src/lib/tooltips/Tooltip.svelte
+++ src/lib/tooltips/Tooltip.svelte
@@ -2,13 +2,13 @@
   import Popper from '../utils/Popper.svelte';
   import classNames from 'classnames';
 
-  export let style: 'dark' | 'light' | 'auto' | 'custom' = 'auto';
+  export let style: 'dark' | 'light' | 'auto' | 'custom' = 'dark';
   export let defaultClass: string = 'py-2 px-3 text-sm font-medium';
 
   const styles = {
-    dark: 'border-gray-800 bg-gray-900 text-white dark:bg-gray-700 dark:border-gray-600',
-    light: 'border-gray-200 bg-white text-gray-900',
-    auto: 'border-gray-200 bg-white text-gray-900 dark:bg-gray-700 dark:text-white dark:border-gray-600',
+    dark: 'bg-gray-900 text-white dark:bg-gray-700',
+    light: 'border border-gray-200 bg-white text-gray-900',
+    auto: ' bg-white text-gray-900 dark:bg-gray-700 dark:text-white border border-gray-200 dark:border-0',
     custom: ''
   };
 
@@ -20,6 +20,6 @@
   }
 </script>
 
-<Popper data-tooltip rounded border shadow {...$$restProps} class={toolTipClass} on:show>
+<Popper data-tooltip rounded shadow {...$$restProps} class={toolTipClass} on:show>
   <slot />
 </Popper>
