6e4d9d158c27ebb13f9a964d4bbfea17bba068cc
Oliver Weber <ow@uix.io>
2022-12-07 12:40:30 +0100

    fix: add eventhandler to label (mouse, drag, etc.) (#433)

--- src/lib/forms/Dropzone.svelte
+++ src/lib/forms/Dropzone.svelte
@@ -5,21 +5,30 @@
   export let files: FileList | undefined = undefined;
   export let defaultClass: string =
     'flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600';
+
+  let input: HTMLInputElement;
+
+  function keydown(ev: KeyboardEvent) {
+    if ([' ', 'Enter'].includes(ev.key)) {
+      ev.preventDefault();
+      input.click();
+    }
+  }
 </script>
 
-<label class={classNames(defaultClass, $$props.class)}>
+<label
+  class={classNames(defaultClass, $$props.class)}
+  tabIndex="0"
+  on:keydown={keydown}
+  on:focus
+  on:blur
+  on:mouseenter
+  on:mouseleave
+  on:mouseover
+  on:dragenter
+  on:dragleave
+  on:dragover
+  on:drop>
   <slot />
-  <input
-    {...$$restProps}
-    bind:value
-    bind:files
-    type="file"
-    class="hidden"
-    on:click
-    on:change
-    on:focus
-    on:blur
-    on:mouseenter
-    on:mouseleave
-    on:mouseover />
+  <input {...$$restProps} bind:value bind:files bind:this={input} type="file" class="hidden" on:change on:click />
 </label>
