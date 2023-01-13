16c66504d42a378705a120fae5a6ca147a776bd4
Shinichi Okada <okada.shin@gmail.com>
2023-01-14 06:47:00 +0900

    fix: #530

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -5,7 +5,6 @@ const config = {
   plugins: [
     //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
     tailwindcss(), //But others, like autoprefixer, need to run after,
-    autoprefixer,
     autoprefixer
   ]
 };
