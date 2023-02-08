fc7bc9cfab4fe7e59a03cf8b2ccc1c7b73a67aae
jjagielka <jjagielka@gmail.com>
2023-02-08 10:27:37 +0100

    Reversed `fluid` flag

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -30,7 +30,7 @@
 </script>
 
 <Frame tag="nav" {color} {...$$restProps} class={classNames(navClass, $$props.class)}>
-  <div class={classNames(navDivClass, fluid && 'container')}>
+  <div class={classNames(navDivClass, fluid || 'container')}>
     <slot {hidden} {toggle} />
   </div>
 </Frame>
