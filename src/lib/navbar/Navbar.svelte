4311bbba0bd590d66b388f6c0f8e688b1bd85a1c
jjagielka <jjagielka@gmail.com>
2023-02-08 11:30:49 +0100

    fix: fluid setting for Navbar (#563)
    * Reversed `fluid` flag
    
    * No `fluid` by default

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -4,7 +4,7 @@
 
   export let navClass: string = 'px-2 sm:px-4 py-2.5 w-full';
   export let navDivClass: string = 'mx-auto flex flex-wrap justify-between items-center ';
-  export let fluid: boolean = true;
+  export let fluid: boolean = false;
   export let color:
     | 'gray'
     | 'red'
@@ -30,7 +30,7 @@
 </script>
 
 <Frame tag="nav" {color} {...$$restProps} class={classNames(navClass, $$props.class)}>
-  <div class={classNames(navDivClass, fluid && 'container')}>
+  <div class={classNames(navDivClass, fluid || 'container')}>
     <slot {hidden} {toggle} />
   </div>
 </Frame>
