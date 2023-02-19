f080e3d1c02c45ce532feb79a4fb7ae3e57ad691
Shinichi Okada <okada.shin@gmail.com>
2023-02-19 14:34:44 +0900

    fix: update playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	testDir: './tests/',
 	webServer: {
 		command: 'npm run build && npm run preview',
 		timeout: 240000,
