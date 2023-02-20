a7978dd854ded3238aa522b5ee47ebc2f0865cf9
Shinichi Okada <okada.shin@gmail.com>
2023-02-20 20:02:54 +0900

    fix: update from index.ts to index.js in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ const config = {
 	},
 	resolve: {
 		alias: {
-			'flowbite-svelte': path.resolve(process.cwd(), './src/lib/index.ts')
+			'flowbite-svelte': path.resolve(process.cwd(), './src/lib/index.js')
 		}
 	}
 };
