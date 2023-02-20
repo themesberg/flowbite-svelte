0ec3274033ff17dc0ee003641d72044133ba5061
Shinichi Okada <okada.shin@gmail.com>
2023-02-20 12:02:46 +0900

    fix: update package-json-writer.js

--- scripts/package-json-writer.js
+++ scripts/package-json-writer.js
@@ -1,3 +1,14 @@
+/* This script reads the package.json file, makes any necessary adjustments 
+to the JSON object, and writes the updated package.json file to a 
+specified output directory. 
+The output directory defaults to "./dist", but can be changed by updating the
+"outputDirectory" variable. 
+If any errors occur during this process, the script will log an error message to the console.
+
+Add the following to package.json scripts section
+"write-package-json": "node ./scripts/package-json-writer.js",
+*/
+
 import fs from 'fs';
 import path from 'path';
 
