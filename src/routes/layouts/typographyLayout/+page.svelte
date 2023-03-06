0ed5ebc1b3bf94749f833ee5a3aa14e0802b89bd
Shinichi Okada <okada.shin@gmail.com>
2023-03-07 06:58:40 +0900

    fix: add carbon (#602)

--- src/routes/layouts/typographyLayout/+page.svelte
+++ src/routes/layouts/typographyLayout/+page.svelte
@@ -89,6 +89,10 @@
   </div>
 </div>
 
+<aside class="fixed z-50 hidden right-5 bottom-5 sm:block">
+  <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CEAIC53I&placement=flowbite-sveltecom" id="_carbonads_js"></script>
+</aside>
+
 <style>
   .link {
     color: #f83c00;
