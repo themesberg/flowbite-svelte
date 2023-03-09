d390fdc1147ee56d3c80b5c087dfb2421f871c45
Shinichi Okada <okada.shin@gmail.com>
2023-03-10 08:29:14 +0900

    fix: snapshot page

--- src/routes/examples/snapshot/+page.svelte
+++ src/routes/examples/snapshot/+page.svelte
@@ -9,19 +9,16 @@
 </script>
 
 <script lang="ts">
-  import { MetaTag } from '../utils'
-  import type { Snapshot } from "./$types"; 
-  import { Input, Label, Button, Checkbox, A, Breadcrumb, BreadcrumbItem, Heading, P } from '$lib'
+  import { MetaTag } from '../../utils'
+  import type { Snapshot } from "../$types"; 
+  import { Input, Label, A, Breadcrumb, BreadcrumbItem, Heading, P } from '$lib'
   const dir ='Examples'
   const breadcrumb_title = 'Svelte Snapshot'
   const title = 'Svelte Snapshot'
 
   let formData = {
       first_name:'',
-      last_name: '',
-      company: '',
-      website:'',
-      email: '',
+      last_name: ''
     }
   export const snapshot: Snapshot = { 
     capture: () => formData, 
@@ -52,28 +49,5 @@
       <Label for="last_name" class="mb-2">Last name</Label>
       <Input type="text" id="last_name" placeholder="Doe" required bind:value={formData.last_name} />
     </div>
-    <div>
-      <Label for="company" class="mb-2">Company</Label>
-      <Input type="text" id="company" placeholder="Flowbite" required bind:value={formData.company} />
-    </div>
-    <div>
-      <Label for="website" class="mb-2">Website URL</Label>
-      <Input type="url" id="website" placeholder="flowbite.com" bind:value={formData.website} />
-    </div>
-  </div>
-  <div class="mb-6">
-    <Label for="email" class="mb-2">Email address</Label>
-    <Input type="email" id="email" placeholder="john.doe@company.com" required bind:value={formData.email} />
-  </div>
-  <div class="mb-6">
-    <Label for="password" class="mb-2">Password</Label>
-    <Input type="password" id="password" placeholder="•••••••••" />
-  </div>
-  <div class="mb-6">
-    <Label for="confirm_password" class="mb-2">Confirm password</Label>
-    <Input type="password" id="confirm_password" placeholder="•••••••••" />
-  </div>
-  <Checkbox class="mb-6 space-x-1" required>I agree with the <A href="/">terms and conditions</A>.</Checkbox>
-  <Button type="submit">Submit</Button>
 </form>
 
---
+++ src/routes/examples/testsnap.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { Input, Label, Button, Checkbox, A, Breadcrumb, BreadcrumbItem, Heading, P } from '$lib'
+  let formData = {
+      first_name:'',
+      last_name: '',
+      company: '',
+      website:'',
+      email: '',
+    }
+  export const snapshot = { 
+    capture: () => formData, 
+    restore: (value) => (formData = value)
+  };
+</script>
+
+<form class="p-16">
+  <div class="grid gap-6 mb-6 md:grid-cols-2">
+    <div>
+      <Label for="first_name" class="mb-2">First name</Label>
+      <Input type="text" id="first_name" placeholder="John" required bind:value={formData.first_name} />
+    </div>
+    <div>
+      <Label for="last_name" class="mb-2">Last name</Label>
+      <Input type="text" id="last_name" placeholder="Doe" required bind:value={formData.last_name} />
+    </div>
+    <div>
+      <Label for="company" class="mb-2">Company</Label>
+      <Input type="text" id="company" placeholder="Flowbite" required bind:value={formData.company} />
+    </div>
+    <div>
+      <Label for="website" class="mb-2">Website URL</Label>
+      <Input type="url" id="website" placeholder="flowbite.com" bind:value={formData.website} />
+    </div>
+  </div>
+  <div class="mb-6">
+    <Label for="email" class="mb-2">Email address</Label>
+    <Input type="email" id="email" placeholder="john.doe@company.com" required bind:value={formData.email} />
+  </div>
+  <div class="mb-6">
+    <Label for="password" class="mb-2">Password</Label>
+    <Input type="password" id="password" placeholder="•••••••••" />
+  </div>
+  <div class="mb-6">
+    <Label for="confirm_password" class="mb-2">Confirm password</Label>
+    <Input type="password" id="confirm_password" placeholder="•••••••••" />
+  </div>
+  <Checkbox class="mb-6 space-x-1" required>I agree with the <A href="/">terms and conditions</A>.</Checkbox>
+  <Button type="submit">Submit</Button>
+</form>
