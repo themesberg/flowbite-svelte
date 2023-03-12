7c3d62d434fb6c0a760ea8dce074a90e481dee16
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-03-12 11:13:01 +0200

    feat: add new custom max width class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,9 @@ const config = {
 	darkMode: 'class',
 	theme: {
 		extend: {
+			maxWidth: {
+                '8xl': '90rem',
+            },
 			colors: {
 				// rose
 				primary: { "50": "#fff1f2", "100": "#ffe4e6", "200": "#fecdd3", "300": "#fda4af", "400": "#fb7185", "500": "#f43f5e", "600": "#e11d48", "700": "#be123c", "800": "#9f1239", "900": "#881337" }
