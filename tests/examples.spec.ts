fcb5089dd6cbc8f6562f4721361b823ab342dc21
Jakub Jagielka <jjagielka@gmail.com>
2023-04-14 17:21:54 +0200

    fix: tests corrections

--- tests/examples.spec.ts
+++ tests/examples.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('Snapshot page should have h1', async ({ page }) => {
   await page.goto('/docs/examples/snapshot');
-  expect(await page.textContent('h1')).toBe('Svelte Snapshot');
+  expect(await page.textContent('h1')).toBe('Svelte Snapshot - Flowbite');
 });
 
 test('Responsive Sidebar Layout page should have h1', async ({ page }) => {
