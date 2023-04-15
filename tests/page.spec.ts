da1fbde04bfbc0e9e6345947c5f4374f12d59b55
Jakub <jjagielka@gmail.com>
2023-04-15 18:13:59 +0200

    fix: test corrected

--- tests/page.spec.ts
+++ tests/page.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('index page should have h1', async ({ page }) => {
   await page.goto('/');
-  expect(await page.textContent('h1 > span')).toBe('Flowbite Svelte');
+  expect(await page.textContent('h1 > span')).toBe('Speed up your web development with');
 });
 
 test('About page should have h1', async ({ page }) => {
