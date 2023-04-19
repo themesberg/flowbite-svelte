083c5eb51d5452e708d34afdec131eeab24706b7
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-04-19 19:26:50 +0300

    test(pages): match new page titles

--- tests/page.spec.ts
+++ tests/page.spec.ts
@@ -7,12 +7,12 @@ test('index page should have h1', async ({ page }) => {
 
 test('About page should have h1', async ({ page }) => {
   await page.goto('/docs/pages/introduction');
-  expect(await page.textContent('h1')).toBe('Flowbite Svelte Component Library');
+  expect(await page.textContent('h1')).toBe('Flowbite Svelte - UI Component Library');
 });
 
 test('Getting Started page should have h1', async ({ page }) => {
   await page.goto('/docs/pages/getting-started');
-  expect(await page.textContent('h1')).toBe('Getting Started - Flowbite Svelte');
+  expect(await page.textContent('h1')).toBe('Quickstart - Flowbite Svelte');
 });
 
 test('How to contribute page should have h1', async ({ page }) => {
@@ -22,15 +22,15 @@ test('How to contribute page should have h1', async ({ page }) => {
 
 test('License page should have h1', async ({ page }) => {
   await page.goto('/docs/pages/license');
-  expect(await page.textContent('h1')).toBe('License');
+  expect(await page.textContent('h1')).toBe('License - Flowbite Svelte');
 });
 
 test('Type list page should have h1', async ({ page }) => {
   await page.goto('/docs/pages/types');
-  expect(await page.textContent('h1')).toBe('Types');
+  expect(await page.textContent('h1')).toBe('Svelte TypeScript Types - Flowbite');
 });
 
 test('Faster Compiling Speed page should have h1', async ({ page }) => {
-  await page.goto('/docs/pages/faster-compiling-speed');
-  expect(await page.textContent('h1')).toBe('Faster Compiling Speed');
+  await page.goto('/docs/pages/compiler-speed');
+  expect(await page.textContent('h1')).toBe('Svelte Compiler Speed - Flowbite');
 });
