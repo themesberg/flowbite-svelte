51f5f2df8080157816d921745eb828a04ee6183e
Jakub Jagielka <jjagielka@gmail.com>
2023-04-19 14:04:56 +0200

    footer extension ready

--- src/routes/utils/Footer.svelte
+++ src/routes/utils/Footer.svelte
@@ -1,4 +1,5 @@
-<script>
+<script lang="ts">
+  import { page } from '$app/stores';
   import Footer from '$lib/footer/Footer.svelte';
   import FooterBrand from '$lib/footer/FooterBrand.svelte';
   import FooterCopyright from '$lib/footer/FooterCopyright.svelte';
@@ -6,11 +7,31 @@
   import FooterLink from '$lib/footer/FooterLink.svelte';
   import FooterLinkGroup from '$lib/footer/FooterLinkGroup.svelte';
   let logo = '/images/flowbite-svelte-icon-logo.svg';
+
+  let isHomePage: boolean;
+  $: isHomePage = $page.route.id === '/';
+
+  const footer_links = {
+    Resources: {
+      GitHub: 'https://github.com/themesberg/flowbite-svelte',
+      Flowbite: 'https://flowbite.com/',
+      'Tailwind CSS': 'https://tailwindcss.com/',
+      'Figma Design': 'https://flowbite.com/figma/'
+    },
+    'Help and Support': {
+      'Discord Community': 'https://discord.gg/4eeurUVvTy',
+      'GitHub Discussions': 'https://github.com/themesberg/flowbite-svelte/discussions'
+    },
+    Legal: {
+      License: 'https://github.com/themesberg/flowbite-svelte/blob/main/LICENSE',
+      'Brand usage': 'https://flowbite.com/brand/'
+    }
+  };
 </script>
 
 <Footer footerType="custom" customClass="bg-white dark:bg-gray-900">
   <div class="flex flex-col py-6">
-    <div class="flex flex-col lg:flex-row items-start md:justify-between gap-8">
+    <div class="flex flex-col lg:flex-row items-start md:justify-between gap-8 {isHomePage ? '' : ''}">
       <div class="mb-6 md:mb-0 w-full max-w-sm">
         <FooterBrand
           href="https://flowbite-svelte.com"
@@ -23,44 +44,28 @@
           CSS that can help you build websites faster.
         </p>
         <p class="max-w-sm mb-3 text-gray-600 dark:text-gray-400">
-          Code licensed <a
+          Code licensed
+          <a
             href="https://github.com/themesberg/flowbite-svelte/blob/main/LICENSE"
-            class="text-primary-600 hover:underline">MIT</a
-          >, docs
+            class="text-primary-600 hover:underline">
+            MIT
+          </a>, docs
           <a href="https://creativecommons.org/licenses/by/3.0/" class="text-primary-600 hover:underline"
             >CC BY 3.0</a
           >.
         </p>
       </div>
       <div class="flex flex-col sm:flex-row gap-8 w-full justify-between items-start">
-        <div>
-          <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-          <FooterLinkGroup>
-            <FooterLink liClass="mb-4" href="https://github.com/themesberg/flowbite-svelte"
-              >GitHub</FooterLink>
-            <FooterLink liClass="mb-4" href="https://flowbite.com/">Flowbite</FooterLink>
-            <FooterLink liClass="mb-4" href="https://tailwindcss.com/">Tailwind CSS</FooterLink>
-            <FooterLink liClass="mb-4" href="https://flowbite.com/figma/">Figma Design</FooterLink>
-          </FooterLinkGroup>
-        </div>
-        <div>
-          <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-            Help and Support
-          </h2>
-          <FooterLinkGroup>
-            <FooterLink liClass="mb-4" href="https://discord.gg/4eeurUVvTy">Discord Community</FooterLink>
-            <FooterLink liClass="mb-4" href="https://github.com/themesberg/flowbite-svelte/discussions"
-              >GitHub Discussions</FooterLink>
-          </FooterLinkGroup>
-        </div>
-        <div>
-          <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Legal</h2>
-          <FooterLinkGroup>
-            <FooterLink liClass="mb-4" href="https://github.com/themesberg/flowbite-svelte/blob/main/LICENSE"
-              >License</FooterLink>
-            <FooterLink liClass="mb-4" href="https://flowbite.com/brand/">Brand usage</FooterLink>
-          </FooterLinkGroup>
-        </div>
+        {#each Object.entries(footer_links) as [column, links]}
+          <div>
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">{column}</h2>
+            <FooterLinkGroup>
+              {#each Object.entries(links) as [name, href]}
+                <FooterLink liClass="mb-4" {href}>{name}</FooterLink>
+              {/each}
+            </FooterLinkGroup>
+          </div>
+        {/each}
       </div>
     </div>
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
