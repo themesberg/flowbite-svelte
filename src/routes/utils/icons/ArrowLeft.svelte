af84aaf359c8095e17d7b1cc277e762b8c1efcf2
Jakub Jagielka <jjagielka@gmail.com>
2023-04-21 11:45:49 +0200

    feat: prev/next buttons

---
+++ src/routes/utils/icons/ArrowLeft.svelte
@@ -0,0 +1,8 @@
+<svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M5.66667 10.6666L1 5.99992M1 5.99992L5.66667 1.33325M1 5.99992H13"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round" />
+</svg>
