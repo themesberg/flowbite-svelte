62b2b0aeaf3f81fe2c1be4b92330a33986449ecf
Jakub <jjagielka@gmail.com>
2023-04-24 23:18:56 +0200

    wip

--- src/routes/landing/SocialProof.svelte
+++ src/routes/landing/SocialProof.svelte
@@ -16,70 +16,72 @@
   }
 </script>
 
-<section
-  class="overflow-hidden mx-auto max-w-8xl flex flex-col pt-6 sm:pt-8 lg:pt-16 pb-6 sm:pb-8 lg:pb-24 px-4 lg:px-20">
-  <div class="flex flex-col sm:flex-row gap-20">
-    <div class="border border-green-300 flex flex-col items-start justify-center gap-3 md:gap-5 w-full">
-      <div class="flex flex-col items-start justify-center gap-6 w-full">
-        <div class="flex flex-col items-start py-2">
-          <Quote />
-        </div>
-        <div class="flex flex-col items-start justify-center gap-5 max-w-2xl pr-8 self-stretch">
-          <p class="leading-relaxed text-lg font-medium text-gray-900 dark:text-white">
-            Flowbite provides a robust set of design tokens and components based on the popular Tailwind CSS
-            framework. From the most used UI components like forms and navigation bars to the whole app
-            screens designed both for desktop and mobile, this UI kit provides a solid foundation for any
-            project.
-          </p>
-          <p class="leading-relaxed text-lg font-medium text-gray-900 dark:text-white">
-            Designing with Figma components that can be easily translated to the utility classes of Tailwind
-            CSS is a huge timesaver!
-          </p>
-        </div>
-        <div class="flex flex-row items-center gap-3.5 self-stretch">
-          <Avatar size="xs" />
-          <div class="flex items-center gap-3">
-            <span class="leading-tight font-semibold text-gray-900 dark:text-white">Micheal Gough</span>
-            <span class="leading-tight font-semibold text-gray-900 dark:text-white">/</span>
-            <span class="leading-tight font-normal text-sm">CEO at Google</span>
+<section class="overflow-hidden flex flex-col">
+  <div
+    class="border max-w-8xl mx-auto flex flex-col pt-6 sm:pt-8 lg:pt-16 pb-6 sm:pb-8 lg:pb-24 px-4 lg:px-20">
+    <div class="flex flex-col md:flex-row gap-4 md:gap-20 mx-auto max-w-8xl">
+      <div class="flex flex-col items-start justify-center gap-3 md:gap-5 w-full">
+        <div class="flex flex-col items-start justify-center gap-6 w-full">
+          <div class="flex flex-col items-start py-2">
+            <Quote />
+          </div>
+          <div class="flex flex-col items-start justify-center gap-5 max-w-2xl pr-8 self-stretch">
+            <p class="leading-relaxed text-lg font-medium text-gray-900 dark:text-white">
+              Flowbite provides a robust set of design tokens and components based on the popular Tailwind
+              CSS framework. From the most used UI components like forms and navigation bars to the whole app
+              screens designed both for desktop and mobile, this UI kit provides a solid foundation for any
+              project.
+            </p>
+            <p class="leading-relaxed text-lg font-medium text-gray-900 dark:text-white">
+              Designing with Figma components that can be easily translated to the utility classes of
+              Tailwind CSS is a huge timesaver!
+            </p>
+          </div>
+          <div class="flex flex-row items-center gap-3.5 self-stretch">
+            <Avatar size="xs" />
+            <div class="flex items-center gap-3">
+              <span class="leading-tight font-semibold text-gray-900 dark:text-white">Micheal Gough</span>
+              <span class="leading-tight font-semibold text-gray-900 dark:text-white">/</span>
+              <span class="leading-tight font-normal text-sm">CEO at Google</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="border border-red-300 flex flex-col items-start gap-6 md:gap-3 w-full">
-      <div class="flex flex-row items-start lg:justify-end gap-2 md:gap-12 lg:gap-2 self-stretch">
-        <div
-          class="flex flex-col items-start gap-4 md:gap-6 text-gray-400 w-full max-w-[272px] lg:py-6 lg:px-8">
-          <GitHub />
-          <div class="flex flex-col items-start gap-2">
-            <p class="leading-tight text-2xl font-bold text-gray-900 dark:text-white">{format(github)}</p>
-            <p class="text-sm md:text-base text-gray-500">Stars on Github</p>
+      <div class="border flex flex-col items-start gap-6 md:gap-3 w-full">
+        <div class="flex flex-row items-start lg:justify-end gap-2 md:gap-12 lg:gap-2 self-stretch">
+          <div
+            class="flex flex-col items-start gap-4 md:gap-6 text-gray-400 w-full max-w-[272px] lg:py-6 lg:px-8">
+            <GitHub />
+            <div class="flex flex-col items-start gap-2">
+              <p class="leading-tight text-2xl font-bold text-gray-900 dark:text-white">{format(github)}</p>
+              <p class="text-sm md:text-base text-gray-500">Stars on Github</p>
+            </div>
+          </div>
+          <div
+            class="flex flex-col items-start gap-4 md:gap-6 text-gray-400 w-full max-w-[272px] lg:py-6 lg:px-8">
+            <Npm />
+            <div class="flex flex-col items-start gap-2">
+              <p class="leading-tight text-2xl font-bold text-gray-900 dark:text-white">{format(npm)}</p>
+              <p class="text-sm md:text-base text-gray-500">Downloads on NPM</p>
+            </div>
           </div>
         </div>
-        <div
-          class="flex flex-col items-start gap-4 md:gap-6 text-gray-400 w-full max-w-[272px] lg:py-6 lg:px-8">
-          <Npm />
-          <div class="flex flex-col items-start gap-2">
-            <p class="leading-tight text-2xl font-bold text-gray-900 dark:text-white">{format(npm)}</p>
-            <p class="text-sm md:text-base text-gray-500">Downloads on NPM</p>
+        <div class="flex flex-row items-start lg:justify-end gap-2 md:gap-12 lg:gap-2 self-stretch">
+          <div
+            class="flex flex-col items-start gap-4 md:gap-6 text-gray-400 w-full max-w-[272px] lg:py-6 lg:px-8">
+            <Figma />
+            <div class="flex flex-col items-start gap-2">
+              <p class="leading-tight text-2xl font-bold text-gray-900 dark:text-white">{format(figma)}</p>
+              <p class="text-sm md:text-base text-gray-500">Figma duplicates</p>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="flex flex-row items-start lg:justify-end gap-2 md:gap-12 lg:gap-2 self-stretch">
-        <div
-          class="flex flex-col items-start gap-4 md:gap-6 text-gray-400 w-full max-w-[272px] lg:py-6 lg:px-8">
-          <Figma />
-          <div class="flex flex-col items-start gap-2">
-            <p class="leading-tight text-2xl font-bold text-gray-900 dark:text-white">{format(figma)}</p>
-            <p class="text-sm md:text-base text-gray-500">Figma duplicates</p>
-          </div>
-        </div>
-        <div
-          class="flex flex-col items-start gap-4 md:gap-6 text-gray-400 w-full max-w-[272px] lg:py-6 lg:px-8">
-          <Discord />
-          <div class="flex flex-col items-start gap-2">
-            <p class="leading-tight text-2xl font-bold text-gray-900 dark:text-white">{format(discord)}</p>
-            <p class="text-sm md:text-base text-gray-500">Discord members online</p>
+          <div
+            class="flex flex-col items-start gap-4 md:gap-6 text-gray-400 w-full max-w-[272px] lg:py-6 lg:px-8">
+            <Discord />
+            <div class="flex flex-col items-start gap-2">
+              <p class="leading-tight text-2xl font-bold text-gray-900 dark:text-white">{format(discord)}</p>
+              <p class="text-sm md:text-base text-gray-500">Discord members online</p>
+            </div>
           </div>
         </div>
       </div>
