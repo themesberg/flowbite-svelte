ee2bc97646531c1b2bcccb5e97848dd7f18c7dc2
Jakub <jjagielka@gmail.com>
2023-04-29 06:53:49 +0200

    fix: CTA should have tinted background

--- src/routes/landing/utils/A.svelte
+++ src/routes/landing/utils/A.svelte
@@ -1,5 +1,5 @@
-<script>
-  export let href;
+<script lang="ts">
+  export let href: string;
 </script>
 
 <a {href} class="underline text-lg font-medium hover:no-underline text-gray-900 dark:text-white"><slot /></a>
