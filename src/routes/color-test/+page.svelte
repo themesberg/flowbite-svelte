0fef95f4806e351ba1d9be77d9e1d60c805d4a8d
Jakub <jjagielka@gmail.com>
2023-04-30 10:52:23 +0200

    fix: missing route

---
+++ src/routes/color-test/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Alert from '$lib/alerts/Alert.svelte';
+</script>
+
+<div class="flex mx-auto pt-24 gap-8">
+  <Alert color="info" border>Info color</Alert>
+  <Alert color="info1" border>Info color</Alert>
+</div>
