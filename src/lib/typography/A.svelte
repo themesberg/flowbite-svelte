85b56bd46f96e0e75058dc04b3770b7b56175f9f
Shinichi Okada <okada.shin@gmail.com>
2023-05-28 16:24:47 +0900

    feat: documenting component upto Span

--- src/lib/typography/A.svelte
+++ src/lib/typography/A.svelte
@@ -9,3 +9,37 @@
 <a {...$$restProps} {href} class={classNames(aClass, color, $$props.class)}>
   <slot />
 </a>
+
+
+<!--
+  @component
+  ## Features
+  [Go to Link](https://flowbite-svelte.com/docs/typography/link)
+  - Setup
+  - Default link
+  - Button
+  - Paragraph
+  - Icon link
+  - CTA link
+  - Card link
+  - Image link
+  ## Props
+  @prop italic: boolean = false;
+  @prop underline: boolean = false;
+  @prop linethrough: boolean = false;
+  @prop uppercase: boolean = false;
+  @prop gradient: boolean = false;
+  @prop highlight: boolean = false;
+  @prop highlightClass: string = 'text-blue-600 dark:text-blue-500';
+  @prop decorationClass: string = 'decoration-2 decoration-blue-400 dark:decoration-blue-600';
+  @prop gradientClass: string =
+    'text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400';
+  ## Example
+  ```
+  <script>
+    import { A } from 'flowbite-svelte';
+  </script>
+
+  <A class="font-medium hover:underline">Read more</A>
+  ```
+-->
--- src/lib/typography/List.svelte
+++ src/lib/typography/List.svelte
@@ -26,3 +26,41 @@
 <svelte:element this={tag} {...$$restProps} class={classList}>
   <slot />
 </svelte:element>
+
+<!--
+  @component
+  ## Features
+  [Go to List](https://flowbite-svelte.com/docs/typography/list)
+  - Setup
+  - Unordored list
+  - Icons
+  - Nested
+  - Unstyled
+  - Ordered list
+  - Nested
+  - Description List
+  - List with icon
+  - Advanced layout
+  - Horizontal List
+  ## Props
+  @prop tag: 'ul' | 'ol' | 'dl' = 'ul';
+  @prop list: 'disc' | 'none' | 'decimal' | undefined = undefined;
+  @prop position: 'inside' | 'outside' = 'inside';
+  ## Example
+  ```
+  <script>
+    import { Li, List, Heading } from 'flowbite-svelte';
+  </script>
+  
+  <Heading
+    tag="h2"
+    customSize="text-lg font-semibold"
+    class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Password requirements</Heading>
+  <List tag="ul" class="space-y-1">
+    <Li>At least 10 characters (and up to 100 characters)</Li>
+    <Li>At least one lowercase character</Li>
+    <Li>Inclusion of at least one special character, e.g., ! @ # ?</Li>
+  </List>
+  ```
+-->
+
