f00c46867da4897972f60ae5ae358061a044506f
Shinichi Okada <okada.shin@gmail.com>
2023-05-29 08:22:35 +0900

    feat: add primary to Toggle

--- src/lib/forms/Radio.svelte
+++ src/lib/forms/Radio.svelte
@@ -40,7 +40,7 @@
   import type { FormColorType } from '../types';
   import Label from './Label.svelte';
 
-  export let color: FormColorType = 'blue';
+  export let color: FormColorType = 'primary';
   export let custom: boolean = false;
   export let inline: boolean = false;
   export let group: number | string = '';
--- src/lib/list-group/Listgroup.svelte
+++ src/lib/list-group/Listgroup.svelte
@@ -7,11 +7,11 @@
 
   // propagate props type from underlying Frame
   interface $$Props extends ComponentProps<Frame> {
-    items?: ListGroupItemType[]|string[];
+    items?: ListGroupItemType[] | string[];
     active?: boolean;
   }
 
-  export let items: ListGroupItemType[]|string[]  = [];
+  export let items: ListGroupItemType[] | string[] = [];
   export let active: boolean = false;
   export let defaultClass: string = 'divide-y divide-gray-200 dark:divide-gray-600';
 
@@ -21,7 +21,6 @@
   $: groupClass = classNames(defaultClass, $$props.class);
 </script>
 
-
 <Frame tag={active ? 'div' : 'ul'} {...$$restProps} rounded border class={groupClass}>
   {#each items as item, index}
     {#if typeof item === 'object'}
@@ -30,7 +29,7 @@
       <ListgroupItem {active} {index} on:click><slot {item} {index} /></ListgroupItem>
     {/if}
   {:else}
-    <slot item={items[0]}/>
+    <slot item={items[0]} />
   {/each}
 </Frame>
 
