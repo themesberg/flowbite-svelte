9ef819b24e2188c39d40d84ea60778ea42815a8a
Shinichi Okada <okada.shin@gmail.com>
2023-06-13 15:05:04 +0900

    feat: update docs and remove classnames from package.json

--- starters/flowbite-svelte-starter/src/routes/+page.svelte
+++
@@ -1,4 +0,0 @@
-<h1 class="text-3xl dark:text-white">Welcome to SvelteKit</h1>
-<p class="dark:text-white">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
