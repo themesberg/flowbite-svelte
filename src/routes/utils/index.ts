6e0bb4dd942ee766e3056a50a69b0a787e902389
Shinichi Okada <okada.shin@gmail.com>
2023-08-23 21:36:47 +0200

    fix: move charts to plugins dir

--- src/routes/utils/index.ts
+++ src/routes/utils/index.ts
@@ -26,6 +26,7 @@ export const fetchMarkdownPosts = async () => {
   const extendFiles = import.meta.glob<Mdsvex>('/src/routes/docs/extend/*.md');
   const exampleFiles = import.meta.glob<Mdsvex>('/src/routes/docs/examples/*.md');
   const experimentalFiles = import.meta.glob<Mdsvex>('/src/routes/docs/experimental/*.md');
+  const pluginsFiles = import.meta.glob<Mdsvex>('/src/routes/docs/plugins/*.md');
   // returns an array of files
   const iterableComponentFiles = Object.entries(componentFiles);
   const iterableFormFiles = Object.entries(formFiles);
@@ -35,6 +36,7 @@ export const fetchMarkdownPosts = async () => {
   const iterableExtendFiles = Object.entries(extendFiles);
   const iterableExampleFiles = Object.entries(exampleFiles);
   const iterableExperimentalFiles = Object.entries(experimentalFiles);
+  const iterablePluginsFiles = Object.entries(pluginsFiles);
 
   const allComponents = await Promise.all(
     iterableComponentFiles.map(async ([path, resolver]) => {
@@ -68,6 +70,16 @@ export const fetchMarkdownPosts = async () => {
   // returns an array of paths, /closebutton from /src/routes/utilities/closebutton.md
   const allUtils = await Promise.all(
     iterableUtilFiles.map(async ([path, resolver]) => {
+      const { metadata } = await resolver();
+      return {
+        meta: metadata,
+        path: filePath(path)
+      };
+    })
+  );
+
+  const allPlugins = await Promise.all(
+    iterablePluginsFiles.map(async ([path, resolver]) => {
       const { metadata } = await resolver();
       return {
         meta: metadata,
@@ -128,6 +140,7 @@ export const fetchMarkdownPosts = async () => {
     examples: allExamples,
     extend: allExtends,
     utilities: allUtils,
+    plugins: allPlugins,
     experimental: allExperimental
   };
 };
