e140d324682d636d5d7eba6b1e4075397323a390
Shinichi Okada <okada.shin@gmail.com>
2023-08-25 21:06:55 +0200

    fix: ads position since it does not show unless you refres the browser (#1019)

--- src/routes/layouts/component/+page.svelte
+++ src/routes/layouts/component/+page.svelte
@@ -41,6 +41,4 @@
   <Toc {extract} headingSelector="#mainContent > :where(h2, h3)" />
 </div>
 
-<aside class="fixed z-50 hidden right-5 bottom-5 sm:block">
-  <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CEAIC53I&placement=flowbite-sveltecom" id="_carbonads_js"></script>
-</aside>
+
