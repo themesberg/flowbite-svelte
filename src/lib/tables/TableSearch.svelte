a974201e465ca8791ad554798644f87beaaa5a45
Shinichi Okada <okada.shin@gmail.com>
2023-09-04 20:01:40 +0200

    fix: TableSearch pl-10 posision

--- src/lib/tables/TableSearch.svelte
+++ src/lib/tables/TableSearch.svelte
@@ -13,7 +13,7 @@
   export let searchClass: string = 'relative mt-1';
   export let svgDivClass: string = 'absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none';
   export let svgClass: string = 'w-5 h-5 text-gray-500 dark:text-gray-400';
-  export let inputClass: string = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500';
+  export let inputClass: string = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 pl-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500';
 
   let inputCls = twMerge(inputClass, $$props.classInput);
   let svgDivCls = twMerge(svgDivClass, $$props.classSvgDiv);
