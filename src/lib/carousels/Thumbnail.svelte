59da5edbaea85bd1e570349768f2de9d14292166
jjagielka <jjagielka@gmail.com>
2023-09-06 09:00:56 +0200

    feat: new API for Carousel (#1032)
    * feat: new API for Carousel
    
    * fix: run lib-helpers
    
    * fix: icon used in advanced customization
    
    * merge with main

--- src/lib/carousels/Thumbnail.svelte
+++ src/lib/carousels/Thumbnail.svelte
@@ -1,35 +1,20 @@
 <script lang="ts">
-  export let thumbImg: string = '';
-  export let altTag: string = '';
-  export let titleLink: string = '';
-  export let id: number;
-  export let thumbWidth: number = 100;
+  import type { HTMLImgAttributes } from 'svelte/elements';
+  import { twMerge } from 'tailwind-merge';
+
   export let selected: boolean = false;
-  export let thumbClass: string = '';
-  export let thumbBtnClass: string = '';
+
+  export let activeClass = 'opacity-100';
+  export let inactiveClass = 'opacity-60';
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<button on:click aria-label="Click to view image" class={thumbBtnClass}>
-  <img class={thumbClass} class:active={selected} id={id.toString()} src={thumbImg} alt={altTag} title="Image from {titleLink}" width="{thumbWidth}%" />
-</button>
-
-<style>
-  .active {
-    opacity: 1;
-  }
-</style>
+<img alt="..." {...$$restProps} class={twMerge(selected ? activeClass : inactiveClass, $$props.class)} />
 
 <!--
 @component
 [Go to docs](https://flowbite-svelte.com/)
 ## Props
-@prop export let thumbImg: string = '';
-@prop export let altTag: string = '';
-@prop export let titleLink: string = '';
-@prop export let id: number;
-@prop export let thumbWidth: number = 100;
 @prop export let selected: boolean = false;
-@prop export let thumbClass: string = '';
-@prop export let thumbBtnClass: string = '';
+@prop export let activeClass = 'opacity-100';
+@prop export let inactiveClass = 'opacity-60';
 -->
