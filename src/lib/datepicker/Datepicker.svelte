756ff3f32b3fbf25255ad0946157186ba9faf416
Darwin Darakananda <darwindarak@gmail.com>
2023-09-07 15:32:37 +0700

    fix: add check for empty attributes (#1041)
    The `Datepicker` component has a `attributes` prop that can be used to set toggle-type attributes, causing issues when the attribute is an empty string (which it is by default).  This fix adds a check to filter out empty attributes.

--- src/lib/datepicker/Datepicker.svelte
+++ src/lib/datepicker/Datepicker.svelte
@@ -12,7 +12,9 @@
   export let inputClass: string = 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500';
 
   $: setAttribute = (node: HTMLElement, params: any) => {
-    node.setAttribute(params, '');
+    if (params) {
+      node.setAttribute(params, '');
+    }
   };
 </script>
 
