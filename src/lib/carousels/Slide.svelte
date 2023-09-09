a804f71e4227eb2eed4802ad9667e16652880bd1
jjagielka <jjagielka@gmail.com>
2023-09-09 14:09:50 +0200

    fix: display on iPads (#1052)
    * fix: display on iPads
    
    * fix: class/restProps for Slide

--- src/lib/carousels/Slide.svelte
+++ src/lib/carousels/Slide.svelte
@@ -2,15 +2,19 @@
   import { quintOut } from 'svelte/easing';
   import type { HTMLImgAttributes } from 'svelte/elements';
   import { fade, type TransitionConfig } from 'svelte/transition';
+  import { twMerge } from 'tailwind-merge';
 
   type TransitionFunc = (node: HTMLElement, params: any) => TransitionConfig;
 
   export let image: HTMLImgAttributes;
   export let transition: TransitionFunc = (x) => fade(x, { duration: 700, easing: quintOut });
+
+  let imgClass: string;
+  $: imgClass = twMerge('absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover', $$props.class);
 </script>
 
 {#key image}
-  <img alt="..." {...image} transition:transition={{}} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover" />
+  <img alt="..." {...image} transition:transition={{}} {...$$restProps} class={imgClass} />
 {/key}
 
 <!--
