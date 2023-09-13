9f707a8f707e7869a02b1ec7946363050664a87c
Jakub <jjagielka@gmail.com>
2023-09-13 21:11:16 +0200

    fix: default size back to md

--- src/lib/avatar/Avatar.svelte
+++ src/lib/avatar/Avatar.svelte
@@ -10,7 +10,7 @@
   export let stacked: boolean = false;
   export let dot: object | undefined = undefined;
   export let alt: string = '';
-  export let size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'xl';
+  export let size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'md';
 
   $: dot = dot && { placement: 'top-right', color: 'gray', size: 'lg', ...dot };
 
