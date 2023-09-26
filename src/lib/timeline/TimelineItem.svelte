1ac1eb1b7c754987abc49f2cdeff4ee74573c074
jjagielka <jjagielka@gmail.com>
2023-09-26 08:34:18 +0200

    Timelineitem removal (#1089)
    * fix: removal if item vertical/horizontal
    
    * fix: TimelineHorizontal removal, TimelineItem refactor
    
    * fix: removal of test file
    
    * fix: clean up of packages.json

--- src/lib/timeline/TimelineItem.svelte
+++ src/lib/timeline/TimelineItem.svelte
@@ -38,72 +38,30 @@
 </script>
 
 <li class={liCls}>
-  {#if order === 'default'}
-    <div class={divCls} />
-    {#if date}
-      <time class={timeCls}>{date}</time>
-    {/if}
-    {#if title}
-      <h3 class={h3Cls}>
-        {title}
-      </h3>
-    {/if}
-    <slot />
-  {:else if order === 'vertical'}
-    <div class={divCls} />
-    {#if $$slots.icon}
-      <slot name="icon" />
-    {:else}
+  <div class={divCls} />
+  {#if order !== 'default'}
+    <slot name="icon">
       <svg aria-hidden="true" class={svgClass} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
       </svg>
-    {/if}
-    {#if title}
-      <h3 class={h3Cls}>
-        {title}
-      </h3>
-    {/if}
-    {#if date}
-      <time class={timeCls}>{date}</time>
-    {/if}
-    <slot />
-  {:else if order === 'horizontal'}
-    <div class={divCls} />
-    {#if $$slots.icon}
-      <slot name="icon" />
-    {:else}
-      <svg aria-hidden="true" class={svgClass} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-      </svg>
-    {/if}
-    {#if title}
-      <h3 class={h3Cls}>
-        {title}
-      </h3>
-    {/if}
-    {#if date}
-      <time class={timeCls}>{date}</time>
-    {/if}
-    <slot />
-  {:else}
-    <div class={divCls} />
-    {#if $$slots.icon}
-      <slot name="icon" />
-    {:else}
-      <svg aria-hidden="true" class={svgClass} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
-      </svg>
-    {/if}
-    {#if title}
-      <h3 class={h3Cls}>
-        {title}
-      </h3>
-    {/if}
-    {#if date}
-      <time class={timeCls}>{date}</time>
-    {/if}
-    <slot />
+    </slot>
+  {:else if date}
+    <time class={timeCls}>{date}</time>
   {/if}
+
+  {#if title}
+    <h3 class={h3Cls}>
+      {title}
+    </h3>
+  {/if}
+
+  {#if order !== 'default'}
+    {#if date}
+      <time class={timeCls}>{date}</time>
+    {/if}
+  {/if}
+
+  <slot />
 </li>
 
 <!--
