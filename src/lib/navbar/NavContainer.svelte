35a8f39f5be46135529cfe4798ceee0c24eb545b
Jakub <jjagielka@gmail.com>
2023-09-30 15:36:37 +0200

    navbar simplifications

---
+++ src/lib/navbar/NavContainer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements';
+  import { twMerge } from 'tailwind-merge';
+
+  interface $$Props extends HTMLAttributes<HTMLDivElement> {
+    fluid?: boolean;
+  }
+
+  export let fluid: boolean = false;
+</script>
+
+<div class={twMerge('mx-auto flex flex-wrap justify-between items-center ', fluid ? 'w-full' : 'container', $$props.class)}>
+  <slot />
+</div>
