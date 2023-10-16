743e6453d84bfa5c563889cbe873e7863b1c6760
sandro gianola <sandro.gianola@4gifters.com>
2023-10-16 22:55:59 +0200

    fix: footer component

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { twMerge } from 'tailwind-merge';
 
-  export let footerType: 'sitemap' | 'default' | 'logo' | 'socialmedia' | '' = 'default';
+  export let footerType: 'sitemap' | 'default' | 'logo' | 'socialmedia' | undefined = undefined;
 
   let footerClass = twMerge(footerType === 'sitemap' && 'bg-gray-800', footerType === 'socialmedia' && 'p-4 bg-white sm:p-6 dark:bg-gray-800', footerType === 'logo' && 'p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800', footerType === 'default' && 'p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800', $$props.class);
 </script>
@@ -14,5 +14,5 @@
 @component
 [Go to docs](https://flowbite-svelte.com/)
 ## Props
-@prop export let footerType: 'sitemap' | 'default' | 'logo' | 'socialmedia' = 'default';
+@prop export let footerType: 'sitemap' | 'default' | 'logo' | 'socialmedia' | undefined = undefined;
 -->
