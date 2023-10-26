e5991f89b67494689f5f1c555049adb4e18d4c8f
Jakub Jagielka <jjagielka@gmail.com>
2023-10-26 11:56:49 +0200

    fix: modal divide-y instead of border

--- src/lib/modal/Modal.svelte
+++ src/lib/modal/Modal.svelte
@@ -101,7 +101,7 @@
   };
 
   let frameClass: string;
-  $: frameClass = twMerge(defaultClass, 'w-full', $$props.class);
+  $: frameClass = twMerge(defaultClass, 'w-full divide-y', $$props.class);
 
   const isScrollable = (e: HTMLElement): boolean[] => [e.scrollWidth > e.clientWidth && ['scroll', 'auto'].indexOf(getComputedStyle(e).overflowX) >= 0, e.scrollHeight > e.clientHeight && ['scroll', 'auto'].indexOf(getComputedStyle(e).overflowY) >= 0];
 
@@ -123,7 +123,7 @@
       <Frame rounded shadow {...$$restProps} class={frameClass}>
         <!-- Modal header -->
         {#if $$slots.header || title}
-          <Frame color={$$restProps.color} class="flex justify-between items-center p-4 rounded-t border-b">
+          <Frame color={$$restProps.color} class="flex justify-between items-center p-4 rounded-t">
             <slot name="header">
               <h3 class="text-xl font-semibold {$$restProps.color ? '' : 'text-gray-900 dark:text-white'} p-0">
                 {title}
@@ -140,7 +140,7 @@
         </div>
         <!-- Modal footer -->
         {#if $$slots.footer}
-          <Frame color={$$restProps.color} class="flex items-center p-6 space-x-2 rounded-b border-t">
+          <Frame color={$$restProps.color} class="flex items-center p-6 space-x-2 rounded-b">
             <slot name="footer" />
           </Frame>
         {/if}
