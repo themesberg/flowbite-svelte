fe4b4d524393c5635a998582966a3d3b0fc4038f
Jakub Jagielka <jjagielka@gmail.com>
2023-12-03 18:07:42 +0100

    fix: chevrons for breadcrumbs

--- src/lib/breadcrumb/BreadcrumbItem.svelte
+++ src/lib/breadcrumb/BreadcrumbItem.svelte
@@ -23,7 +23,7 @@
     {#if $$slots.icon}
       <slot name="icon" />
     {:else}
-      <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <svg class="w-6 h-6 text-gray-400 rtl:rotate-180" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
       </svg>
     {/if}
