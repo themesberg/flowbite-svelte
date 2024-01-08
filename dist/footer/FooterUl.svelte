ea2604d021e5f781de8319631b3fa496be0f8a4a
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-08 08:32:55 +0100

    > Gitignore updated at 2024-01-08 08:32:55

--- dist/footer/FooterUl.svelte
+++
@@ -1,7 +0,0 @@
-<script>import { twMerge } from "tailwind-merge";
-let { ulClass, children, ...attributes } = $props();
-</script>
-
-<ul {...attributes} class={twMerge('text-gray-600 dark:text-gray-400', ulClass)}>
-  {@render children()}
-</ul>
