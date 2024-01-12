1aabdcc55017efb0133bd9773653bca74914b1a9
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-12 06:55:35 +0100

    docs: update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Svelte 5 UI Lib: Work in progress</h1>
 
 <p>
-    Welcome to Svelte 5 UI Lib - a UI library project crafted for personal use. While you're welcome to utilize it, please note that it was designed with a limited set of components, and I regret that I am unable to provide any support. However, your suggestions are always welcome!
+    Welcome to Svelte 5 UI Lib - an experimental UI library project crafted for personal use with Svelte 5. The design is based on Flowbite. While you're welcome to utilize it, please note that it was designed with a limited set of components, and I regret that I am unable to provide any support. However, your suggestions are always welcome!
 </p>
 
 <h2>Dependencies</h2>
