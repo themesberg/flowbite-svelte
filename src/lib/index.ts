73c76dd334be5ba6a5697e6446bc7b504a8b6405
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-14 14:31:08 +0100

    chore: organize index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,37 +1,37 @@
 export { uiHelpers, clickOutside } from './uiHelpers.svelte';
 
+// breadcrumb
 export { default as Breadcrumb } from './breadcrumb/Breadcrumb.svelte'
 export { default as BreadcrumbItem } from './breadcrumb/BreadcrumbItem.svelte'
-
+// button
 export { default as Button } from './buttons/Button.svelte';
 export { default as GradientButton } from './buttons/GradientButton.svelte';
-
+// card
 export { default as Card } from './cards/Card.svelte';
-
+// darkmode
 export { default as Darkmode } from './darkmode/Darkmode.svelte'
-
-export { default as Navbar } from './nav/Navbar.svelte';
-export { default as NavUl } from './nav/NavUl.svelte';
-export { default as NavBrand } from './nav/NavBrand.svelte';
-export { default as NavLi } from './nav/NavLi.svelte';
-
+// dropdown
+export { default as Dropdown } from './dropdown/Dropdown.svelte';
+export { default as DropdownDivider } from './dropdown/DropdownDivider.svelte';
+export { default as DropdownItem } from './dropdown/DropdownItem.svelte';
+// footer
 export { default as Footer } from './footer/Footer.svelte';
 export { default as FooterBrand } from './footer/FooterBrand.svelte';
 export { default as FooterCopyright } from './footer/FooterCopyright.svelte';
 export { default as FooterLi } from './footer/FooterLi.svelte';
 export { default as FooterUl } from './footer/FooterUl.svelte';
-
-export { default as Dropdown } from './dropdown/Dropdown.svelte';
-export { default as DropdownDivider } from './dropdown/DropdownDivider.svelte';
-export { default as DropdownItem } from './dropdown/DropdownItem.svelte';
-
+// navbar
+export { default as Navbar } from './nav/Navbar.svelte';
+export { default as NavUl } from './nav/NavUl.svelte';
+export { default as NavBrand } from './nav/NavBrand.svelte';
+export { default as NavLi } from './nav/NavLi.svelte';
+// sidebar
 export { default as Sidebar } from './sidebar/Sidebar.svelte';
 export { default as SidebarGroup } from './sidebar/SidebarGroup.svelte';
 export { default as SidebarItem } from './sidebar/SidebarItem.svelte';
 export { default as SidebarDropdownWrapper } from './sidebar/SidebarDropdownWrapper.svelte';
 export { default as SidebarDropdownItem } from './sidebar/SidebarDropdownItem.svelte';
-
-
+// skeleton
 export { default as CardPlaceholder } from './skeleton/CardPlaceholder.svelte'
 export { default as Skeleton } from './skeleton/Skeleton.svelte'
 export { default as ImagePlaceholder } from './skeleton/ImagePlaceholder.svelte'
