fc63588d1f65d00da1d8f07937766de5e6b1d575
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-24 15:26:47 +0100

    style: sidebarmenu gb and footer border top

--- src/routes/components/Footer.svelte
+++ src/routes/components/Footer.svelte
@@ -2,7 +2,7 @@
 	import { Footer, FooterBrand, FooterLi, FooterUl } from '$lib';
 </script>
 
-<Footer footerclass="shadow-none dark:bg-gray-950 border-t border-gray-700" footerType="logo">
+<Footer footerclass="shadow-none dark:bg-gray-950 rounded-none border-t border-gray-200 dark:border-gray-600" footerType="logo">
 	<div class="sm:flex sm:items-center sm:justify-between">
 		<FooterBrand href="/" name="Svelt 5 UI Lib" />
 		<FooterUl
