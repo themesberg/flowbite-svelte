722c357f5e0e9c2403ce4c52709c5fc8fa5f13e2
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-03 15:17:47 +0100

    fix: add openMainMenu to control menu button for a small screen

--- src/lib/nav/Navbar.svelte
+++ src/lib/nav/Navbar.svelte
@@ -9,6 +9,7 @@
     children?: any;
     toggleNav?: () => void;
     closeNav?: () => void;
+    openMainMenu?: boolean;
     navStatus?: boolean;
     fluid?: boolean;
     brand?: any;
@@ -25,6 +26,7 @@
     children,
     toggleNav,
     closeNav,
+    openMainMenu = true,
     navStatus,
     fluid,
     brand,
@@ -83,12 +85,14 @@
     {#if brand}
       {@render brand()}
     {/if}
+    {#if openMainMenu}
     <button onclick={toggleNav} type="button" class={btnCls} aria-controls="navbar-default">
       <span class="sr-only">Open main menu</span>
       <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
       </svg>
     </button>
+    {/if}
     <div class="{navDisplay} {divChildrenCls}" transition:slide={slideParams} role="button" tabindex="0">
       {#if children}
       {@render children()}
