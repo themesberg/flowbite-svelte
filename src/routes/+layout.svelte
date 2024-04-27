a15568b901affaedbafed25c6fbf4864d3d1ba5e
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-27 18:46:00 +0200

    fix: link update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -66,7 +66,7 @@
       <NavLi class="lg:px-2 lg:mb-0" href="/docs/components/accordion">Components</NavLi>
       <NavLi class="lg:px-2 lg:mb-0" href="https://flowbite-svelte-icons.codewithshin.com/">Icons</NavLi>
       <NavLi class="lg:px-2 lg:mb-0" href="https://flowbite-svelte-blocks.codewithshin.com/">Blocks</NavLi>
-      <NavLi class="lg:px-2 lg:mb-0" target="_blank" href="https://flowbite-svelte-admin-dashboard.codewithshin.com/">Dashboard</NavLi>
+      <NavLi class="lg:px-2 lg:mb-0" target="_blank" href="https://flowbite-svelte-admin-dashboard.vercel.app/">Dashboard</NavLi>
     </NavUl>
 
     <div class="flex items-center ms-auto">
