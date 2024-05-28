7b076ab6f985649f0c0b941ae30656aa5d09724c
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-28 21:17:43 +0200

    Update Svelte to next

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,46 +1,228 @@
+interface Page {
+  loc: string;
+  changefreq?: string;
+	priority?: string;
+	lastmod?: string;
+}
+
 const site = 'https://svelte-5-ui-lib.codewithshin.com'; 
-const pages: string[] = [
-	'components/alert',
-	'components/avatar',
-	'components/badge',
-	'components/banner',
-	'components/bottom-navigation',
-	'components/bredcrumb',
-	'components/button',
-	'components/button-group',
-	'components/card',
-	'components/darkmode',
-	'components/device-mockups',
-	'components/drawer',
-	'components/dropdown',
-	'components/footer',
-	'components/gallery',
-	'components/indicator',
-	'components/kbd',
-	'components/list-group',
-	'components/progress',
-	'components/rating',
-	'components/sidebar',
-	'components/skeleton',
-	'components/spinner',
-	'components/tab',
-	'components/table',
-	'components/toast',
-	'components/video',
-	'forms/checkbox',
-	'forms/floating-label',
-	'forms/radio',
-	'forms/range',
-	'forms/select',
-	'forms/toggle',
-	'pages/about',
-	'plugins/chart',
-	'typography/blockquote',
-	'typography/heading',
-	'typography/hr',
-	'typography/link',
-	'typography/list',
-	'typography/paragraph',
+const pages: Page[] = [
+	{
+		loc: '',
+		changefreq: 'weekly',
+		priority: '0.5',
+		lastmod: '2024-05-20'
+	},
+	{
+		loc: 'components/alert',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/avatar',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/badge',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/banner',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/bottom-navigation',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/bredcrumb',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/button',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/button-group',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/card',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/darkmode',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/device-mockups',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/drawer',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/dropdown',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/footer',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/gallery',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/indicator',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/kbd',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/list-group',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/navbar',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/progress',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/rating',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/sidebar',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/skeleton',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/spinner',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/tab',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/table',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/toast',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'components/video',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'forms/checkbox',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'forms/floating-label',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'forms/radio',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'forms/range',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'forms/select',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'forms/toggle',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'pages/about',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'plugins/chart',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'typography/blockquote',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'typography/heading',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'typography/hr',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'typography/link',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'typography/list',
+		changefreq: 'weekly',
+		priority: '0.5',
+	},
+	{
+		loc: 'typography/paragraph',
+		changefreq: 'weekly',
+		priority: '0.5',
+	}
 ];
 
 /** @type {import('./$types').RequestHandler} */
@@ -52,7 +234,7 @@ export async function GET({ url }) {
 	return response;
 }
 
-const sitemap = (pages: string[]) => `<?xml version="1.0" encoding="UTF-8" ?>
+const sitemap = (pages: Page[]) => `<?xml version="1.0" encoding="UTF-8" ?>
 <urlset
   xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
@@ -63,11 +245,12 @@ const sitemap = (pages: string[]) => `<?xml version="1.0" encoding="UTF-8" ?>
 >
   ${pages
 	.map(
-		(page) => `
+		({ loc, changefreq, priority, lastmod }) => `
   <url>
-    <loc>${site}/${page}</loc>
-    <changefreq>daily</changefreq>
-    <priority>0.5</priority>
+    <loc>${site}/${loc}</loc>
+    ${changefreq ? `<changefreq>${changefreq}</changefreq>` : ''}
+		${priority ? `<priority>${priority}</priority>` : ''}
+		${lastmod ? `<lastmod>${lastmod}</lastmod>` : ''}
   </url>
   `
 	)
