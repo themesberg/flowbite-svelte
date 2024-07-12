174b699dfb82c01e099aaee0333c21adc912c847
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-07-12 17:15:17 +0200

    fix: extends svelte/element for Helper

--- src/lib/forms/Helper.svelte
+++ src/lib/forms/Helper.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
   import { twMerge } from 'tailwind-merge';
-  interface Props {
+  import type { HTMLAttributes } from 'svelte/elements';
+
+  interface Props extends HTMLAttributes<HTMLParagraphElement> {
     children: Snippet;
-    id?: string;
     class?: string;
     color?: 'gray' | 'green' | 'red' | 'disabled';
   }
-  let { children, id, class: className, color = 'gray', ...attributes }: Props = $props();
+  let { children, class: className, color = 'gray', ...attributes }: Props = $props();
   const colorClasses = {
     gray: 'text-gray-900 dark:text-gray-300',
     green: 'text-green-700 dark:text-green-500',
@@ -15,11 +16,8 @@
     disabled: 'text-gray-400 dark:text-gray-500'
   };
 </script>
-
-
   <p
     {...attributes}
-    {id}
     class={twMerge(
       'text-xs font-normal text-gray-500 dark:text-gray-300',
       colorClasses[color],
@@ -28,14 +26,3 @@
   >
     {@render children()}
   </p>
-
-<!--
-@component
-[Go to docs](https://svelte-5-ui-lib.codewithshin.com/)
-## Props
-@prop children
-@prop id
-@prop class: className
-@prop color = 'gray'
-@prop ...attributes
--->
