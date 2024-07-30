bff24049c60367abe91d4de8ce22f4d14eb0d420
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-07-30 20:55:59 +0200

    feat: working on Card variants

--- src/lib/cards/Card.svelte
+++ src/lib/cards/Card.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
-  import type { Snippet } from 'svelte';
-  import { twMerge } from 'tailwind-merge';
-  import type { HTMLAttributes } from 'svelte/elements';
-  type CardSizeType = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-
-  type ImgType = {
-    src: string | undefined | null;
-    alt: string | undefined | null;
-  };
-  interface Props extends HTMLAttributes<HTMLDivElement> {
-    children: Snippet;
-    href?: string | undefined | null;
-    horizontal?: boolean;
-    target?: string | undefined | null;
-    shadow?: boolean;
-    reverse?: boolean;
-    img?: ImgType;
-    padding?: 'none' | 'sm' | 'md' | 'lg' | 'xl';
-    size?: CardSizeType;
-    class?: string | undefined;
-    onclick?: () => void;
-  }
-
+  // import type { Snippet } from 'svelte';
+  // import { twMerge } from 'tailwind-merge';
+  // import type { HTMLAttributes } from 'svelte/elements';
+  import { cardVariants, type CardProps as Props } from '.'
+  
   let {
     children,
     href,
@@ -37,72 +19,83 @@
     ...attributes
   }: Props = $props();
 
-  const paddings = {
-    none: 'p-0',
-    sm: 'p-4 sm:p-6 md:p-8',
-    md: 'p-4 sm:p-5',
-    lg: 'p-4 sm:p-6',
-    xl: 'p-4 sm:p-8'
-  };
+  // const paddings = {
+  //   none: 'p-0',
+  //   sm: 'p-4 sm:p-6 md:p-8',
+  //   md: 'p-4 sm:p-5',
+  //   lg: 'p-4 sm:p-6',
+  //   xl: 'p-4 sm:p-8'
+  // };
 
-  const sizes = {
-    xs: 'max-w-xs',
-    sm: 'max-w-sm',
-    md: 'max-w-lg',
-    lg: 'max-w-2xl',
-    xl: 'max-w-screen-xl'
-  };
-  // let role = href ? 'link' : 'presentation';
-  let innerPadding: string = $state('');
-  let cardClass: string = $state('');
-  // let tag = href ? 'a' : 'div';
-  let imgClass: string = $state('');
-  // $inspect('href: ', href)
-  // $inspect('shadow: ', shadow)
-  $effect(() => {
-    innerPadding = paddings[padding];
-    cardClass = twMerge(
-      'w-full flex max-w-sm bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700',
-      sizes[size],
-      shadow && 'shadow-md',
-      reverse ? 'flex-col-reverse' : 'flex-col',
-      horizontal && (reverse ? 'md:flex-row-reverse' : 'md:flex-row'),
-      href && 'cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700',
-      !img && innerPadding,
-      classname
-    );
-    imgClass = twMerge(
-      reverse ? 'rounded-b-lg' : 'rounded-t-lg',
-      horizontal &&
-        'object-cover w-full h-96 md:h-auto md:w-48 md:rounded-none',
-      horizontal && (reverse ? 'md:rounded-e-lg' : 'md:rounded-s-lg')
-    );
+  // const sizes = {
+  //   xs: 'max-w-xs',
+  //   sm: 'max-w-sm',
+  //   md: 'max-w-lg',
+  //   lg: 'max-w-2xl',
+  //   xl: 'max-w-screen-xl'
+  // };
+  // // let role = href ? 'link' : 'presentation';
+  // let innerPadding: string = $state('');
+  // let cardClass: string = $state('');
+  // // let tag = href ? 'a' : 'div';
+  // let imgClass: string = $state('');
+  // // $inspect('href: ', href)
+  // // $inspect('shadow: ', shadow)
+  // $effect(() => {
+  //   innerPadding = paddings[padding];
+  //   cardClass = twMerge(
+  //     'w-full flex max-w-sm bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700',
+  //     sizes[size],
+  //     shadow && 'shadow-md',
+  //     reverse ? 'flex-col-reverse' : 'flex-col',
+  //     horizontal && (reverse ? 'md:flex-row-reverse' : 'md:flex-row'),
+  //     href && 'cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700',
+  //     !img && innerPadding,
+  //     classname
+  //   );
+  //   imgClass = twMerge(
+  //     reverse ? 'rounded-b-lg' : 'rounded-t-lg',
+  //     horizontal &&
+  //       'object-cover w-full h-96 md:h-auto md:w-48 md:rounded-none',
+  //     horizontal && (reverse ? 'md:rounded-e-lg' : 'md:rounded-s-lg')
+  //   );
+  // });
+  const {base, image, content} = cardVariants({
+    size,
+    shadow,
+    padding,
+    horizontal,
+    reverse,
+    href: !!href
   });
 </script>
 
-{#snippet content()}
-  {#if img}
-    <img class={imgClass} src={img.src} alt={img.alt} />
-    <div class={innerPadding}>
-      {@render children()}
-    </div>
-  {:else}
-    {@render children()}
-  {/if}
-{/snippet}
 
-<div
-  {...attributes}
-  class={cardClass}
-  role={href ? 'link' : 'presentation'}
-  {onclick}
->
+<div class={base()} role={href ? 'link' : 'presentation'} onclick={onclick} {...attributes}>
   {#if href}
     <a {href} {target}>
-      {@render content()}
+      {#if img}
+        <img class={image()} src={img.src} alt={img.alt} />
+        <div class={content()}>
+          {@render children()}
+        </div>
+      {:else}
+        <div class={content()}>
+          {@render children()}
+        </div>
+      {/if}
     </a>
   {:else}
-    {@render content()}
+    {#if img}
+      <img class={image()} src={img.src} alt={img.alt} />
+      <div class={content()}>
+        {@render children()}
+      </div>
+    {:else}
+      <div class={content()}>
+        {@render children()}
+      </div>
+    {/if}
   {/if}
 </div>
 
--- src/lib/cards/index.ts
+++ src/lib/cards/index.ts
@@ -1,3 +1,83 @@
 import Card from './Card.svelte';
+import { type VariantProps, tv } from 'tailwind-variants';
+import type { Snippet } from 'svelte';
+import type { HTMLAttributes } from 'svelte/elements';
 
-export { Card };
+const cardVariants = tv({
+  slots: {
+    base: 'w-full flex max-w-sm bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700',
+    image: 'rounded-t-lg',
+    content: '',
+  },
+  variants: {
+    size: {
+      xs: { base: 'max-w-xs' },
+      sm: { base: 'max-w-sm' },
+      md: { base: 'max-w-lg' },
+      lg: { base: 'max-w-2xl' },
+      xl: { base: 'max-w-screen-xl' },
+    },
+    shadow: {
+      true: { base: 'shadow-md' },
+    },
+    padding: {
+      none: { content: 'p-0' },
+      sm: { content: 'p-4 sm:p-6 md:p-8' },
+      md: { content: 'p-4 sm:p-5' },
+      lg: { content: 'p-4 sm:p-6' },
+      xl: { content: 'p-4 sm:p-8' },
+    },
+    horizontal: {
+      true: { base: 'md:flex-row', img: 'object-cover w-full h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg' },
+    },
+    reverse: {
+      true: { base: 'flex-col-reverse', img: 'rounded-b-lg' },
+    },
+    href: {
+      true: { base: 'cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700' },
+    },
+  },
+  compoundVariants: [
+    {
+      horizontal: true,
+      reverse: true,
+      class: { base: 'md:flex-row-reverse', img: 'md:rounded-e-lg' },
+    },
+  ],
+  defaultVariants: {
+    size: 'sm',
+    shadow: true,
+    padding: 'lg',
+    horizontal: false,
+    reverse: false,
+  },
+});
+
+// type CardSizeType = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+type CardSizeType = VariantProps<typeof cardVariants>['size'];
+type PaddingType = VariantProps<typeof cardVariants>['padding'];
+
+
+type ImgType = {
+  src: string | undefined | null;
+  alt: string | undefined | null;
+};
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+  children: Snippet;
+  href?: string | undefined | null;
+  horizontal?: boolean;
+  target?: string | undefined | null;
+  shadow?: boolean;
+  reverse?: boolean;
+  img?: ImgType;
+  padding?: PaddingType;
+  size?: CardSizeType;
+  class?: string | undefined;
+  onclick?: () => void;
+}
+
+export {
+  Card,
+  cardVariants,
+  type CardProps,
+};
