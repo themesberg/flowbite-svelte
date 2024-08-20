0d1b1b932b0d6adf069aec6e664aa7cb16d77fd5
koplas <54645365+koplas@users.noreply.github.com>
2024-08-20 11:15:02 +0200

    chore: migrate to flat file config

---
+++ eslint.config.mjs
@@ -0,0 +1,52 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import parser from 'svelte-eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  {
+    ignores: ['**/*.cjs', '.vercel/', '.svelte-kit/']
+  },
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:svelte/recommended'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    }
+  },
+  {
+    files: ['**/*.svelte'],
+
+    languageOptions: {
+      parser: parser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  }
+];
