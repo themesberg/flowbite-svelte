c8f9b43dfb37873bb7067227c76a0582ce3351d8
Andrew Risse <andrewrisse@gmail.com>
2024-08-30 16:43:23 -0600

    init

--- src/lib/toast/Toast.svelte
+++ src/lib/toast/Toast.svelte
@@ -62,7 +62,7 @@
   };
   // | 'gray'| 'red'| 'yellow'| 'green'| 'indigo'| 'purple'| 'pink'| 'blue'| 'primary'| 'none';
 
-  let iconClass: string = twMerge(
+  $: iconClass = twMerge(
     'inline-flex items-center justify-center shrink-0 rounded-lg',
     colors[color],
     defaultIconClass
