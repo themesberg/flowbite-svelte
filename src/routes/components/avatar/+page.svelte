6bfb553c90ea21dcc6f15db02911134cd394559e
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-09-10 18:54:52 +0200

    docs: avatar page

--- src/routes/components/avatar/+page.svelte
+++ src/routes/components/avatar/+page.svelte
@@ -142,9 +142,7 @@
       <Radio labelClass="w-40 my-1" onclick={() => (exampleExpand = false)} name="block_style" bind:group={selectedExample} value={style.name}>{style.name}</Radio>
     {/each}
   </div>
-  <div class="md:h-40">
     <SelectedComponent />
-  </div>
   {#snippet codeblock()}
     <DynamicCodeBlockHighlight replaceLib {handleExpandClick} expand={exampleExpand} {showExpandButton} code={exampleModules[`./examples/${markdown}`] as string} />
   {/snippet}
