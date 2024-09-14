73d532e169e5208d7fcbb1e753f712200d36671b
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-09-14 18:15:49 +0200

    docs: breadcrumb page

--- src/routes/components/breadcrumb/+page.svelte
+++ src/routes/components/breadcrumb/+page.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
-  import { Breadcrumb, BreadcrumbItem, Button } from '$lib';
-  import { HomeOutline, ChevronDoubleRightOutline } from 'flowbite-svelte-icons';
-  import HighlightCompo from '../../utils/HighlightCompo.svelte';
+  import { type Component } from 'svelte';
+  import { Breadcrumb, BreadcrumbItem, Button, Label, Radio, uiHelpers } from '$lib';
+  import DynamicCodeBlockHighlight from '../../utils/DynamicCodeBlockHighlight.svelte';
   import CodeWrapper from '../../utils/CodeWrapper.svelte';
   import H1 from '../../utils/H1.svelte';
   import H2 from '../../utils/H2.svelte';
+  import { isSvelteOverflow, getExampleFileName } from '../../utils/helpers';
   // for Props table
   import CompoAttributesViewer from '../../utils/CompoAttributesViewer.svelte';
   const dirName = 'breadcrumb';
-  const modules = import.meta.glob('./md/*.md', {
-    query: '?raw',
-    import: 'default',
-    eager: true
-  });
 
   let navClass: Breadcrumb['class'] = $state('');
   const changeNavClass = () => {
@@ -22,15 +18,60 @@
   const changeOlClass = () => {
     olClass = olClass === '' ? 'border border-blue-500 p-2' : '';
   };
+  // for examples section that dynamically changes the svelte component and markdown content
+  import * as ExampleComponents from './examples';
+  const exampleModules = import.meta.glob('./examples/*.svelte', {
+    query: '?raw',
+    import: 'default',
+    eager: true
+  });
+
+  const exampleArr = [
+    { name: 'Default', component: ExampleComponents.Default },
+    { name: 'Solid', component: ExampleComponents.Solid },
+    { name: 'Icon', component: ExampleComponents.Icon },
+  ];
+  let selectedExample = $state(exampleArr[0].name);
+  let markdown = $derived(getExampleFileName(selectedExample, exampleArr));
+
+  function findObject(arr: { name: string; component: Component }[], name: string) {
+    const matchingObject = arr.find((obj) => obj.name === name);
+    return matchingObject ? matchingObject.component : null;
+  }
+  const SelectedComponent = $derived(findObject(exampleArr, selectedExample));
+  // end of dynamic svelte component
+
+// for examples DynamicCodeBlockHighlight
+let codeBlock = uiHelpers();
+  let exampleExpand = $state(false);
+  let showExpandButton = $derived(isSvelteOverflow(markdown, exampleModules));
+  const handleExpandClick = () => {
+    exampleExpand = !exampleExpand;
+  };
+  // end of DynamicCodeBlock setup
+  $effect(() => {
+    exampleExpand = codeBlock.isOpen;
+  });
 </script>
 
 <H1>Breadcrumb</H1>
 
-<H2>Setup</H2>
+<H2>Examples</H2>
 
-<HighlightCompo code={modules['./md/setup.md'] as string} />
+<CodeWrapper>
+  <div class="mb-12 flex flex-wrap">
+    <Label class="mb-4 w-full font-bold">Example:</Label>
+    {#each exampleArr as style}
+      <Radio labelClass="w-40 my-1" onclick={() => (exampleExpand = false)} name="block_style" bind:group={selectedExample} value={style.name}>{style.name}</Radio>
+    {/each}
+  </div>
+  <SelectedComponent />
+  {#snippet codeblock()}
+    <DynamicCodeBlockHighlight replaceLib {handleExpandClick} expand={exampleExpand} {showExpandButton} code={exampleModules[`./examples/${markdown}`] as string} />
+  {/snippet}
+</CodeWrapper>
 
-<H2>Default Breadcrumb</H2>
+<H2>navClass and olClass</H2>
 
 <CodeWrapper innerClass="overflow-x-scroll p-2 sm:p-6">
   <div class="h-20">
@@ -44,49 +85,6 @@
     <Button class="w-48" onclick={changeNavClass}>{navClass ? 'Remove navClass' : 'Add navClass'}</Button>
     <Button class="w-48" color="green" onclick={changeOlClass}>{olClass ? 'Remove olClass' : 'Add olClass'}</Button>
   </div>
-  {#snippet codeblock()}
-    <HighlightCompo code={modules['./md/defaultbreadcrumb.md'] as string} />
-  {/snippet}
-</CodeWrapper>
-
-<H2>Solid Breadcrumb</H2>
-
-<CodeWrapper innerClass="overflow-x-scroll p-2 sm:p-6">
-  <Breadcrumb solid class="overflow-x-scroll">
-    <BreadcrumbItem href="/" home>Home</BreadcrumbItem>
-    <BreadcrumbItem href="/">Projects</BreadcrumbItem>
-    <BreadcrumbItem>Flowbite Svelte</BreadcrumbItem>
-  </Breadcrumb>
-  {#snippet codeblock()}
-    <HighlightCompo code={modules['./md/solidbreadcrumb.md'] as string} />
-  {/snippet}
-</CodeWrapper>
-
-<H2>Icons</H2>
-
-<CodeWrapper innerClass="overflow-x-scroll p-2 sm:p-6">
-  <Breadcrumb class="overflow-x-scroll bg-gray-50 px-5 py-3 dark:bg-gray-900">
-    <BreadcrumbItem href="/" home>
-      {#snippet icon()}
-        <HomeOutline class="me-2 h-4 w-4" />
-      {/snippet}Home
-    </BreadcrumbItem>
-    <BreadcrumbItem href="/">
-      {#snippet icon()}
-        <ChevronDoubleRightOutline class="mx-2 h-5 w-5 dark:text-white" />
-      {/snippet}
-      Projects
-    </BreadcrumbItem>
-    <BreadcrumbItem>
-      {#snippet icon()}
-        <ChevronDoubleRightOutline class="mx-2 h-5 w-5 dark:text-white" />
-      {/snippet}
-      Flowbite Svelte
-    </BreadcrumbItem>
-  </Breadcrumb>
-  {#snippet codeblock()}
-    <HighlightCompo code={modules['./md/icons.md'] as string} />
-  {/snippet}
 </CodeWrapper>
 
 <H2>Component data</H2>
---
+++ src/routes/components/breadcrumb/examples/Default.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Breadcrumb, BreadcrumbItem } from '$lib';
+</script>
+
+<div class="overflow-x-scroll p-2 sm:p-6">
+  <Breadcrumb>
+    <BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+    <BreadcrumbItem href="/">Projects</BreadcrumbItem>
+    <BreadcrumbItem>Flowbite Svelte</BreadcrumbItem>
+  </Breadcrumb>
+</div>
