361e753d95b9e13920e6f0b25adcfe4bcb78bcde
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-09-16 06:54:38 +0200

    chore: cleanup navbar dir

--- src/routes/components/navbar/+page.svelte
+++ src/routes/components/navbar/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { type Component } from 'svelte';
-  import { Navbar, NavLi, NavBrand, NavUl, uiHelpers, ImagePlaceholder, Skeleton, TextPlaceholder, P, Label, Radio } from '$lib';
-  import HighlightCompo from '../../utils/HighlightCompo.svelte';
+  import { Navbar, uiHelpers, Label, Radio } from '$lib';
   import DynamicCodeBlockHighlight from '../../utils/DynamicCodeBlockHighlight.svelte';
   import CodeWrapper from '../../utils/CodeWrapper.svelte';
   import H1 from '../../utils/H1.svelte';
@@ -10,35 +9,6 @@
   // for Props table
   import CompoAttributesViewer from '../../utils/CompoAttributesViewer.svelte';
   const dirName = 'nav';
-  const modules = import.meta.glob('./md/*.md', {
-    query: '?raw',
-    import: 'default',
-    eager: true
-  });
-
-  let breakPoint: Navbar['breakpoint'] = $state('md');
-  let nav = uiHelpers();
-  let navStatus = $state(false);
-  let toggleNav = nav.toggle;
-  let closeNav = nav.close;
-
-  let nav2 = uiHelpers();
-  let navStatus2 = $state(false);
-  let toggleNav2 = nav2.toggle;
-  let closeNav2 = nav2.close;
-
-  let navLg = uiHelpers();
-  let navStatusLg = $state(false);
-  let toggleNavLg = navLg.toggle;
-  let closeNavLg = navLg.close;
-
-  $effect(() => {
-    // this can be done adding nav.navStatus directly to DOM element
-    // without using effect
-    navStatus = nav.isOpen;
-    navStatus2 = nav2.isOpen;
-    navStatusLg = navLg.isOpen;
-  });
 
   // for examples section that dynamically changes the svelte component and markdown content
   import * as ExampleComponents from './examples';
