1c80d4688b34c9dc2ef98b74cf742130b1408fb4
Giulio Ginesi <nanc23@rocketmail.com>
2024-09-23 14:10:23 +0100

    Bind input value to component value

--- src/lib/forms/search/Search.svelte
+++ src/lib/forms/search/Search.svelte
@@ -12,7 +12,7 @@
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
     </svg>
   </div>
-  <input type="search" class={inputCls({ class: inputClass })} {placeholder} required {...restProps} />
+  <input type="search" bind:value class={inputCls({ class: inputClass })} {placeholder} required {...restProps} />
   {#if children}
     <div class={content()}>
       {@render children()}
