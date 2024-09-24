893a2c82ea2a21d52dd9d37f2f8d100db2c18758
Christoph Klassen <100708552+cintek@users.noreply.github.com>
2024-09-24 18:03:32 +0200

    Make it easier to see if controls are disabled (#1448)

--- src/lib/forms/Label.svelte
+++ src/lib/forms/Label.svelte
@@ -11,7 +11,7 @@
     gray: 'text-gray-900 dark:text-gray-300',
     green: 'text-green-700 dark:text-green-500',
     red: 'text-red-700 dark:text-red-500',
-    disabled: 'text-gray-400 dark:text-gray-500'
+    disabled: 'text-gray-400 dark:text-gray-500 grayscale contrast-50'
   };
 
   // function checkDisabled(node: HTMLLabelElement) {
