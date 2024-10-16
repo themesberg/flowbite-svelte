8ebae26f97730f272640a76a5345f62947addb50
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-10-16 12:53:23 +0200

    fix: remove value from Fileupload

--- src/lib/forms/Fileupload.svelte
+++ src/lib/forms/Fileupload.svelte
@@ -4,17 +4,16 @@
   import Input from './Input.svelte';
   
   interface $$Props extends ComponentProps<Input> {
-    value?: string;
     files?: FileList | undefined;
     inputClass?: string;
   }
-  export let value: $$Props['value'] = '';
+  
   export let files: $$Props['files'] = undefined;
   export let inputClass: $$Props['inputClass'] = 'border !p-0 dark:text-gray-400';
 </script>
 
 <Input {...$$restProps} class={twMerge(inputClass, $$props.class)} let:props>
-  <input type="file" on:change on:keyup on:keydown on:keypress on:focus on:blur on:click on:mouseover on:mouseenter on:mouseleave on:paste bind:value bind:files {...props} />
+  <input type="file" on:change on:keyup on:keydown on:keypress on:focus on:blur on:click on:mouseover on:mouseenter on:mouseleave on:paste bind:files {...props} />
 </Input>
 
 <!--
