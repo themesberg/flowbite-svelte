3d5e50503aab848dd60a4f3e123169e1541e4304
Abdul-Kadir Coskun <59434446+Chizaruu@users.noreply.github.com>
2024-10-17 20:36:02 +1100

    feat: add prop 'icon' to timepicker

--- src/lib/forms/Timepicker.svelte
+++ src/lib/forms/Timepicker.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
-  import { ClockSolid } from 'flowbite-svelte-icons';
   import type { Colors } from '$lib/types';
   import { getBorderClass, getFocusRingClass } from '$lib/utils/getStyles';
+  import type { ComponentType } from 'svelte';
 
   export let id: string = 'time';
   export let value: string = '00:00';
@@ -11,17 +11,40 @@
   export let required: boolean = true;
   export let disabled: boolean = false;
   export let color: Colors = 'primary';
+  export let icon: ComponentType | string = `<svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6v4l3.276 3.276M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+  </svg>`;
 
   const dispatch = createEventDispatcher();
+
   function handleChange(event: Event) {
     const newValue = (event.target as HTMLInputElement).value;
     dispatch('change', newValue);
   }
 </script>
 
-<div class="relative">
-  <div class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
-    <ClockSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
-  </div>
-  <input type="time" {id} class="bg-gray-50 border leading-none text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white {getBorderClass(color)} {getFocusRingClass(color)}" {min} {max} bind:value {required} {disabled} on:change={handleChange} />
+<div class="flex">
+  <input {id} type="time" class="rounded-none rounded-s-lg bg-gray-50 border text-gray-900 leading-none focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 {getBorderClass(color)} {getFocusRingClass(color)} {icon ? 'rounded-e-none' : 'rounded-e-lg'}" {min} {max} bind:value {required} {disabled} on:change={handleChange} />
+  <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-s-0 border-s-0 border-gray-300 rounded-e-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+    {#if typeof icon === 'string'}
+      {@html icon}
+    {:else if icon}
+      <svelte:component this={icon} class="w-4 h-4 text-gray-500 dark:text-gray-400" />
+    {/if}
+  </span>
 </div>
+
+<!--
+@component
+[Go to docs](https://flowbite-svelte.com/)
+## Props
+@prop export let id: string = 'time';
+@prop export let value: string = '00:00';
+@prop export let min: string = '';
+@prop export let max: string = '';
+@prop export let required: boolean = true;
+@prop export let disabled: boolean = false;
+@prop export let color: Colors = 'primary';
+@prop export let icon: ComponentType | string = `<svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6v4l3.276 3.276M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/> </svg>`;
+-->
--- src/routes/docs/forms/timepicker.md
+++ src/routes/docs/forms/timepicker.md
@@ -8,13 +8,11 @@ description: Use the timepicker component to allow the user to select a time usi
 ---
 
 <script>
-  import { TableProp, TableDefaultRow, DocBadgeList, CompoAttributesViewer, GitHubCompoLinks, toKebabCase } from '../../utils'
-  import { Badge, A } from '$lib'
-
+  import { CompoAttributesViewer, GitHubCompoLinks } from '../../utils'
   const components = 'Timepicker'
 </script>
 
-The timepicker component can be used to allow the user to select a specific time using a native time input element. This component is built with Svelte and uses the native time input from HTML to handle the time functionality.
+The timepicker component can be used to allow the user to select a specific time using a custom time input element and provides flexibility in styling and icon usage.
 
 ## Setup
 
@@ -35,6 +33,27 @@ Use this example of a default timepicker component to allow the user to select a
 
 <Label>Select Time:</Label>
 <Timepicker />
+```
+
+## Timepicker with icon
+
+This example can be used to select a time via an input field where you can add an icon to the input group. You can use either a Flowbite Svelte icon component or a custom SVG.
+
+```svelte example
+<script>
+  import { Label, Timepicker } from 'flowbite-svelte';
+  import { ClockSolid } from 'flowbite-svelte-icons';
+
+  const customSvg = `<svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6v4l3.276 3.276M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+  </svg>`;
+</script>
+
+<Label>Select Time (Flowbite Icon):</Label>
+<Timepicker icon={ClockSolid} />
+
+<Label>Select Time (Custom SVG):</Label>
+<Timepicker icon={customSvg} />
 ```
 
 ## Timepicker with custom props
