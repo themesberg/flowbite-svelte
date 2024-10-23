f95317db06f85591ad6537b95f4e537b9666e927
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-10-23 17:16:50 +0200

    docs: update tooltip ex Inline

--- src/routes/components/tooltip/examples/Inline.svelte
+++ src/routes/components/tooltip/examples/Inline.svelte
@@ -2,6 +2,6 @@
   import { Span, P, Tooltip } from "$lib";
 </script>
 
-<P>Lorem ipsum dolor sit amet, consectetur adipiscing <Span highlight="red" id="ex-inline">tool tip in a paragraph</Span> elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</P>
+<P class="mt-20">Lorem ipsum dolor sit amet, consectetur adipiscing <Span highlight="red" id="ex-inline">tool tip in a paragraph</Span> elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</P>
 
 <Tooltip triggeredBy="#ex-inline" color="yellow" position="top" class="p-4">Tooltip content</Tooltip>
