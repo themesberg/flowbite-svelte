59c5ef390b70a0388315cf490d571549a3d2bf6b
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-23 20:51:04 +0100

    docs: change span to div in indicators/examples/legend

--- src/routes/components/indicators/examples/Legend.svelte
+++ src/routes/components/indicators/examples/Legend.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="flex flex-wrap justify-start gap-4 md:justify-center">
-  <span class="flex items-center"><Indicator size="sm" color="orange" class="me-1.5" />Visitors</span>
-  <span class="flex items-center"><Indicator size="sm" color="purple" class="me-1.5" />Sessions</span>
-  <span class="flex items-center"><Indicator size="sm" color="indigo" class="me-1.5" />Customers</span>
-  <span class="flex items-center"><Indicator size="sm" color="teal" class="me-1.5" />Revenue</span>
+  <div class="flex items-center"><Indicator size="sm" color="orange" class="me-1.5" />Visitors</div>
+  <div class="flex items-center"><Indicator size="sm" color="purple" class="me-1.5" />Sessions</div>
+  <div class="flex items-center"><Indicator size="sm" color="indigo" class="me-1.5" />Customers</div>
+  <div class="flex items-center"><Indicator size="sm" color="teal" class="me-1.5" />Revenue</div>
 </div>
