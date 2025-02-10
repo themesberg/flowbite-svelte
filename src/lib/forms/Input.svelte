e7885382c4a8fcad7a099092560cb70a229673c2
michael.rowe <michael@iplexone.com>
2025-02-10 17:20:59 -0500

    Fix Input clearable

--- src/lib/forms/Input.svelte
+++ src/lib/forms/Input.svelte
@@ -97,7 +97,7 @@
   </div>
   {/if}
   {#if clearable && value && `${value}`.length > 0}
-    <CloseButton {size} on:click={clearAll} color="none" class=" {twMerge(floatClass, classRight)} focus:ring-0 end-6 focus:ring-gray-400 dark:text-white" />
+    <CloseButton {size} on:click={clearAll} color="none" class=" {twMerge(floatClass, classRight)} focus:ring-0 end-1 focus:ring-gray-400 dark:text-white" />
   {/if}
 </Wrapper>
 
