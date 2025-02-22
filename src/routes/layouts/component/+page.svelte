fc020ef792a75c6933eef29e7650333221feb5fa
jjagielka <jjagielka@gmail.com>
2025-02-22 13:23:14 +0100

    docs layout corrections (#1543)

--- src/routes/layouts/component/+page.svelte
+++ src/routes/layouts/component/+page.svelte
@@ -30,7 +30,7 @@
 <MetaTag {breadcrumb_title} {title} {dir} {description} />
 
 <div class="flex w-full">
-  <div class="flex flex-col max-w-4xl mx-auto px-4 min-w-0 pt-6 lg:px-8 lg:pt-8 pb:12 xl:pb-24 lg:pb-16 divide-y divide-gray-200 dark:divide-gray-800">
+  <div class="flex flex-col max-w-4xl mx-auto px-4 min-w-0 pt-6 lg:px-8 lg:pt-8 pb:12 xl:pb-24 lg:pb-16">
     <PageHeadSection {title} {description} />
     <div id="mainContent" class="py-8">
       <slot />
@@ -41,5 +41,3 @@
   </div>
   <Toc {extract} headingSelector="#mainContent > :where(h2, h3)" />
 </div>
-
-
