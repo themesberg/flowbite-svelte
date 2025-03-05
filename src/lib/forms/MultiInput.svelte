ba3053b314a3a2ab835059ff95b83d3bfee8ddd7
Rath Pascal <rath@oxl.at>
2025-03-05 21:09:12 +0100

    feat: multi-input form-component - check fixes

--- src/lib/forms/MultiInput.svelte
+++ src/lib/forms/MultiInput.svelte
@@ -1,85 +1,115 @@
-<script context="module">export function clampSize(s) {
-  return s && s === "xs" ? "sm" : s === "xl" ? "lg" : s;
-}
+<script context="module" lang="ts">
+  import type { SizeType, FormSizeType } from '$lib/types';
+  export function clampSize(s: SizeType) {
+    return s && s === 'xs' ? 'sm' : s === 'xl' ? 'lg' : s;
+  }
 </script>
 
-<script>import Wrapper from "$lib/utils/Wrapper.svelte";
-import { twMerge } from "tailwind-merge";
-import { createEventDispatcher, getContext } from "svelte";
-import Badge from "$lib/badge/Badge.svelte";
-import CloseButton from '$lib/utils/CloseButton.svelte';
-export let type = "text";
-export let value = [];
-export let size = void 0;
-export let clearable = false;
-export let defaultClass = "block w-full disabled:cursor-not-allowed disabled:opacity-50 rtl:text-right";
-export let color = "base";
-export let floatClass = "flex absolute inset-y-0 items-center text-gray-500 dark:text-gray-400";
-export let classLeft = "";
-export let classRight = "";
-export let placeholder = "";
-export let disabled = false;
-export let separators = ["Enter", ","];
-export let validationCallback = undefined;
-export let validationErrorClass = "text-red-700 dark:text-red-500";
-let inputCurrent = "";
-let inputInvalid = false;
-let idCounter = 0;
-$: inputItems = value.map(item => {
-  idCounter += 1;
-  return { value: item, id: idCounter };
-});
-const dispatcher = createEventDispatcher();
-const multiInputClass = "relative border border-gray-300 flex items-center rounded-lg gap-2 dark:border-gray-600 ring-primary-500 dark:ring-primary-500 focus-visible:outline-none bg-gray-50 text-gray-900 dark:bg-gray-600 dark:text-white";
-const sizes = {
-  sm: "px-2 py-1 min-h-[2.4rem]",
-  md: "px-3 py-1 min-h-[2.7rem]",
-  lg: "px-4 py-2 min-h-[3.2rem]"
-};
-let background = getContext("background");
-let group = getContext("group");
-const textSizes = { sm: "sm:text-xs", md: "text-sm", lg: "sm:text-base" };
-$: _size = size || clampSize(group?.size) || "md";
-let inputClass;
-$: {
-  const _color = color === "base" && background ? "tinted" : color;
-  inputClass = twMerge([defaultClass, textSizes[_size], group || "rounded-lg", group && "first:rounded-s-lg last:rounded-e-lg", group && "[&:not(:first-child)]:-ms-px", $$props.class]) + " border-none rounded-none p-0";
-}
-const clearAll = (e) => {
-  e.stopPropagation();
-  value = [];
-  dispatcher("change");
-};
-const clearThisOption = (thisValue) => {
-  if (disabled) return;
-  if (value.includes(thisValue)) {
-    value = value.filter((o) => o !== thisValue);
+<script lang="ts">
+  import Wrapper from "$lib/utils/Wrapper.svelte";
+  import { twMerge } from "tailwind-merge";
+  import { createEventDispatcher, getContext } from "svelte";
+  import Badge from "$lib/badge/Badge.svelte";
+  import CloseButton from '$lib/utils/CloseButton.svelte';
+
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  interface $$Props extends Omit<HTMLInputAttributes, 'size'> {
+    type?: InputType;
+    value?: any;
+    size?: FormSizeType;
+    clearable?: boolean;
+    defaultClass?: string;
+    color?: 'base' | 'green' | 'red';
+    floatClass?: string;
+    classLeft?: string;
+    classRight?: string;
+    placeholder?: string;
+    disabled?: boolean;
+    separators?: string[];
+    validationCallback?: CallableFunction;
+    validationErrorClass?: string;
+  }
+
+  export let type: $$Props['type'] = 'text';
+  export let value: NonNullable<$$Props['value']> = [];
+  export let size: $$Props['size'] = undefined;
+  export let clearable: $$Props['clearable'] = false;
+  export let defaultClass: $$Props['defaultClass'] = 'block w-full disabled:cursor-not-allowed disabled:opacity-50 rtl:text-right';
+  export let color: NonNullable<$$Props['color']> = 'base';
+  export let floatClass: $$Props['floatClass'] = 'flex absolute inset-y-0 items-center text-gray-500 dark:text-gray-400';
+  export let classLeft: $$Props['classLeft'] = '';
+  export let classRight: $$Props['classRight'] = '';
+  export let placeholder: $$Props['placeholder'] = '';
+  export let disabled: $$Props['disabled'] = false;
+  export let separators: $$Props['separators'] = ["Enter", ","];
+  export let validationCallback: $$Props['validationCallback'] = undefined;
+  export let validationErrorClass: $$Props['validationErrorClass'] = "text-red-700 dark:text-red-500";
+
+  const dispatcher = createEventDispatcher();
+  const multiInputClass = "relative border border-gray-300 flex items-center rounded-lg gap-2 dark:border-gray-600 ring-primary-500 dark:ring-primary-500 focus-visible:outline-none bg-gray-50 text-gray-900 dark:bg-gray-600 dark:text-white";
+  let idCounter = 0;
+  $: inputItems = value.map(item => {
+    idCounter += 1;
+    return { value: item, id: idCounter };
+  });
+  let inputCurrent = "";
+  let inputInvalid = false;
+
+  const sizes = {
+    sm: "px-2 py-1 min-h-[2.4rem]",
+    md: "px-3 py-1 min-h-[2.7rem]",
+    lg: "px-4 py-2 min-h-[3.2rem]"
+  };
+
+  let background = getContext("background");
+  let group = getContext("group");
+  const textSizes = { sm: "sm:text-xs", md: "text-sm", lg: "sm:text-base" };
+
+  $: _size = size || clampSize(group?.size) || 'md';
+  let inputClass: string;
+  $: {
+    const _color = color === "base" && background ? "tinted" : color;
+    inputClass = twMerge([defaultClass, textSizes[_size], group || "rounded-lg", group && "first:rounded-s-lg last:rounded-e-lg", group && "[&:not(:first-child)]:-ms-px", $$props.class]) + " border-none rounded-none p-0";
+  }
+
+  const clearAll = (e) => {
+    e.stopPropagation();
+    value = [];
     dispatcher("change");
-  }
-};
-function handleInputSeparation() {
-  inputCurrent = inputCurrent.trim();
-  if (inputCurrent && !value.includes(inputCurrent)) {
-    if (validationCallback && !validationCallback(inputCurrent)) {
-      inputInvalid = true;
-      return;
+  };
+
+  const clearThisOption = (thisValue: string) => {
+    if (disabled) return;
+    if (value.includes(thisValue)) {
+      value = value.filter((o: string) => o !== thisValue);
+      dispatcher("change");
     }
-    value = [...value, inputCurrent];
-  }
-  inputCurrent = "";
-};
-function handleKeyDown(event) {
-  if (disabled) return;
-  inputInvalid = false;
-  for (let s of separators) {
-    if (event.key == s) {
-      handleInputSeparation();
-      event.stopPropagation();
-      event.preventDefault();
-      return;
+  };
+
+  function handleInputSeparation() {
+    inputCurrent = inputCurrent.trim();
+    if (inputCurrent && !value.includes(inputCurrent)) {
+      if (validationCallback && !validationCallback(inputCurrent)) {
+        inputInvalid = true;
+        return;
+      }
+      value = [...value, inputCurrent];
     }
-  }
-};
+    inputCurrent = "";
+  };
+  function handleKeyDown(event: KeyboardEvent) {
+    if (disabled) return;
+    inputInvalid = false;
+    for (let s of separators) {
+      if (event.key == s) {
+        handleInputSeparation();
+        event.stopPropagation();
+        event.preventDefault();
+        return;
+      }
+    }
+  };
+
 </script>
 
 <Wrapper class="relative w-full" show={$$slots.left || $$slots.right}>
