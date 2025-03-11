17ff2ab9ff3eb62721418bdf72d2501f4e85b861
Jakub Jagielka <jjagielka@gmail.com>
2025-03-11 23:58:31 +0100

    checkbox button - corrections

--- src/lib/forms/checkbox-button/CheckboxButton.svelte
+++ src/lib/forms/checkbox-button/CheckboxButton.svelte
@@ -4,25 +4,26 @@
   import { type CheckboxButtonProps as Props } from ".";
   import { checkButton } from "./theme";
 
-  let { children, class: className, group = $bindable(), value, checked, inline, pill, outline, size, color, shadow, ...restProps }: Props = $props();
+  let { children, class: className, group = $bindable([]), value, checked, inline, pill, outline, size, color, shadow, ...restProps }: Props = $props();
 
   // react on external group changes
   function init(_: HTMLElement, _group: (string | number)[]) {
     group = _group ?? [];
 
-    if (checked === undefined && value !== undefined) checked = _group.includes(value);
+    if (checked === undefined && value !== undefined) checked = group.includes(value);
     onChange();
 
-    return {
-      update(_group: (string | number)[]) {
-        if (value !== undefined) {
-          checked = _group.includes(value);
-        }
+    $effect(() => {
+      if (value !== undefined) {
+        checked = group.includes(value);
       }
-    };
+    });
   }
 
   function onChange() {
+    console.log("onChange", value);
+    if (!value) return;
+
     // There's a bug in Svelte and bind:group is not working with wrapped checkbox
     // This workaround is taken from:
     // https://svelte.dev/repl/de117399559f4e7e9e14e2fc9ab243cc?version=3.12.1
@@ -57,7 +58,7 @@
 ## Props
 @props: children: any;
 @props:class: string;
-@props:group: any = $bindable();
+@props:group: any = $bindable([]);
 @props:value: any;
 @props:checked: any;
 @props:inline: any;
