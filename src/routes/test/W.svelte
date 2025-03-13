d730aecc7c2987fb97229de3f0f1f1aba5941fdb
Jakub Jagielka <jjagielka@gmail.com>
2025-03-14 00:07:05 +0100

    removal of the test page

--- src/routes/test/W.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  let { group = $bindable(), ...restProps } = $props();
-</script>
-
-<input type="checkbox" bind:group {...restProps} value="e" />
-<input type="checkbox" bind:group {...restProps} value="f" />
