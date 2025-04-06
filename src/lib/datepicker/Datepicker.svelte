2242e3b2a2e2093aba1f41af601c9c93f3926bd1
Teddy <54421295+uhteddy@users.noreply.github.com>
2025-04-06 05:22:36 -0400

    Fix Datepicker text coloring (#1568)

--- src/lib/datepicker/Datepicker.svelte
+++ src/lib/datepicker/Datepicker.svelte
@@ -280,7 +280,7 @@
           <Button on:click={() => changeMonth(-1)} {color} size="sm" aria-label="Previous month">
             <svg class="w-3 h-3 rtl:rotate-180 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"></path></svg>
           </Button>
-          <h3 class="text-lg font-semibold dark:text-white" aria-live="polite">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-white" aria-live="polite">
             {currentMonth.toLocaleString(locale, { month: 'long', year: 'numeric' })}
           </h3>
           <Button on:click={() => changeMonth(1)} {color} size="sm" aria-label="Next month">
