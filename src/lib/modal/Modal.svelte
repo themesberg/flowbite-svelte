4923d96172bbaf307ba151f7060f3d1bf27c6732
Jakub Jagielka <jjagielka@gmail.com>
2025-06-25 07:47:17 +0200

    fix: #1623

--- src/lib/modal/Modal.svelte
+++ src/lib/modal/Modal.svelte
@@ -63,7 +63,7 @@
 </script>
 
 {#if open}
-  <dialog use:trapFocus={{ onEscape: handleEscape }} bind:this={dlg} {...restProps} class={twMerge(base(), clsx(className))} tabindex="-1" oncancel={_oncancel} onclick={_onclick} transition:transition={paramsOptions as ParamsType} onintrostart={() => (modal ? dlg?.showModal() : dlg?.show())} onoutroend={() => dlg?.close()}>
+  <dialog use:trapFocus={{ onEscape: handleEscape }} bind:this={dlg} {...restProps} class={twMerge(base(), clsx(className))} tabindex="-1" oncancel={_oncancel} onclick={_onclick} transition:transition|global={paramsOptions as ParamsType} onintrostart={() => (console.log("opening"), modal ? dlg?.showModal() : dlg?.show())} onoutroend={() => dlg?.close()}>
     {#if title || header}
       <div class={twMerge(headerCls(), clsx(headerClass))}>
         {#if title}
