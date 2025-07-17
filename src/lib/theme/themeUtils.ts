06b833274b84665e804bdf10d912b0881b406c6a
Jakub Jagielka <jjagielka@gmail.com>
2025-07-17 07:18:10 +0200

    types corrections

--- src/lib/theme/themeUtils.ts
+++ src/lib/theme/themeUtils.ts
@@ -7,12 +7,12 @@ export function getTheme<K extends keyof ThemeConfig>(componentKey: K) {
   return theme?.[componentKey];
 }
 
-type Slots<T extends { slots: any }> = Omit<T["slots"], "base">
-export type Classes<T extends { slots: Record<string, any> }> = {
+type Slots<T extends { slots: Record<string, unknown> }> = Omit<T["slots"], "base">
+export type Classes<T extends { slots: Record<string, unknown> }> = {
   classes?: Partial<{ [K in keyof Slots<T>]: ClassValue; }>;
 };
 
-export function themeDeprecated(component: string, names: { [x: string]: any }): void {
+export function themeDeprecated(component: string, names: Record<string, unknown>): void {
   const nonEmptyNames = Object.keys(names).filter(name => names[name]).map(name => `"${name}"`);
   if (nonEmptyNames.length === 0) return;
   console.warn(`The following "${component}" props are deprecated: ${nonEmptyNames.join(", ")}.\nPlease update your code to use the new "classes" property.`);
