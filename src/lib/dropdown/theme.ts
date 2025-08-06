83b5eb8481734d4d80eef763a92a1b9c5142bfba
jjagielka <jjagielka@gmail.com>
2025-08-06 19:26:56 +0200

    fix: dropdown layouts (#1707)

--- src/lib/dropdown/theme.ts
+++ src/lib/dropdown/theme.ts
@@ -17,8 +17,9 @@ export type DropdownItemVariants = VariantProps<typeof dropdownItem> & Classes<t
 
 export const dropdownItem = tv({
   slots: {
-    anchor: "block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white",
-    activeAnchor: "block px-4 py-2 text-primary-700 dark:text-primary-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+    base: "block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white",
+    active: "block px-4 py-2 text-primary-700 dark:text-primary-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white",
+    li: ""
   }
 });
 
