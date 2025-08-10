2a7d30a5e55dce5ac30db903cf5f0d5b640f834d
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-08-10 16:36:33 +0200

    fix: remove analytics

--- src/routes/admin-dashboard/+layout.svelte
+++ src/routes/admin-dashboard/+layout.svelte
@@ -2,16 +2,12 @@
   import type { LayoutProps } from './$types';
   import modeobserver from './utils/modeobserver';
   import { onMount } from 'svelte';
-  import { Runatics } from 'runatics';
 
   let { children, data }: LayoutProps = $props();
-  const analyticsId = data.ANALYTICS_ID as string;
-  // console.log('analyticsId', data.ANALYTICS_ID);
+
+  // console.log('data.FATHOM_ID', data.FATHOM_ID);
 
   onMount(modeobserver);
 </script>
 
-{#if analyticsId}
-  <Runatics {analyticsId} />
-{/if}
 {@render children()}
