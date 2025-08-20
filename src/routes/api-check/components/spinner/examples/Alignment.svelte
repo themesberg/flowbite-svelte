ab0828ddda2c8f5e7ffe6545f2d94a524989b139
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-08-20 08:14:15 +0200

    fix(SpeedDial): warnThemeDeprecatoin
    api-check: speed-dial and spinner

---
+++ src/routes/api-check/components/spinner/examples/Alignment.svelte
@@ -0,0 +1,5 @@
+<script>
+  import { Spinner } from "flowbite-svelte";
+</script>
+
+<div class="text-left"><Spinner /></div><div class="text-center"><Spinner /></div><div class="text-right"><Spinner /></div>
---
+++ src/routes/api-check/components/spinner/examples/Sizes.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { Spinner } from "flowbite-svelte";
+</script>
+
+<Spinner size="4" />
+<Spinner size="6" />
+<Spinner size="8" />
