2bb22aca5b9fdd56b59dc2fbe5d9442916e1155f
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-08-20 19:54:36 +0200

    api-check: toast

---
+++ src/routes/api-check/components/toast/examples/Fly.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Toast } from "flowbite-svelte";
+  import { fly } from "svelte/transition";
+  import { DownloadOutline } from "flowbite-svelte-icons";
+</script>
+
+<Toast transition={fly} params={{ x: 200 }} color="green" class="mb-4">
+  {#snippet icon()}
+    <DownloadOutline class="h-6 w-6" />
+  {/snippet}
+  Transition type: fly right
+</Toast>
+
+<Toast transition={fly} params={{ y: 200 }} color="green">
+  {#snippet icon()}
+    <DownloadOutline class="h-6 w-6" />
+  {/snippet}
+  Transition type: fly down
+</Toast>
---
+++ src/routes/api-check/components/toast/examples/Simple.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { Toast } from "flowbite-svelte";
+  import { PaperPlaneOutline } from "flowbite-svelte-icons";
+</script>
+
+<Toast dismissable={false}>
+  {#snippet icon()}
+    <PaperPlaneOutline class="text-primary-600 dark:text-primary-500 h-5 w-5 rotate-45" />
+  {/snippet}
+  <div class="ps-4 text-sm font-normal">Message sent successfully.</div>
+</Toast>
