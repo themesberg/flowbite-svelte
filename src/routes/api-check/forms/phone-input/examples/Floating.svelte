21ff2378529260b7467839f38507b62c1abfb866
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-08-23 15:16:38 +0200

    api-check: phone-input

---
+++ src/routes/api-check/forms/phone-input/examples/Floating.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { PhoneInput } from "flowbite-svelte";
+</script>
+
+<form class="mx-auto max-w-xs">
+  <PhoneInput phoneType="floating" aria-describedby="helper-text-explanation" id="floating-phone-number" placeholder=" " required />
+</form>
---
+++ src/routes/api-check/typography/paragraph/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import HighlightCompo from "../../utils/HighlightCompo.svelte";
+  import CodeWrapper from "../../utils/CodeWrapper.svelte";
+  import H1 from "../../utils/H1.svelte";
+  import H2 from "../../utils/H2.svelte";
+
+  import * as ExampleComponents from "./examples";
+  const exampleModules = import.meta.glob("./examples/*.svelte", {
+    query: "?raw",
+    import: "default",
+    eager: true
+  }) as Record<string, string>;
+</script>
+
+<H1>Paragraph</H1>
