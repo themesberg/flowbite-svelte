7ed8a6d5b4bdf3ce64522f70f81adcc5e74601ec
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-08-23 18:47:21 +0200

    api-check: search-input

--- src/routes/api-check/forms/search-input/+page.svelte
+++ src/routes/api-check/forms/search-input/+page.svelte
@@ -13,3 +13,66 @@
 </script>
 
 <H1>Search Input</H1>
+<H2>Default</H2>
+<CodeWrapper>
+  <ExampleComponents.Default />
+  {#snippet codeblock()}
+    <HighlightCompo codeLang="ts" code={exampleModules["./examples/Default.svelte"] as string} />
+  {/snippet}
+</CodeWrapper>
+
+<H2>Clearable</H2>
+<CodeWrapper>
+  <ExampleComponents.Clearable />
+  {#snippet codeblock()}
+    <HighlightCompo codeLang="ts" code={exampleModules["./examples/Clearable.svelte"] as string} />
+  {/snippet}
+</CodeWrapper>
+
+<H2>Event</H2>
+<CodeWrapper>
+  <ExampleComponents.Event />
+  {#snippet codeblock()}
+    <HighlightCompo codeLang="ts" code={exampleModules["./examples/Event.svelte"] as string} />
+  {/snippet}
+</CodeWrapper>
+
+<H2>ElementRef</H2>
+<CodeWrapper>
+  <ExampleComponents.ElementRef />
+  {#snippet codeblock()}
+    <HighlightCompo codeLang="ts" code={exampleModules["./examples/ElementRef.svelte"] as string} />
+  {/snippet}
+</CodeWrapper>
+
+<H2>Dropdown</H2>
+<CodeWrapper>
+  <ExampleComponents.Dropdown />
+  {#snippet codeblock()}
+    <HighlightCompo codeLang="ts" code={exampleModules["./examples/Dropdown.svelte"] as string} />
+  {/snippet}
+</CodeWrapper>
+
+<H2>Location</H2>
+<CodeWrapper>
+  <ExampleComponents.Location />
+  {#snippet codeblock()}
+    <HighlightCompo codeLang="ts" code={exampleModules["./examples/Location.svelte"] as string} />
+  {/snippet}
+</CodeWrapper>
+
+<H2>Voice</H2>
+<CodeWrapper>
+  <ExampleComponents.Voice />
+  {#snippet codeblock()}
+    <HighlightCompo codeLang="ts" code={exampleModules["./examples/Voice.svelte"] as string} />
+  {/snippet}
+</CodeWrapper>
+
+<H2>Example</H2>
+<CodeWrapper>
+  <ExampleComponents.Example />
+  {#snippet codeblock()}
+    <HighlightCompo codeLang="ts" code={exampleModules["./examples/Example.svelte"] as string} />
+  {/snippet}
+</CodeWrapper>
