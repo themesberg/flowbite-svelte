316b323bcfaa99d0ba92079c9ac5217242abd8ce
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-08-24 18:28:59 +0200

    api-check: utilities

---
+++ src/routes/api-check/utilities/toolbar/examples/CommentBox.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Toolbar, ToolbarButton, ToolbarGroup, Textarea, Button } from "flowbite-svelte";
+  import { PaperClipOutline, MapPinAltSolid, ImageOutline } from "flowbite-svelte-icons";
+</script>
+
+<form class="mb-4">
+  <Textarea placeholder="Write a comment">
+    {#snippet footer()}
+      <div class="flex items-center justify-between">
+        <Button type="submit">Post comment</Button>
+        <Toolbar embedded>
+          <ToolbarButton name="Attach file"><PaperClipOutline class="h-5 w-5 rotate-45" /></ToolbarButton>
+          <ToolbarButton name="Embed map"><MapPinAltSolid class="h-5 w-5" /></ToolbarButton>
+          <ToolbarButton name="Upload image"><ImageOutline class="h-5 w-5" /></ToolbarButton>
+        </Toolbar>
+      </div>
+    {/snippet}
+  </Textarea>
+</form>
+<p class="ms-auto text-xs text-gray-500 dark:text-gray-400">
+  Remember, contributions to this topic should follow our <a href="/" class="text-blue-600 hover:underline dark:text-blue-500">Community Guidelines</a>
+  .
+</p>
---
+++ src/routes/api-check/utilities/toolbar/examples/Groups.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Toolbar, ToolbarButton, ToolbarGroup } from "flowbite-svelte";
+  import { HomeOutline, EnvelopeOutline, ImageOutline, CogOutline } from "flowbite-svelte-icons";
+</script>
+
+<Toolbar color="green">
+  <ToolbarGroup>
+    <ToolbarButton color="green"><HomeOutline class="h-6 w-6" /></ToolbarButton>
+    <ToolbarButton color="green"><EnvelopeOutline class="h-6 w-6" /></ToolbarButton>
+    <ToolbarButton color="green"><ImageOutline class="h-6 w-6" /></ToolbarButton>
+  </ToolbarGroup>
+  <ToolbarGroup>
+    <ToolbarButton color="green"><HomeOutline class="h-6 w-6" /></ToolbarButton>
+    <ToolbarButton color="green"><EnvelopeOutline class="h-6 w-6" /></ToolbarButton>
+    <ToolbarButton color="green"><ImageOutline class="h-6 w-6" /></ToolbarButton>
+  </ToolbarGroup>
+  {#snippet end()}
+    <ToolbarButton color="green"><CogOutline class="h-6 w-6" /></ToolbarButton>
+  {/snippet}
+</Toolbar>
