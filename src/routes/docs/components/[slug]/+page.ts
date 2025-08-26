448e574dfaa7cb528f5348263e23fb999e590be5
Jakub Jagielka <jjagielka@gmail.com>
2025-08-26 20:29:31 +0200

    reload page load after example update

--- src/routes/docs/components/[slug]/+page.ts
+++ src/routes/docs/components/[slug]/+page.ts
@@ -1,7 +1,11 @@
 import type { PageLoad } from "./$types";
+import { redirect } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params , url}) => {
   const post = await import(`../${params.slug}.md`);
+  if(!post.metadata)  {
+    redirect(307, url)
+  }
   const { title, dir } = post.metadata;
   const content = post.default;
 
