4f3da69b123f06f993afa7889338e673057f1d0c
Jakub Jagielka <jjagielka@gmail.com>
2025-08-27 08:02:39 +0200

    workaround for update less dangerous

--- src/routes/docs/components/[slug]/+page.ts
+++ src/routes/docs/components/[slug]/+page.ts
@@ -1,10 +1,11 @@
 import type { PageLoad } from "./$types";
-import { redirect } from '@sveltejs/kit';
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 export const load: PageLoad = async ({ params , url}) => {
   const post = await import(`../${params.slug}.md`);
   if(!post.metadata)  {
-    redirect(307, url)
+    await delay(1000)
   }
   const { title, dir } = post.metadata;
   const content = post.default;
