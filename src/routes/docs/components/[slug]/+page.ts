c68f52610bdb0a664e9711b2fadf6df9e37d4dd9
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-08-27 10:00:20 +0200

    fix: change delay to 2000 and add type

--- src/routes/docs/components/[slug]/+page.ts
+++ src/routes/docs/components/[slug]/+page.ts
@@ -1,11 +1,11 @@
 import type { PageLoad } from "./$types";
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+const delay = (ms: number) => new Promise<void>((res) => setTimeout(res, ms));
 
 export const load: PageLoad = async ({ params , url}) => {
   const post = await import(`../${params.slug}.md`);
   if(!post.metadata)  {
-    await delay(1000)
+    await delay(2000)
   }
   const { title, dir } = post.metadata;
   const content = post.default;
