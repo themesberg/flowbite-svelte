240c4e602372d2a24725bea0ded0fe85fcd524df
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-11-07 09:00:50 +0100

    docs: update virtual-list page (#1824)
    * docs: update virtual-list page
    
    * docs: fix Jump example

--- src/routes/docs-examples/extend/virtual-list/ClassOverride.svelte
+++ src/routes/docs-examples/extend/virtual-list/ClassOverride.svelte
@@ -5,6 +5,8 @@
 
 <VirtualList {items} classes={{ item: "[contain:layout_style_paint] h-12" }}>
   {#snippet children(item, index)}
+    <div class="text-gray-900 dark:text-white p-3 hover:bg-gray-50 dark:hover:bg-gray-800">
     {index + 1}: {item}
+    </div>
   {/snippet}
 </VirtualList>
