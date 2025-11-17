5b19d2476462369a5bf2da49b9553d25bf962cc0
Jakub Jagielka <jjagielka@gmail.com>
2025-11-17 09:48:40 +0100

    Button loading uses Spinner

--- src/lib/buttons/Button.svelte
+++ src/lib/buttons/Button.svelte
@@ -4,6 +4,7 @@
   import type { SizeType, ButtonProps } from "$lib";
   import { getTheme } from "$lib/theme/themeUtils";
   import { button } from "./theme";
+  import Spinner from "$lib/spinner/Spinner.svelte";
 
   const group: SizeType = getContext("group");
   const ctxDisabled: boolean | undefined = getContext("disabled");
@@ -28,10 +29,7 @@
   <button type="button" {...restProps} class={btnCls} disabled={isDisabled}>
     {@render children?.()}
     {#if loading}
-      <svg class="ml-2 h-4 w-4 animate-spin text-white" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z" />
-      </svg>
+      <Spinner size="4" class="ms-2" type="loading" {color} />
     {/if}
   </button>
 {:else}
--- src/lib/spinner/Spinner.svelte
+++ src/lib/spinner/Spinner.svelte
@@ -77,6 +77,11 @@
       <animateTransform attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="1.2s" repeatCount="indefinite" />
     </g>
   </svg>
+{:else if type === "loading"}
+  <svg class={spinnerClass} fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z" />
+  </svg>
 {/if}
 
 <!--
