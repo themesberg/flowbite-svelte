2eacf67d6ea4ed7cfa83c3dd0b88036f47be806f
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-11-21 13:25:52 +0100

    docs: remove extra space

--- src/routes/docs-examples/components/device-mockups/Smartwatch.svelte
+++ src/routes/docs-examples/components/device-mockups/Smartwatch.svelte
@@ -4,5 +4,5 @@
 
 <DeviceMockup device="smartwatch">
   <img src="https://flowbite.s3.amazonaws.com/docs/device-mockups/watch-screen-image.png" class="w-[188px] h-[193px] dark:hidden" alt="smartwatch example 1" />
-  <img src="https://flowbite.s3.amazonaws.com/docs/device-mockups/watch-screen-image-dark.png" class="w-[188px] h-[193px] hidden  dark:block" alt="smartwatch example 2" />
+  <img src="https://flowbite.s3.amazonaws.com/docs/device-mockups/watch-screen-image-dark.png" class="w-[188px] h-[193px] hidden dark:block" alt="smartwatch example 2" />
 </DeviceMockup>
