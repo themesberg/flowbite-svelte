df01323b2f52e9f237a1a8a33c635acd5e84d96c
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2026-03-29 10:05:40 +0200

    fix: footer style

--- src/lib/footer/theme.ts
+++ src/lib/footer/theme.ts
@@ -2,14 +2,14 @@ import { tv, type VariantProps } from "tailwind-variants";
 import type { Classes } from "$lib/theme-provider/themeUtils";
 
 export const footer = tv({
-  base: "bg-white dark:bg-gray-800",
+  base: "bg-neutral-primary-soft",
   variants: {
     footerType: {
-      default: "p-4 rounded-base shadow md:flex md:items-center md:justify-between md:p-6",
-      sitemap: "bg-white dark:bg-gray-900",
+      default: "p-4 rounded-base shadow-xs border border-default md:flex md:items-center md:justify-between md:p-6",
+      sitemap: "bg-neutral-primary",
       socialmedia: "p-4 sm:p-6",
-      logo: "p-4 rounded-lg shadow md:px-6 md:py-8",
-      sticky: "fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600"
+      logo: "p-4 rounded-base shadow-xs border border-default md:px-6 md:py-8",
+      sticky: "fixed bottom-0 left-0 z-20 w-full p-4 border-t border-default shadow-sm md:flex md:items-center md:justify-between md:p-6"
     }
   }
 });
@@ -19,7 +19,7 @@ export type FooterBrandVariants = VariantProps<typeof footerBrand> & Classes<typ
 export const footerBrand = tv({
   slots: {
     base: "flex items-center",
-    label: "self-center text-2xl font-semibold whitespace-nowrap dark:text-white",
+    label: "text-heading self-center text-2xl font-semibold whitespace-nowrap",
     image: "me-3 h-8"
   }
 });
@@ -28,18 +28,18 @@ export type FooterCopyrightVariants = VariantProps<typeof footerCopyright> & Cla
 
 export const footerCopyright = tv({
   slots: {
-    base: "block text-sm text-gray-500 sm:text-center dark:text-gray-400",
+    base: "block text-sm text-body sm:text-center",
     link: "hover:underline",
     label: "ms-1"
   }
 });
 
 export const footerIcon = tv({
-  base: "text-gray-500 hover:text-gray-900 dark:hover:text-white"
+  base: "text-body hover:text-heading"
 });
 
 export const footerLinkGroup = tv({
-  base: "text-gray-600 dark:text-gray-400"
+  base: "text-body font-medium"
 });
 
 export type FooterLinkVariants = VariantProps<typeof footerLink> & Classes<typeof footerLink>;
